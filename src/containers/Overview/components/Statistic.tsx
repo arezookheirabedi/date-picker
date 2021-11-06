f2d9634b824de2c485d066d12f63a36fedc93701
emadabbasii <emadabbasee@gmail.com>
2021-11-06 11:01:55 +0330

    scale icons on  hover action

--- src/containers/Overview/components/Statistic.tsx
+++ src/containers/Overview/components/Statistic.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-const Statistic: React.FC<{icon: any; text: string; count: number}> = ({icon, text, count}) => (
+const Statistic: React.FC<{ icon: any; text: string; count: number }> = ({icon, text, count}) => (
   <fieldset className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
-    <legend className="text-black mx-auto px-3">
-      <img src={icon} alt="" />
+    <legend className="text-black mx-auto px-3 transform transition duration-500 hover:scale-150">
+      <img src={icon} alt=""/>
     </legend>
     <div className="flex text-black text-lg mx-auto">{(count || 0).toLocaleString('fa')}</div>
     <div className="flex text-gray-500 text-sm  mx-auto">{text || ''}</div>
