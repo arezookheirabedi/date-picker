ccf2810513ee1b1d9dc607c6388128ee80189c1d
emadabbasii <emadabbasee@gmail.com>
2021-11-22 15:03:08 +0330

    add some property to route model

--- src/models/route.ts
+++ src/models/route.ts
@@ -4,12 +4,15 @@ export interface IRoute {
   keyIndex: string;
   icon?: (active: boolean, disabled?: boolean) => ReactElement;
   title: string;
-  link: string;
+  link?: string;
+  simLink?: string;
+  keyString?: string;
   exact?: boolean;
   disabled?: boolean;
   inMenu?: boolean;
   showGuildList?: boolean;
   deleteable?: boolean;
   children?: IRoute[];
+  subMenu?: IRoute[];
   main?: (props: any) => any;
 }
