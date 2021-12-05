8d8f3369754b0793f9c7619f55eeaf273d5fb5d1
DESKTOP-P5OO4UH\VASL <muh.shahabipour@gmail.com>
2021-12-05 12:49:17 +0330

    add overview per category

--- src/services/transport.service.ts
+++ src/services/transport.service.ts
@@ -3,10 +3,18 @@ import request from "../helpers/request";
 
 function testsInTransport(params: any) {
   return request
-    .withHeaders({"Content-Type": "application/json;utf-8"})
-    .build().get(`/api/v1/transport/cars/health/test-results/service-type-based/count`, params);
+    .withHeaders({ "Content-Type": "application/json;utf-8" })
+    .build().get(`/api/v1/transport/cars/health/test-results/service-type-based/count?lang=fa`, params);
+}
+
+
+function overviewCategory(params: any) {
+  return request
+    .withHeaders({ "Content-Type": "application/json;utf-8" })
+    .build().get(`/api/v1/transport/cars/service-type-based/count?lang=fa`, params);
 }
 
 export default {
-  testsInTransport
+  testsInTransport,
+  overviewCategory
 }
