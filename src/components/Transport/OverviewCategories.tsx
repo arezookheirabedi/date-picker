3fc2ee98c7451d0d392e72795eb71f72d6b2b597
DESKTOP-P5OO4UH\VASL <muh.shahabipour@gmail.com>
2021-12-05 12:49:17 +0330

    add overview per category

--- src/components/Transport/OverviewCategories.tsx
+++ src/components/Transport/OverviewCategories.tsx
@@ -1,174 +1,231 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from 'react';
+import transportService from 'src/services/transport.service';
+import DatePickerModal from '../DatePickerModal';
+import calendar from '../../assets/images/icons/calendar.svg';
+import Table from '../Table';
+import CategoryDonut from '../../containers/Guild/components/CategoryDonut';
+import {toPersianDigit} from '../../helpers/utils';
+import Spinner from '../Spinner';
 
-import DatePickerModal from "../DatePickerModal";
-import calendar from "../../assets/images/icons/calendar.svg";
-import Table from "../Table";
-import CategoryDonut from "../../containers/Guild/components/CategoryDonut";
-import {toPersianDigit} from "../../helpers/utils";
-
-
-const OverviewCategories = () => {
+const OverviewCategories: React.FC<{}> = () => {
   const [showDatePicker, setShowDatePicker] = useState(false);
+  const [loading, setLoading] = useState(false);
   // eslint-disable-next-line
   const [selectedDayRange, setSelectedDayRange] = useState({
     from: {day: 1, month: 9, year: 1400},
-    to: {day: 20, month: 9, year: 1400}
+    to: {day: 20, month: 9, year: 1400},
   }) as any;
+
+  async function fetcher(params: any) {
+    setLoading(true);
+    try {
+      const {data} = await transportService.overviewCategory(params);
+
+      console.log(data);
+
+      // let normalizedDate = [] as any;
+      // data.map((item: any) => {
+      //   if (item.total !== 0) {
+      //     console.log(item);
+      //     // return normalizedDate.push({
+      //     //   title: getServiceTypeName(item.serviceType),
+      //     //   percentage: ((item.count * 100) / item.total).toFixed(2),
+      //     //   color: getColorByServiceTypeName(item.serviceType),
+      //     // });
+      //   }
+      //   return null;
+      // });
+
+      // normalizedDate = normalizedDate.sort((a: any, b: any) => {
+      //   return b.percentage - a.percentage;
+      // });
+
+      // setPyramidData(normalizedDate);
+      // // setPyramidData(data);
+      // // console.log(data);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetcher({});
+  }, []);
 
   const focusFromDate = () => {
     setShowDatePicker(true);
-  }
+  };
 
   const generateFromDate: any = () => {
     // eslint-disable-next-line
-    return selectedDayRange.from ? selectedDayRange.from.year + '/' + selectedDayRange.from.month + '/' + selectedDayRange.from.day : '';
-  }
+    return selectedDayRange.from
+      ? // eslint-disable-next-line
+        selectedDayRange.from.year +
+          '/' +
+          selectedDayRange.from.month +
+          '/' +
+          selectedDayRange.from.day
+      : '';
+  };
 
   const generateToDate: any = () => {
     // eslint-disable-next-line
-    return selectedDayRange.to ? selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day : '';
-  }
+    return selectedDayRange.to
+      ? // eslint-disable-next-line
+        selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day
+      : '';
+  };
 
   return (
     <fieldset className="text-center border rounded-xl p-4 mb-16">
-      <legend className="text-black mx-auto px-3">
-        نگاه کلی رسته‌های حمل و نقل
-      </legend>
+      <legend className="text-black mx-auto px-3">نگاه کلی رسته‌های حمل و نقل</legend>
       <div className="flex align-center justify-start mb-8">
-        {showDatePicker ? <DatePickerModal setSelectedDayRange={setSelectedDayRange} selectedDayRange={selectedDayRange}
-                                           setShowDatePicker={setShowDatePicker} showDatePicker/> : null}
+        {showDatePicker ? (
+          <DatePickerModal
+            setSelectedDayRange={setSelectedDayRange}
+            selectedDayRange={selectedDayRange}
+            setShowDatePicker={setShowDatePicker}
+            showDatePicker
+          />
+        ) : null}
         <div className="relative z-20 inline-block text-left shadow-custom rounded-lg px-4 py-1">
           <div
             className="inline-flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
             onClick={focusFromDate}
           >
-                    <span className="ml-4 whitespace-nowrap truncate text-xs">
-                      {toPersianDigit(generateFromDate())}
-                     </span>
-            <img src={calendar} alt="x" className="w-5 h-5"/>
+            <span className="ml-4 whitespace-nowrap truncate text-xs">
+              {toPersianDigit(generateFromDate())}
+            </span>
+            <img src={calendar} alt="x" className="w-5 h-5" />
           </div>
         </div>
         <div className="flex items-center justify-start mx-4">
-          <span className="dash-separator"/>
+          <span className="dash-separator" />
         </div>
         <div className=" shadow-custom rounded-lg px-4 py-1">
           <div
             className="flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
             onClick={focusFromDate}
           >
-                    <span className="ml-4 whitespace-nowrap truncate text-xs">
-                      {toPersianDigit(generateToDate())}
-                     </span>
-            <img src={calendar} alt="x" className="w-5 h-5"/>
+            <span className="ml-4 whitespace-nowrap truncate text-xs">
+              {toPersianDigit(generateToDate())}
+            </span>
+            <img src={calendar} alt="x" className="w-5 h-5" />
           </div>
         </div>
       </div>
       <div className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
-        <Table
-          dataSet={[
-            {
-              id: '617d54a39d4b1f0efd2d5904',
-              name: 'اسنپ',
-              employeesCount: 60,
-              infectedCount: 2253,
-              saveCount: 2279,
-              deadCount: 91,
-              infectedPercent: 24,
-            },
-            {
-              id: '617d54a3e34765550c16dce3',
-              name: 'تپسی',
-              employeesCount: 840,
-              infectedCount: 605,
-              saveCount: 2930,
-              deadCount: 1294,
-              infectedPercent: 93,
-            },
-            {
-              id: '617d54a341381bf85e5b6eca',
-              name: 'سرویس مدارس',
-              employeesCount: 3565,
-              infectedCount: 3727,
-              saveCount: 3089,
-              deadCount: 741,
-              infectedPercent: 92,
-            },
-            {
-              id: '617d54a3b02e2e7d71ca9d12',
-              name: 'تاکسی فرودگاهی',
-              employeesCount: 1998,
-              infectedCount: 2748,
-              saveCount: 628,
-              deadCount: 2815,
-              infectedPercent: 35,
-            },
-            {
-              id: '617d54a35649c264fcd29dc7',
-              name: 'اتوبوس رانی',
-              employeesCount: 3384,
-              infectedCount: 2138,
-              saveCount: 3535,
-              deadCount: 2525,
-              infectedPercent: 74,
-            },
-            {
-              id: '617d54a3feaea113cefef758',
-              name: 'تاکسی تلفنی',
-              employeesCount: 134,
-              infectedCount: 647,
-              saveCount: 807,
-              deadCount: 1156,
-              infectedPercent: 72,
-            },
-          ]}
-          pagination={{pageSize: 20, maxPages: 3}}
-          columns={[
-            {
-              name: 'وضعیت کلی',
-              key: '',
-              render: () => <CategoryDonut/>,
-              className: 'flex justify-center w-full',
-            },
-            {
-              name: 'رسته های حمل و نقل',
-              key: 'name',
-              render: (v: any, record, index: number) => (
-                <span>
-                  {(index + 1).toLocaleString('fa')}.{v}
-                </span>
-              ),
-            },
-            {
-              name: 'تعداد رانندگان',
-              key: 'employeesCount',
-              render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
-            },
-            {
-              name: 'درصد ابتلا',
-              key: 'infectedPercent',
-              render: (v: any) => <span>{(v as number).toLocaleString('fa')}%</span>,
-            },
-            {
-              name: 'تعداد مبتلایان',
-              key: 'infectedCount',
-              render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
-            },
-            {
-              name: 'تعداد بهبودیافتگان',
-              key: 'saveCount',
-              render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
-            },
-            {
-              name: 'تعداد فوت‌شدگان',
-              key: 'deadCount',
-              render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
-            },
-          ]}
-          totalItems={0}
-        />
+        {loading ? (
+          <Spinner />
+        ) : (
+          <Table
+            dataSet={[
+              {
+                id: '617d54a39d4b1f0efd2d5904',
+                name: 'اسنپ',
+                employeesCount: 60,
+                infectedCount: 2253,
+                saveCount: 2279,
+                deadCount: 91,
+                infectedPercent: 24,
+              },
+              {
+                id: '617d54a3e34765550c16dce3',
+                name: 'تپسی',
+                employeesCount: 840,
+                infectedCount: 605,
+                saveCount: 2930,
+                deadCount: 1294,
+                infectedPercent: 93,
+              },
+              {
+                id: '617d54a341381bf85e5b6eca',
+                name: 'سرویس مدارس',
+                employeesCount: 3565,
+                infectedCount: 3727,
+                saveCount: 3089,
+                deadCount: 741,
+                infectedPercent: 92,
+              },
+              {
+                id: '617d54a3b02e2e7d71ca9d12',
+                name: 'تاکسی فرودگاهی',
+                employeesCount: 1998,
+                infectedCount: 2748,
+                saveCount: 628,
+                deadCount: 2815,
+                infectedPercent: 35,
+              },
+              {
+                id: '617d54a35649c264fcd29dc7',
+                name: 'اتوبوس رانی',
+                employeesCount: 3384,
+                infectedCount: 2138,
+                saveCount: 3535,
+                deadCount: 2525,
+                infectedPercent: 74,
+              },
+              {
+                id: '617d54a3feaea113cefef758',
+                name: 'تاکسی تلفنی',
+                employeesCount: 134,
+                infectedCount: 647,
+                saveCount: 807,
+                deadCount: 1156,
+                infectedPercent: 72,
+              },
+            ]}
+            pagination={{pageSize: 20, maxPages: 3}}
+            columns={[
+              {
+                name: 'وضعیت کلی',
+                key: '',
+                render: () => <CategoryDonut />,
+                className: 'flex justify-center w-full',
+              },
+              {
+                name: 'رسته های حمل و نقل',
+                key: 'name',
+                render: (v: any, record, index: number) => (
+                  <span>
+                    {(index + 1).toLocaleString('fa')}.{v}
+                  </span>
+                ),
+              },
+              {
+                name: 'تعداد رانندگان',
+                key: 'employeesCount',
+                render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'درصد ابتلا',
+                key: 'infectedPercent',
+                render: (v: any) => <span>{(v as number).toLocaleString('fa')}%</span>,
+              },
+              {
+                name: 'تعداد مبتلایان',
+                key: 'infectedCount',
+                render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد بهبودیافتگان',
+                key: 'saveCount',
+                render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد فوت‌شدگان',
+                key: 'deadCount',
+                render: (v: any) => <span>{(v as number).toLocaleString('fa')}</span>,
+              },
+            ]}
+            totalItems={0}
+          />
+        )}
       </div>
     </fieldset>
-  )
-}
+  );
+};
 
 export default OverviewCategories;
