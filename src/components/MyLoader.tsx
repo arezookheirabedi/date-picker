5061c0a0d0f26fbd418bab47f91a8a9d8130272b
emadabbasii <emadabbasee@gmail.com>
2021-12-05 16:49:36 +0330

    add overview api

---
+++ src/components/MyLoader.tsx
@@ -0,0 +1,18 @@
+import React from "react"
+import ContentLoader from "react-content-loader"
+
+const MyLoader = (props : any) => (
+  <ContentLoader
+    speed={2}
+    width={400}
+    height={160}
+    viewBox="0 0 400 160"
+    backgroundColor="#f3f3f3"
+    foregroundColor="#ecebeb"
+    {...props}
+  >
+    <rect x="48" y="8" rx="3" ry="3" width="88" height="6" />
+  </ContentLoader>
+)
+
+export default MyLoader
