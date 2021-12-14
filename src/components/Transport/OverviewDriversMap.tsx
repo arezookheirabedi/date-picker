0cbaff5f5087bb125b46663e2cd7f5cebfd3b77f
emadabbasii <emadabbasee@gmail.com>
2021-12-14 14:16:38 +0330

    pull options from map component to overview driver map component

--- src/components/Transport/OverviewDriversMap.tsx
+++ src/components/Transport/OverviewDriversMap.tsx
@@ -1,15 +1,161 @@
 import React from "react";
+import {useHistory} from "react-router-dom";
+
 import Charts from "../Charts";
+import map from "../Charts/ir-all.geo.json";
+import mapData from "../Charts/mapData.json";
+
 
 const {Map} = Charts;
 
 interface OverviewDriversMapProps {
   sideCityStatus?: any;
-  cityTitle: string
+  cityTitle: string;
+  setQueryParams: any;
 }
 
-const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({sideCityStatus, cityTitle}) => {
 
+const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({
+                                                                 sideCityStatus,
+                                                                 cityTitle,
+                                                                 setQueryParams
+                                                               }) => {
+  const history = useHistory();
+  const options = {
+    chart: {
+      map,
+      height: "70%",
+      className: 'guild-map-chart',
+      // backgroundColor: '#F3F5F9',
+      margin: [0, 0, 50, 0],
+    },
+
+    title: {
+      text: '',
+    },
+    credits: {
+      enabled: false,
+    },
+
+    plotOptions: {
+      series: {
+        cursor: 'pointer',
+        point: {
+          events: {
+            click(e: any) {
+              history.push({
+                search: `?provinceId=${e.point.name}`
+              })
+              setQueryParams({provinceId: e.point.name})
+              // console.log(e.point.name);
+              // history.push({
+              //   pathname: '/dresses',
+              //   search: '?color=blue'
+              // })
+            }
+          }
+        }
+      }
+    },
+    subtitle: {
+      text: '',
+      floating: true,
+      align: 'right',
+      y: 50,
+      style: {
+        fontSize: '16px',
+      },
+    },
+    legend: {
+      itemStyle: {
+        fontSize: "12px",
+        fontWeight: '400',
+        color: '#707070',
+        transform: 'translate(-20px,20px)',
+        direction: 'rtl',
+        textAlign: 'right',
+      },
+      align: "center",
+      verticalAlign: "bottom",
+      squareSymbol: false,
+      valueDecimals: 0,
+      symbolRadius: 4,
+      symbolHeight: 7,
+      symbolWidth: 85,
+      itemDistance: -40
+    },
+    colorAxis: {
+      dataClasses: [
+        {
+          from: 500,
+          to: 1000,
+          color: '#AB0A0A',
+          name: 'بیشتر از ۵۰٪',
+        },
+        {
+          from: 400,
+          to: 500,
+          color: '#CF0D0D',
+          name: '%۲۰ الی ۵۰%',
+        },
+        {
+          from: 300,
+          to: 400,
+          color: '#FF9114',
+          name: '%۱۰ الی ۲۰%',
+        }, {
+          from: 200,
+          to: 300,
+          color: '#FFC700',
+          name: '%۵ الی ۱۰%',
+        }, {
+          from: 100,
+          to: 200,
+          color: '#F8D354',
+          name: '%۲ الی ۵%',
+
+        }, {
+          from: 1,
+          to: 100,
+          color: '#FBE186',
+          name: '%کمتر از ۲'
+        }]
+    },
+
+    mapNavigation: {
+      enableButtons: false,
+      enabled: true,
+      enableMouseWheelZoom: false,
+      enableDoubleClickZoomTo: false,
+      enableDoubleClickZoom: false,
+      enableTouchZoom: false,
+      buttonOptions: {
+        verticalAlign: 'bottom',
+      },
+    },
+
+    series: [
+      {
+        data: mapData,
+        keys: ['hasc', 'value'],
+        joinBy: 'hasc',
+        name: 'covid',
+        borderColor: '#ffffff',
+        borderWidth: 1,
+        dataLabels: {
+          enabled: true,
+          format: '{point.properties.fa-name}',
+          style: {
+            fontWeight: "400",
+            color: '#ffffff',
+            fontFamily: 'inherit',
+            backgroundColor: "transparent"
+          }
+        },
+
+      },
+    ],
+  };
   return (
     <fieldset className="text-center border rounded-xl p-4">
       <legend className="text-black mx-auto px-3">نگاه کلی به وضعیت حمل و نقل عمومی استان‌
@@ -18,7 +164,7 @@ const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({sideCityStatus, 
       </legend>
       <div className="flex w-full rounded-xl bg-white pb-8 pt-8  shadow">
         <ul className="w-5/6">
-          <Map/>
+          <Map options={options}/>
         </ul>
         <ul className="w-1/6">
           {
