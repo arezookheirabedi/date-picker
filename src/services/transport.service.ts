6c15c09ba9e1b5b5ffc4ff3510398ba998b3aff8
emadabbasii <emadabbasee@gmail.com>
2021-12-20 15:41:13 +0330

    custom some service with handle new params

--- src/services/transport.service.ts
+++ src/services/transport.service.ts
@@ -1,91 +1,102 @@
 import request from "../helpers/request";
 
 
+function numberOfDrivers(province: any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfDrivers: true,
+      province
+    });
+}
+
+function numberOfPositiveDrivers(province: any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfPositiveDrivers: true,
+      province
+    });
+}
+
+function numberOfRecoveredDrivers(province: any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfRecoveredDrivers: true,
+      province
+    });
+}
+
+
+function numberOfVaccination(province: any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfVaccination: true,
+      province
+    });
+}
+
+
+function numberOfPlaqueVisited(province:any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfPlaqueVisited: true,
+      province
+    });
+}
+
+
+function numberOfTestResults(province:any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfTestResults: true,
+      province
+    });
+}
+
+
+function numberOfPositivePlaqueVisited(province:any = null) {
+  return request
+    .withHeaders({"Content-Type": "application/json;utf-8"})
+    .build().get(`/api/v1/transport/reports/general${province ? '/province' : ''}`, {
+      numberOfPositivePlaqueVisited: true,
+      province
+    });
+}
+
+
 function testsInTransport(params: any) {
   return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
+    .withHeaders({"Content-Type": "application/json;utf-8"})
     .build().get(`/api/v1/transport/cars/health/test-results/service-type-based/count?lang=fa&status=POSITIVE&status=NEGATIVE`, params);
 }
 
 
 function overviewCategory(params: any) {
   return request
-    .withHeaders({ "Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000) })
+    .withHeaders({"Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000)})
     .build().get(`/api/v1/transport/cars/service-type-based/count?lang=fa`, params);
 }
 
-function numberOfDrivers() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfDrivers: true
-    });
-}
-
-function numberOfPlaqueVisited() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfPlaqueVisited: true
-    });
-}
-
-function numberOfPositiveDrivers() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfPositiveDrivers: true
-    });
-}
-
-function numberOfPositivePlaqueVisited() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfPositivePlaqueVisited: true
-    });
-}
-
-function numberOfRecoveredDrivers() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfRecoveredDrivers: true
-    });
-}
-
-function numberOfTestResults() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfTestResults: true
-    });
-}
-
-function numberOfVaccination() {
-  return request
-    .withHeaders({ "Content-Type": "application/json;utf-8" })
-    .build().get(`/api/v1/transport/reports/general`, {
-      numberOfVaccination: true
-    });
-}
-
-
 function overviewVaccine(params: any) {
   return request
-    .withHeaders({ "Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000) })
+    .withHeaders({"Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000)})
     .build().get(`/api/v1/transport/reports/vaccine?lang=fa`, params);
 }
 
 function overviewVaccinePercent(params: any) {
   return request
-    .withHeaders({ "Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000) })
+    .withHeaders({"Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000)})
     .build().get(`/api/v1/transport/drivers/health/vaccination-doses/service-based/count?lang=fa`, params);
 }
 
 function linearOverviewPublicTransport(params: any) {
   return request
-    .withHeaders({ "Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000) })
+    .withHeaders({"Content-Type": "application/json;utf-8", timeout: (3 * 60 * 1000)})
     .build().get(`/api/v1/transport/drivers/health/test-result/time-based/count?lang=fa`, params);
 }
 
