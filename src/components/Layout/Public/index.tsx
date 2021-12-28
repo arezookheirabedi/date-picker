51a0005a539f11944dd337ea5632675a89d16930
emadabbasii <emadabbasee@gmail.com>
2021-12-28 15:35:40 +0330

    change title of website :

--- src/components/Layout/Public/index.tsx
+++ src/components/Layout/Public/index.tsx
@@ -35,7 +35,7 @@ export const PublicRoute: (any) => any = props => {
     <Route
       {...rest}
       render={comprops => (
-        restricted && isLogin() ? <Redirect to="/dashboard/overview"/> :
+        restricted && isLogin() ? <Redirect to="/dashboard/transport/public"/> :
           <PublicLayout>
             <Component {...comprops} />
           </PublicLayout>
