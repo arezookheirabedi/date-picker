6b52d0c8ab02e24434a034813b79b6d90147197a
emadabbasii <emadabbasee@gmail.com>
2022-01-02 16:59:47 +0330

    selected city after each click on the map

--- src/components/Transport/OverviewDriversMap.tsx
+++ src/components/Transport/OverviewDriversMap.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import {Link, useHistory} from 'react-router-dom';
+import React, {useEffect, useRef, useState} from 'react';
+import {Link, useHistory, useLocation} from 'react-router-dom';
 
 import Charts from '../Charts';
 import map from '../Charts/ir-all.geo.json';
 import mapData from '../Charts/mapData.json';
+
 
 const {Map} = Charts;
 
@@ -14,12 +15,162 @@ interface OverviewDriversMapProps {
 }
 
 const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({
-  sideCityStatus,
-  cityTitle,
-  destinationId,
-}) => {
+                                                                 sideCityStatus,
+                                                                 cityTitle,
+                                                                 destinationId,
+                                                               }) => {
+
+  const chartRef = useRef<any>(null);
+
+  const {search} = useLocation();
+
+  const sideCities = [
+    {
+      name: 'هرمزگان',
+      color: '#ccc',
+    },
+    {
+      name: 'بوشهر',
+      color: '#ccc',
+    },
+    {
+      name: 'کهگیلویه و بویراحمد',
+      color: '#ccc',
+    },
+    {
+      name: 'فارس',
+      color: '#ccc',
+    },
+    {
+      name: 'اصفهان',
+      color: '#ccc',
+    },
+    {
+      name: 'سمنان',
+      color: '#ccc',
+    },
+    {
+      name: 'گلستان',
+      color: '#ccc',
+    },
+    {
+      name: 'مازندران',
+      color: '#ccc',
+    },
+    {
+      name: 'تهران',
+      color: '#ccc',
+    },
+    {
+      name: 'مرکزی',
+      color: '#ccc',
+    },
+    {
+      name: 'یزد',
+      color: '#ccc',
+    },
+    {
+      name: 'چهارمحال و بختیاری',
+      color: '#ccc',
+    },
+    {
+      name: 'خوزستان',
+      color: '#ccc',
+    },
+    {
+      name: 'لرستان',
+      color: '#ccc',
+    },
+    {
+      name: 'ایلام',
+      color: '#ccc',
+    },
+    {
+      name: 'اردبیل',
+      color: '#ccc',
+    },
+    {
+      name: 'قم',
+      color: '#ccc',
+    },
+    {
+      name: 'همدان',
+      color: '#ccc',
+    },
+    {
+      name: 'زنجان',
+      color: '#ccc',
+    },
+    {
+      name: 'قزوین',
+      color: '#ccc',
+    },
+    {
+      name: 'آذربایجان غربی',
+      color: '#ccc',
+    },
+    {
+      name: 'آذربایجان شرقی',
+      color: '#ccc',
+    },
+    {
+      name: 'کرمانشاه',
+      color: '#ccc',
+    },
+    {
+      name: 'گیلان',
+      color: '#ccc',
+    },
+    {
+      name: 'کردستان',
+      color: '#ccc',
+    },
+    {
+      name: 'خراسان جنوبی',
+      color: '#ccc',
+    },
+    {
+      name: 'خراسان رضوی',
+      color: '#ccc',
+    },
+    {
+      name: 'خراسان شمالی',
+      color: '#ccc',
+    },
+    {
+      name: 'سیستان و بلوچستان',
+      color: '#ccc',
+    },
+    {
+      name: 'کرمان',
+      color: '#ccc',
+    },
+    {
+      name: 'البرز',
+      color: '#ccc',
+    },
+  ];
+
+  useEffect(() => {
+    const data = chartRef?.current?.chart.get('covid').data;
+    const params = new URLSearchParams(search);
+    const provinceName = params.get('provinceName') || ('تهران' as any);
+    const existsCity = sideCities.some((item: any) => {
+      return item.name === provinceName;
+    });
+    if (existsCity) {
+
+      const city = data.find((x: any) => x.properties['fa-name'] === provinceName)
+      city?.select()
+    } else {
+      const city = data.find((x: any) => x.properties['fa-name'] === 'تهران')
+      city?.select()
+    }
+  }, [])
+
+
   const history = useHistory();
-  const options = {
+  const [options] = useState({
     chart: {
       map,
       height: '70%',
@@ -41,6 +192,19 @@ const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({
         point: {
           events: {
             click(e: any) {
+              //
+              // const tag: any = this;
+              //
+              // const changeColor = (param: any) => {
+              //   console.log(previousPoint);
+              //   if (previousPoint) {
+              //     previousPoint.update({color: '#7cb5ec'});
+              //   }
+              //   previousPoint = param;
+              //   tag.update({color: '#fe5800'});
+              // }
+              //
+              // changeColor(tag);
               document.getElementById(destinationId)!.scrollIntoView({behavior: 'smooth'});
               history.push({
                 search: `?provinceName=${e.point.properties['fa-name']}`,
@@ -142,13 +306,31 @@ const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({
         verticalAlign: 'bottom',
       },
     },
-
+    tooltip: {
+      borderRadius: 16,
+      borderWidth: 0,
+      valueDecimals: 0,
+      style: {
+        color: "#fff",
+        fontFamily: 'inherit',
+      },
+      headerFormat: "",
+      pointFormat: '<div>{point.properties.fa-name}</div>',
+      backgroundColor: {
+        linearGradient: [0, 0, 0, 60],
+        stops: [
+          [0, '#374151'],
+          [1, '#6B7280'],
+        ],
+      },
+    },
     series: [
       {
         data: mapData,
+        id: 'covid',
         keys: ['hasc', 'value'],
         joinBy: 'hasc',
-        name: 'covid',
+        name: 'کوید',
         borderColor: '#ffffff',
         borderWidth: 1,
         dataLabels: {
@@ -161,9 +343,16 @@ const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({
             backgroundColor: 'transparent',
           },
         },
+        states: {
+          select: {
+            color: '#3b3b3b'
+          }
+        },
+        allowPointSelect: true
       },
     ],
-  };
+  });
+
   return (
     <fieldset className="text-center border rounded-xl p-4">
       <legend className="text-black mx-auto px-3">
@@ -175,7 +364,7 @@ const OverviewDriversMap: React.FC<OverviewDriversMapProps> = ({
           <div className="button button--primary px-5">نمایش وضعیت کل کشور</div>
         </Link>
         <div className="w-5/6 map-wrapper">
-          <Map options={options} />
+          <Map options={options} ref={chartRef}/>
         </div>
         <ul className="w-1/6">
           {sideCityStatus.map((item: any, index: any) => {
