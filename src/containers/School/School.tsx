2c0fded5fd35cfddc4dc6cedaa1d3275a7816e1a
shahabi.kheyrati <m.shahabipour@vasl.ir>
2022-01-04 12:47:04 +0330

    add fake data

--- src/containers/School/School.tsx
+++ src/containers/School/School.tsx
@@ -1,24 +1,24 @@
 import React from 'react';
 
-// import OverviewSchool from '../../components/School/Overview';
-// import OverviewSchoolEmploye from '../../components/School/OverviewSchoolEmploye';
-// import OverviewSchoolStudent from '../../components/School/OverviewSchoolStudent';
-// import OverviewCategories from '../../components/School/OverviewCategories';
-// import OverviewPatients from '../../components/School/OverviewPatients';
-// import OverviewOfVaccination from '../../components/School/OverviewOfVaccination';
+import OverviewSchool from '../../components/School/Overview';
+import OverviewSchoolEmploye from '../../components/School/OverviewSchoolEmploye';
+import OverviewSchoolStudent from '../../components/School/OverviewSchoolStudent';
+import OverviewCategories from '../../components/School/OverviewCategories';
+import OverviewPatients from '../../components/School/OverviewPatients';
+import OverviewOfVaccination from '../../components/School/OverviewOfVaccination';
 import TestStatus from '../../components/School/TestStatus';
 
 // import IconWrapper from "../../components/IconWrapper";
 const School: React.FC<{}> = () => {
   return (
     <div className="space-y-16 mb-8">
-      {/* <OverviewSchool />
+      <OverviewSchool />
       <OverviewSchoolEmploye />
       <OverviewSchoolStudent />
 
       <OverviewCategories />
       <OverviewPatients />
-      <OverviewOfVaccination /> */}
+      <OverviewOfVaccination />
       <TestStatus />
     </div>
   );
--- src/mocks/hcs.ts
+++ src/mocks/hcs.ts
@@ -66,18 +66,117 @@ instanceMockAdapter
   // eslint-disable-next-line
   .onGet(/\/api\/v1\/hcs\-reporter\/organizations\/(.*)\/test\-result\/tag\-based/g, {})
   .reply(() => {
+    /*
+    [
+      '{{repeat(15, 15)}}',
+      {
+        _id: '{{objectId()}}',
+        tag: 'a{{index()}}',
+        positiveCount: '{{integer(20, 40)}}',
+        negativeCount: '{{integer(30, 40)}}',
+        total: '{{integer(80, 110)}}'
+      }
+    ]
+    */
+
     const res: any = [
       {
+        _id: '61d40d2b964cae329b07652b',
         tag: 'a1',
-        positiveCount: 0,
-        negativeCount: 0,
-        total: 0,
+        positiveCount: 20,
+        negativeCount: 34,
+        total: 86,
       },
       {
+        _id: '61d40d2b781e3ce2d418bb92',
         tag: 'a2',
-        positiveCount: 0,
-        negativeCount: 0,
-        total: 0,
+        positiveCount: 25,
+        negativeCount: 39,
+        total: 105,
+      },
+      {
+        _id: '61d40d2bad4f2224340b34c7',
+        tag: 'a3',
+        positiveCount: 35,
+        negativeCount: 30,
+        total: 83,
+      },
+      {
+        _id: '61d40d2b588fd66e3455e82c',
+        tag: 'a4',
+        positiveCount: 39,
+        negativeCount: 35,
+        total: 95,
+      },
+      {
+        _id: '61d40d2bc636cf67692a0c38',
+        tag: 'a5',
+        positiveCount: 20,
+        negativeCount: 31,
+        total: 105,
+      },
+      {
+        _id: '61d40d2b78261f71ec6a4a41',
+        tag: 'a6',
+        positiveCount: 25,
+        negativeCount: 35,
+        total: 108,
+      },
+      {
+        _id: '61d40d2ba4cf23a0de5c9456',
+        tag: 'a7',
+        positiveCount: 28,
+        negativeCount: 31,
+        total: 89,
+      },
+      {
+        _id: '61d40d2b334834857d6cabe6',
+        tag: 'a8',
+        positiveCount: 38,
+        negativeCount: 40,
+        total: 108,
+      },
+      {
+        _id: '61d40d2b53a9573e73412301',
+        tag: 'a9',
+        positiveCount: 39,
+        negativeCount: 35,
+        total: 89,
+      },
+      {
+        _id: '61d40d2b5096449573435530',
+        tag: 'a10',
+        positiveCount: 35,
+        negativeCount: 39,
+        total: 107,
+      },
+      {
+        _id: '61d40d2b22b9d8c1a2e59dc4',
+        tag: 'a11',
+        positiveCount: 25,
+        negativeCount: 32,
+        total: 97,
+      },
+      {
+        _id: '61d40d2b55cd8da6c90f547c',
+        tag: 'a12',
+        positiveCount: 29,
+        negativeCount: 38,
+        total: 106,
+      },
+      {
+        _id: '61d40d2bfca7370102b6ba82',
+        tag: 'a13',
+        positiveCount: 39,
+        negativeCount: 34,
+        total: 85,
+      },
+      {
+        _id: '61d40d2bf35de7cf347d20bc',
+        tag: 'a14',
+        positiveCount: 27,
+        negativeCount: 39,
+        total: 80,
       },
     ];
 
