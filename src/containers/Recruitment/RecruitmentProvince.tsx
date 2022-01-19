78cf9365ad44df463e6b5730c1469dfbd6102c62
shahabi.kheyrati <m.shahabipour@vasl.ir>
2022-01-19 15:00:37 +0330

    fix test result by province

--- src/containers/Recruitment/RecruitmentProvince.tsx
+++ src/containers/Recruitment/RecruitmentProvince.tsx
@@ -5,7 +5,7 @@ import {sideCities} from 'src/helpers/utils';
 import OverviewMap from '../../components/Recruitment/OverviewMap';
 // import OverviewProvince from '../../components/Recruitment/OverviewProvince';
 // import OverviewPatientsProvince from '../../components/Recruitment/OverviewPatientsProvince';
-// import TestsStatusProvince from '../../components/Recruitment/TestStatusProvince';
+import TestsStatusProvince from '../../components/Recruitment/TestStatusProvince';
 import OverviewCategoriesProvince from '../../components/Recruitment/OverviewCategoriesProvince';
 // import OverviewOfVaccinationProvince from '../../components/Recruitment/OverviewOfVaccinationProvince';
 
@@ -19,12 +19,12 @@ const RecruitmentProvince = () => {
     // console.log(provinceName)
     const existsCity = sideCities.some((item: any) => {
       return item.name === provinceName;
-    })
+    });
 
     if (existsCity) {
       setCityTitle(provinceName);
     }
-  }, [location.search])
+  }, [location.search]);
 
   return (
     <div className="space-y-16 mb-8">
@@ -37,7 +37,7 @@ const RecruitmentProvince = () => {
       <OverviewCategoriesProvince cityTitle={cityTitle} />
       {/* <OverviewPatientsProvince cityTitle={cityTitle} /> */}
       {/* <OverviewOfVaccinationProvince cityTitle={cityTitle} /> */}
-      {/* <TestsStatusProvince cityTitle={cityTitle} /> */}
+      <TestsStatusProvince cityTitle={cityTitle} />
     </div>
   );
 };
