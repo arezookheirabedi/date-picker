3ab559c8bc187b443282162e2edd750780b476ae
emad abbasii <emadabbasii137@gmail.com>
2022-01-31 08:49:15 -0500

    uncomment some component

--- src/containers/School/School.tsx
+++ src/containers/School/School.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
 
-// import OverviewSchool from '../../components/School/Overview';
-// import OverviewSchoolEmploye from '../../components/School/OverviewSchoolEmploye';
+import OverviewSchool from '../../components/School/Overview';
+import OverviewSchoolEmploye from '../../components/School/OverviewSchoolEmploye';
 import OverviewSchoolStudent from '../../components/School/OverviewSchoolStudent';
-// import OverviewCategories from '../../components/School/OverviewCategories';
-// import OverviewPatients from '../../components/School/OverviewPatients';
-// import OverviewOfVaccination from '../../components/School/OverviewOfVaccination';
+import OverviewCategories from '../../components/School/OverviewCategories';
+import OverviewPatients from '../../components/School/OverviewPatients';
+import OverviewOfVaccination from '../../components/School/OverviewOfVaccination';
 import TestStatus from '../../components/School/TestStatus';
 
 // import IconWrapper from "../../components/IconWrapper";
 const School: React.FC<{}> = () => {
   return (
     <div className="space-y-16 mb-8">
-      {/* <OverviewSchool /> */}
-      {/* <OverviewSchoolEmploye /> */}
+      <OverviewSchool />
+      <OverviewSchoolEmploye />
       <OverviewSchoolStudent />
-      {/* <OverviewCategories /> */}
-      {/* <OverviewPatients /> */}
-      {/* <OverviewOfVaccination /> */}
+      <OverviewCategories />
+      <OverviewPatients />
+      <OverviewOfVaccination />
       <TestStatus />
     </div>
   );
