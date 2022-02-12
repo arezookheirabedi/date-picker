b62f770087210138a6a95b063e20501decdc1502
DESKTOP-P5OO4UH\VASL <muh.shahabipour@gmail.com>
2022-02-12 15:04:52 +0330

    connect api to show vaccination report

--- src/components/Vaccination/public/OverviewVaccinationStatus.tsx
+++ src/components/Vaccination/public/OverviewVaccinationStatus.tsx
@@ -1,116 +1,158 @@
-import React from "react";
+import React, {useEffect, useState} from 'react';
+import vaccineService from 'src/services/vaccine.service';
 
-import Statistic from "../../../containers/Guild/components/Statistic";
-import GreenVaccine from "../../../assets/images/icons/green-vaccine.svg";
-import YellowVaccine from "../../../assets/images/icons/yellow-vaccine.svg";
-import PurppleVaccine from "../../../assets/images/icons/purpple-vaccine.svg";
-import NavyVaccine from "../../../assets/images/icons/navy-vaccine.svg";
-import personGrayVaccine from "../../../assets/images/icons/personGrayVaccine.svg"
-import greenVaccineBlackVirus from "../../../assets/images/icons/green-vaccine-black-virus.svg";
-import blueVaccine from "../../../assets/images/icons/blue-vaccine-sm.svg";
-import greyVaccine from "../../../assets/images/icons/gray-vaccine.svg";
+import Statistic from '../../../containers/Guild/components/Statistic';
+import GreenVaccine from '../../../assets/images/icons/green-vaccine.svg';
+import YellowVaccine from '../../../assets/images/icons/yellow-vaccine.svg';
+import PurppleVaccine from '../../../assets/images/icons/purpple-vaccine.svg';
+import NavyVaccine from '../../../assets/images/icons/navy-vaccine.svg';
+import personGrayVaccine from '../../../assets/images/icons/personGrayVaccine.svg';
+import greenVaccineBlackVirus from '../../../assets/images/icons/green-vaccine-black-virus.svg';
+import blueVaccine from '../../../assets/images/icons/blue-vaccine-sm.svg';
+import greyVaccine from '../../../assets/images/icons/gray-vaccine.svg';
 
-const OverviewVaccinationStatus = () => {
+const initialDoses = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, null: 0};
+
+const OverviewVaccinationStatus: React.FC<{}> = () => {
+  const [loading, setLoading] = useState(false);
+  const [numberOf, setNumberOf] = useState<any>({
+    doses: {...initialDoses},
+    dosesPercentage: {...initialDoses},
+    dosesToTotalPopulationPercentage: {...initialDoses},
+    gtDoses: {...initialDoses},
+    gtDosesPercentage: {...initialDoses},
+    gtDosesToTotalPopulationPercentage: {...initialDoses},
+    totalPopulation: 0,
+    totalVaccinesCount: 0,
+    totalVaccinesCountToTotalPopulationPercentage: 0,
+    totalVaccinesPercentage: 0,
+  });
+
+  const getNumberOf = async () => {
+    setLoading(true);
+    try {
+      const {data} = await vaccineService.membersGeneral({});
+
+      setNumberOf({...data});
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    getNumberOf();
+  }, []);
+
   return (
     <fieldset className="text-center border rounded-xl p-4 mb-16">
-      <legend className="text-black mx-auto px-3">
-        نگاه کلی به وضعیت واکسیناسیون کل کشور
-      </legend>
+      <legend className="text-black mx-auto px-3">نگاه کلی به وضعیت واکسیناسیون کل کشور</legend>
 
       <div className="flex flex-col justify-between space-y-8">
-        <div
-          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+        <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
           <Statistic
             icon={GreenVaccine}
             text="تعداد کل واکسیناسیون"
-            count={0}
-            loading={false}
+            count={numberOf.totalVaccinesCount || 0}
+            loading={loading}
           />
           <Statistic
             icon={YellowVaccine}
             text="تعداد واکسیناسیون دوز اول"
-            count={0}
-            loading={false}
+            count={numberOf.doses[1] || 0}
+            loading={loading}
           />
           <Statistic
             icon={PurppleVaccine}
             text="تعداد واکسیناسیون دوز دوم"
-            count={0}
-            loading={false}
+            count={numberOf.doses[2] || 0}
+            loading={loading}
           />
-          <Statistic icon={NavyVaccine} text="تعداد واکسیناسیون دوز سوم" count="-" loading={false}/>
+          <Statistic
+            icon={NavyVaccine}
+            text="تعداد واکسیناسیون دوز سوم"
+            count={numberOf.doses[3] || 0}
+            loading={loading}
+          />
         </div>
-        <div
-          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+        <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
           <Statistic
             icon={GreenVaccine}
             text="درصد واکسیناسیون کل کشور"
-            count={342}
-            loading={false}
+            count={(numberOf.totalVaccinesCountToTotalPopulationPercentage || 0).toFixed(3)}
+            loading={loading}
+            isPercentage
           />
           <Statistic
             icon={YellowVaccine}
             text="درصد افراد با دوز یک"
-            count={342}
-            loading={false}
+            count={(numberOf.dosesPercentage[1] || 0).toFixed(3)}
+            loading={loading}
+            isPercentage
           />
           <Statistic
             icon={PurppleVaccine}
             text="درصد افراد با دوز دوم"
-            count={342}
-            loading={false}
+            count={(numberOf.dosesPercentage[2] || 0).toFixed(3)}
+            loading={loading}
+            isPercentage
           />
-          <Statistic icon={NavyVaccine} text="درصد افراد با دوز سوم" count="-"/>
+          <Statistic
+            icon={NavyVaccine}
+            text="درصد افراد با دوز سوم"
+            count={(numberOf.dosesPercentage[3] || 0).toFixed(3)}
+            loading={loading}
+            isPercentage
+          />
         </div>
 
-        <div
-          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+        <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
           <Statistic
             icon={personGrayVaccine}
             text="مجموع افراد واکسینه نشده"
-            count={342}
-            loading={false}
+            count={numberOf.doses[0] || 0}
+            loading={loading}
           />
           <Statistic
             icon={personGrayVaccine}
             text="درصد افراد واکسینه نشده"
-            count={342}
-            loading={false}
+            count={(numberOf.dosesPercentage[0] || 0).toFixed(3)}
+            loading={loading}
+            isPercentage
           />
           <Statistic
             icon={blueVaccine}
             text="تعداد واکسیناسیون بیش از ۳ دوز"
-            count={342}
-            loading={false}
+            count={numberOf.gtDoses[3] || 0}
+            loading={loading}
           />
-          <Statistic icon={blueVaccine} text="درصد افراد با بیش از ۳ دوز" count="-"/>
+          <Statistic
+            icon={blueVaccine}
+            text="درصد افراد با بیش از ۳ دوز"
+            count={(numberOf.gtDosesPercentage[3] || 0).toFixed(3)}
+            loading={loading}
+            isPercentage
+          />
         </div>
 
-        <div
-          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+        <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
           <Statistic
             icon={greenVaccineBlackVirus}
             text="تعداد فوتی هایی که واکسینه شده"
-            count={342}
-            loading={false}
+            count="-"
           />
-          <Statistic
-            icon={greenVaccineBlackVirus}
-            text="درصد فوتی های واکسینه شده"
-            count={342}
-            loading={false}
-          />
+          <Statistic icon={greenVaccineBlackVirus} text="درصد فوتی های واکسینه شده" count="-" />
+          <Statistic icon={greyVaccine} text="مجموع تعداد دوز واکسن تزریقی" count="-" />
           <Statistic
             icon={greyVaccine}
-            text="مجموع تعداد دوز واکسن تزریقی"
-            count={342}
-            loading={false}
+            text="تعداد اطلاعات مخدوش"
+            count={numberOf.totalPopulation - (numberOf.gtDoses[0] + numberOf.doses[0]) || 0}
           />
-          <Statistic icon={greyVaccine} text="تعداد اطلاعات مخدوش" count="-"/>
         </div>
       </div>
     </fieldset>
   );
-}
+};
 
 export default OverviewVaccinationStatus;
--- src/components/Vaccination/public/OverviewVaccinationStatusChart.tsx
+++ src/components/Vaccination/public/OverviewVaccinationStatusChart.tsx
@@ -1,16 +1,16 @@
-import React, {useEffect, useState} from "react";
+import React, {useEffect, useState} from 'react';
 // @ts-ignore
 import moment from 'moment-jalaali';
-import hcsService from "../../../services/hcs.service";
-import DatePickerModal from "../../DatePickerModal";
-import {toPersianDigit} from "../../../helpers/utils";
-import calendar from "../../../assets/images/icons/calendar.svg";
-import Spinner from "../../Spinner";
-import Charts from "../../Charts";
+import vaccineService from 'src/services/vaccine.service';
+import DatePickerModal from '../../DatePickerModal';
+import {toPersianDigit} from '../../../helpers/utils';
+import calendar from '../../../assets/images/icons/calendar.svg';
+import Spinner from '../../Spinner';
+import Charts from '../../Charts';
 
 const {Stacked} = Charts;
 
-const OverviewVaccinationStatusChart = () => {
+const OverviewVaccinationStatusChart: React.FC<{}> = () => {
   const [dataset, setDataset] = useState<any[]>([]);
   const [categories, setCategories] = useState<any[]>([]);
   const [showDatePicker, setShowDatePicker] = useState(false);
@@ -30,11 +30,11 @@ const OverviewVaccinationStatusChart = () => {
     // eslint-disable-next-line
     return selectedDayRange.from
       ? // eslint-disable-next-line
-      selectedDayRange.from.year +
-      '/' +
-      selectedDayRange.from.month +
-      '/' +
-      selectedDayRange.from.day
+        selectedDayRange.from.year +
+          '/' +
+          selectedDayRange.from.month +
+          '/' +
+          selectedDayRange.from.day
       : '';
   };
 
@@ -42,15 +42,14 @@ const OverviewVaccinationStatusChart = () => {
     // eslint-disable-next-line
     return selectedDayRange.to
       ? // eslint-disable-next-line
-      selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day
+        selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day
       : '';
   };
 
   const [queryParams, setQueryParams] = useState({
     from: '',
     to: '',
-    tags: ['province'].join(','),
-    organization: 'employment',
+    tags: [],
   });
 
   // eslint-disable-next-line
@@ -58,7 +57,7 @@ const OverviewVaccinationStatusChart = () => {
     setLoading(true);
     setErrorMessage(null);
     try {
-      const {data} = await hcsService.dosesTagBased(params);
+      const {data} = await vaccineService.dosesTagBased({});
 
       const provinces: any[] = [];
 
@@ -77,7 +76,7 @@ const OverviewVaccinationStatusChart = () => {
         let more = 0;
 
         // eslint-disable-next-line
-        for (const [key, value] of Object.entries(item.dosesCountMap)) {
+        for (const [key, value] of Object.entries(item.doses)) {
           if (Number(key) === 0) {
             noDose.push(Number(value));
           }
@@ -105,7 +104,7 @@ const OverviewVaccinationStatusChart = () => {
         if (thirdDose.length < index + 1) thirdDose.push(0);
         if (moreThanThreeDose.length < index + 1) moreThanThreeDose.push(more);
 
-        provinces.push(item.tag);
+        provinces.push(item.province);
       });
 
       setDataset([
@@ -163,17 +162,14 @@ const OverviewVaccinationStatusChart = () => {
         ...queryParams,
         from: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
         to: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        organization: 'employment',
-        tags: ['province'].join(','),
+        tags: [],
       });
     }
   }, [selectedDayRange]);
 
   return (
     <fieldset className="text-center border rounded-xl p-4 mb-16">
-      <legend className="text-black mx-auto px-3">
-        نگاه کلی به وضعیت واکسیناسیون کل کشور
-      </legend>
+      <legend className="text-black mx-auto px-3">نگاه کلی به وضعیت واکسیناسیون کل کشور</legend>
       <div className="flex flex-col align-center justify-center w-full rounded-lg bg-white p-4 shadow">
         <div className="flex items-center justify-between mb-10 mt-6 px-8">
           <div className="flex align-center justify-between w-3/4">
@@ -266,6 +262,6 @@ const OverviewVaccinationStatusChart = () => {
       </div>
     </fieldset>
   );
-}
+};
 
 export default OverviewVaccinationStatusChart;
