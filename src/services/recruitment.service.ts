e2b526adc63f2c6afc54dd12ef83861617a27745
Muhammad Sh <muh.shahabipour@gmail.com>
2022-02-28 17:09:27 +0330

    changed test result apis

--- src/services/recruitment.service.ts
+++ src/services/recruitment.service.ts
@@ -1,42 +1,51 @@
-
 // import qs from 'qs';
 import request from '../helpers/request';
 
-function membersGeneral({ ...params }: any = {}, config?: any) {
-    return request
-        .withHeaders({ 'Content-Type': 'application/json;utf-8' })
-        .build()
-        .get(`/api/v1/hcs-reporter/test-results/general`, params, { ...config });
+function membersGeneral({...params}: any = {}, config?: any) {
+  return request
+    .withHeaders({'Content-Type': 'application/json;utf-8'})
+    .build()
+    .get(`/api/v1/hcs-reporter/test-results/general`, params, {...config});
 }
 
-function dosesTagBased({ tag, category, ...params }: any = {}, config?: any) {
-    return request
-        .withHeaders({ 'Content-Type': 'application/json;utf-8' })
-        .build()
-        .get(`/api/v1/hcs-reporter/vaccines/tags/${tag}/categories/${category}`, params, { ...config });
+function dosesTagBased({tag, category, ...params}: any = {}, config?: any) {
+  return request
+    .withHeaders({'Content-Type': 'application/json;utf-8'})
+    .build()
+    .get(`/api/v1/hcs-reporter/vaccines/tags/${tag}/categories/${category}`, params, {...config});
 }
 
-function dosesProvinceBased({ ...params }: any = {}, config?: any) {
-    return request
-        .withHeaders({ 'Content-Type': 'application/json;utf-8' })
-        .build()
-        .get(`/api/v1/hcs-reporter/vaccines/provinces`, params, { ...config });
+function dosesProvinceBased({...params}: any = {}, config?: any) {
+  return request
+    .withHeaders({'Content-Type': 'application/json;utf-8'})
+    .build()
+    .get(`/api/v1/hcs-reporter/vaccines/provinces`, params, {...config});
 }
 
-
-function membersTagBased({ tag, category, ...params }: any = {}, config?: any) {
-    return request
-        .withHeaders({ 'Content-Type': 'application/json;utf-8' })
-        .build()
-        .get(`/api/v1/hcs-reporter/test-results/overview/tags/${tag}/categories/${category}`, params, { ...config });
+function membersTagBased({tag, category, ...params}: any = {}, config?: any) {
+  return request
+    .withHeaders({'Content-Type': 'application/json;utf-8'})
+    .build()
+    .get(`/api/v1/hcs-reporter/test-results/overview/tags/${tag}/categories/${category}`, params, {
+      ...config,
+    });
 }
 
+function testResultTagBased({tag, category, ...params}: any = {}, config?: any) {
+  return request
+    .withHeaders({'Content-Type': 'application/json;utf-8'})
+    .build()
+    .get(`/api/v1/hcs-reporter/test-results/tags/${tag}/categories/${category}`, params, {
+      ...config,
+    });
+}
 
 const recruitmentService = {
-    membersGeneral,
-    dosesTagBased,
-    dosesProvinceBased,
-    membersTagBased,
+  membersGeneral,
+  dosesTagBased,
+  dosesProvinceBased,
+  membersTagBased,
+  testResultTagBased,
 };
 
 export default recruitmentService;
