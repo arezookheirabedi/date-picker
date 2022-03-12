0af551a99190315284e0069bcada3aad00b0bc1f
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-03-12 11:48:00 +0330

    passenger services

--- src/containers/Passenger/Passenger.tsx
+++ src/containers/Passenger/Passenger.tsx
@@ -2,13 +2,14 @@ import OverviewPasengersStatusCard from 'src/components/Pasengers/public/Overvie
 
 import OverviewPasengersVaccinateComponent from 'src/components/Pasengers/public/OverViewPassengerVaccinate';
 import OverviewPassengerPatients from 'src/components/Pasengers/public/OverviewPatients';
-
+import OverViewPassengerTypeStatusCardComponent from 'src/components/Pasengers/public/OverViewPassengerTypeStatusCard'
 import OverViowPassengerStatusVaccinateChart from '../../components/Pasengers/public/OverViewPassengerStatusVaccinateChart';
 
 const Pasenger = () => {
   return (
     <div className="space-y-16 mb-8">
       <OverviewPasengersStatusCard />
+      <OverViewPassengerTypeStatusCardComponent/>
       <OverviewPasengersVaccinateComponent />
       <OverViowPassengerStatusVaccinateChart />
       <OverviewPassengerPatients />
