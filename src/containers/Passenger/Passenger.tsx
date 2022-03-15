9b96224bc0672d7334b0f1e1b355003e4e0d2248
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-03-15 17:48:29 +0330

    fix

--- src/containers/Passenger/Passenger.tsx
+++ src/containers/Passenger/Passenger.tsx
@@ -1,18 +1,24 @@
 import OverviewPasengersStatusCard from 'src/components/Pasengers/public/OverviewPasengersStatusCard';
+import OverviewAirlinesPasengersStatusCard from 'src/components/Pasengers/public/OverviewAirlinesPasengersStatusCard';
+import OverviewBusPasengersStatusCard from 'src/components/Pasengers/public/OverviewBusPasengersStatusCard';
+import OverviewShipPasengersStatusCard from 'src/components/Pasengers/public/OverviewShipPasengersStatusCard';
+import OverviewTrainPasengersStatusCard from 'src/components/Pasengers/public/OverviewTrainPasengersStatusCard';
 
 import OverviewPasengersVaccinateComponent from 'src/components/Pasengers/public/OverViewPassengerVaccinate';
 import OverviewPassengerPatients from 'src/components/Pasengers/public/OverviewPatients';
-import OverViewPassengerTypeStatusCardComponent from 'src/components/Pasengers/public/OverViewPassengerTypeStatusCard'
 import OverViowPassengerStatusVaccinateChart from '../../components/Pasengers/public/OverViewPassengerStatusVaccinateChart';
 
 const Pasenger = () => {
   return (
     <div className="space-y-16 mb-8">
       <OverviewPasengersStatusCard />
-      <OverViewPassengerTypeStatusCardComponent/>
-      <OverviewPasengersVaccinateComponent />
-      <OverViowPassengerStatusVaccinateChart />
-      <OverviewPassengerPatients />
+      <OverviewAirlinesPasengersStatusCard/>
+       <OverviewBusPasengersStatusCard/>
+     <OverviewTrainPasengersStatusCard/>
+      <OverviewShipPasengersStatusCard/> 
+       <OverviewPasengersVaccinateComponent /> 
+       <OverViowPassengerStatusVaccinateChart /> 
+       <OverviewPassengerPatients /> 
     </div>
   );
 };
