22739e6f196d7b677e1944c1386eaf13f4a9c96e
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-05-07 16:25:52 +0430

    guild report service

--- src/containers/Reports/GuildReport.tsx
+++ src/containers/Reports/GuildReport.tsx
@@ -1,64 +1,74 @@
 import React, {useEffect, useState} from 'react';
-import {useHistory, useLocation} from 'react-router-dom';
-import dayjs from 'dayjs';
-import axios from 'axios';
+import {useLocation} from 'react-router-dom';
 import transportService from 'src/services/transport.service';
+// @ts-ignore
+import moment from 'moment-jalaali';
+import guildService from 'src/services/guild.service';
+import Table from 'src/components/TableXHR';
 import DatePickerModal from '../../components/DatePickerModal';
-import calendar from '../../assets/images/icons/calendar.svg';
+import Calendar from '../../components/Calendar';
 import download from '../../assets/images/icons/download.svg';
-import Table from '../../components/Table';
-import {toPersianDigit} from '../../helpers/utils';
+import {
+  cancelTokenSource,
+  convertGregorianDateToJalaliDate,
+  msgRequestCanceled,
+  toPersianDigit,
+} from '../../helpers/utils';
 import Spinner from '../../components/Spinner';
+import {EReportStatus} from './constant';
 
-const GuildReport: React.FC<{}> = () => {
+const TransportReport: React.FC<{}> = () => {
   const location = useLocation();
-  const history = useHistory();
+  // const history = useHistory();
   const [loading, setLoading] = useState(false);
-  const [dataset, setDataset] = useState<any>([]);
+  const [dataSet, setDataSet] = useState<any[]>([]);
+  const [currentPage, setCurrenntPage] = useState(1);
+
   // eslint-disable-next-line
-  // const [totalItems, setTotalItems] = useState(0);
+  const [totalItems, setTotalItems] = useState(0);
   const [showDatePicker, setShowDatePicker] = useState(false);
   // eslint-disable-next-line
   const [selectedDayRange, setSelectedDayRange] = useState({
     from: null,
     to: null,
   }) as any;
+  const [query, setQuery] = useState({
+    fromReportDate: null,
+    toReportDate: null,
+  });
+  const cancelToken = cancelTokenSource();
 
-  const {CancelToken} = axios;
-  const source = CancelToken.source();
-
-  const focusFromDate = () => {
-    setShowDatePicker(true);
-  };
-
-  const generateFromDate: any = () => {
-    // eslint-disable-next-line
-    return selectedDayRange.from
-      ? // eslint-disable-next-line
-        selectedDayRange.from.year +
-          '/' +
-          selectedDayRange.from.month +
-          '/' +
-          selectedDayRange.from.day
-      : '';
-  };
-
-  const generateToDate: any = () => {
-    // eslint-disable-next-line
-    return selectedDayRange.to
-      ? // eslint-disable-next-line
-        selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day
-      : '';
-  };
+  function cancelRequest() {
+    cancelToken.cancel(msgRequestCanceled);
+  }
+  const pageSize = 1;
 
   async function fetchReports(params: any) {
     setLoading(true);
     try {
-      const response = await transportService.fetchRequestedReports(params, {
-        cancelToken: source.token,
+      const {data} = await guildService.guildReportoverviewStatus(params, {
+        cancelToken: cancelToken.token,
       });
-      setDataset([...response.data.content]);
-      // setTotalItems(response.data.totalElements);
+      const normalizedData: any[] = [];
+      data.content.forEach((item: any, index: number) => {
+        normalizedData.push({
+          // id: `ovca_${index}`,
+          // reportName: item.reportName || 'نامشخص',
+          // requestDateTime: item.requestDateTime || 'نامشخص',
+          // lastModifiedDate: item.lastModifiedDate,
+          // trackingCode: item.trackingCode || 'نامشخص',
+          // reportStatus: item.reportStatus || 'نامشخص',
+          id: `ovca_${index}`,
+          reportName: 'sdfsedfdsgfdfg',
+          requestDateTime: new Date().getTime(),
+          lastModifiedDate: new Date().getTime(),
+          trackingCode: '999999999',
+          reportStatus: EReportStatus.DOWNLOADED,
+        });
+      });
+
+      setDataSet([...normalizedData]);
+      setTotalItems(data.totalElements);
     } catch (error: any) {
       // eslint-disable-next-line
       console.log(error);
@@ -68,25 +78,56 @@ const GuildReport: React.FC<{}> = () => {
   }
 
   useEffect(() => {
-    return () => {
-      source.cancel('Operation canceled by the user.');
-      setDataset([]);
-      // setTotalItems(0);
-      setLoading(false);
-    };
-  }, [history]);
+    if (selectedDayRange.from && selectedDayRange.to) {
+      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
+      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
+      setQuery({
+        ...query,
+        fromReportDate: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
+        toReportDate: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
+      });
+    }
+    if (selectedDayRange.clear) {
+      setQuery({
+        ...query,
+        fromReportDate: null,
+        toReportDate: null,
+      });
+    }
+  }, [selectedDayRange]);
 
   useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    fetchReports({pageNumber: Number(params.get('page') || 1) - 1, sort: 'DESC', pageSize: 20});
-  }, [location.search]);
+    const normalizedData = [
+      {
+        id: `ovca_${2}`,
+        reportName: 'sdfsedfdsgfdfg',
+        requestDateTime: new Date().getTime(),
+        lastModifiedDate: new Date().getTime(),
+        trackingCode: '999999999',
+        reportStatus: EReportStatus.PROCESSING,
+      },
+    ];
+
+    setDataSet([...normalizedData]);
+
+    fetchReports(query);
+
+    return () => {
+      setDataSet([]);
+      cancelRequest();
+    };
+  }, [query]);
+
+  const focusFromDate = () => {
+    setShowDatePicker(true);
+  };
 
   async function handlePreDownload(id: string) {
     try {
       const params = new URLSearchParams(location.search);
 
       const response = await transportService.preDownloadReport(id, {
-        cancelToken: source.token,
+        cancelToken: cancelToken.token,
       });
 
       const newWindow = window.open(
@@ -107,7 +148,7 @@ const GuildReport: React.FC<{}> = () => {
       const params = new URLSearchParams(location.search);
 
       await transportService.retryReport(id, {
-        cancelToken: source.token,
+        cancelToken: cancelToken.token,
       });
 
       fetchReports({pageNumber: Number(params.get('page') || 1) - 1, sort: 'DESC', pageSize: 20});
@@ -116,115 +157,33 @@ const GuildReport: React.FC<{}> = () => {
       console.log(error);
     }
   }
-
-  const clearSelectedDayRange = (e: any) => {
-    e.stopPropagation();
-    setSelectedDayRange({
-      from: null,
-      to: null,
-    });
-  };
-
+  function handlePageChange(page: number = 1) {
+    setCurrenntPage(page);
+  }
   return (
     <>
       <fieldset className="text-center border rounded-xl p-1 mb-16">
         <legend className="text-black mx-auto px-3">لیست گزارش اصناف</legend>
 
         <div className="flex align-center justify-between mb-8">
-          <div className="flex align-center justify-start">
-            {showDatePicker ? (
-              <DatePickerModal
-                setSelectedDayRange={setSelectedDayRange}
-                selectedDayRange={selectedDayRange}
-                setShowDatePicker={setShowDatePicker}
-                showDatePicker
-              />
-            ) : null}
-            <div className="relative z-20 inline-block text-left shadow-custom rounded-lg px-4 py-1">
-              <div
-                className="inline-flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
-                onClick={focusFromDate}
-              >
-                <span className="ml-4 whitespace-nowrap truncate text-xs">
-                  {toPersianDigit(generateFromDate())}
-                </span>
-                {selectedDayRange.to || selectedDayRange.from ? (
-                  <button type="button" onClick={clearSelectedDayRange}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 text-gray-400"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={1}
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                      />
-                    </svg>
-                  </button>
-                ) : (
-                  <img src={calendar} alt="x" className="w-5 h-5" />
-                )}
-              </div>
-            </div>
-            <div className="flex items-center justify-start mx-4">
-              <span className="dash-separator" />
-            </div>
-            <div className=" shadow-custom rounded-lg px-4 py-1">
-              <div
-                className="flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
-                onClick={focusFromDate}
-              >
-                <span className="ml-4 whitespace-nowrap truncate text-xs">
-                  {toPersianDigit(generateToDate())}
-                </span>
-                {selectedDayRange.to || selectedDayRange.from ? (
-                  <button type="button" onClick={clearSelectedDayRange}>
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      className="h-5 w-5 text-gray-400"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      stroke="currentColor"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={1}
-                        d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                      />
-                    </svg>
-                  </button>
-                ) : (
-                  <img src={calendar} alt="x" className="w-5 h-5" />
-                )}
-              </div>
-            </div>
-          </div>
-          <div className="flex align-center">
-            <div className="relative inline-flex align-center leading-3">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="w-4 h-4 absolute top-1/2 transform -translate-y-1/2 right-4"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth={2}
-                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          <div className="flex items-center justify-between mb-10 mt-6">
+            <div className="flex align-center justify-between flex-grow px-8">
+              <div className="flex align-center justify-between">
+                {showDatePicker ? (
+                  <DatePickerModal
+                    setSelectedDayRange={setSelectedDayRange}
+                    selectedDayRange={selectedDayRange}
+                    setShowDatePicker={setShowDatePicker}
+                    showDatePicker
+                  />
+                ) : null}
+                <Calendar
+                  action={focusFromDate}
+                  from={selectedDayRange.from}
+                  to={selectedDayRange.to}
+                  setSelectedDayRange={setSelectedDayRange}
                 />
-              </svg>
-              <input
-                type="text"
-                placeholder="جستجو"
-                className="py-2 px-4 pr-10 text-sm border border-gray-300 rounded-lg focus:outline-none"
-              />
+              </div>
             </div>
           </div>
         </div>
@@ -235,59 +194,41 @@ const GuildReport: React.FC<{}> = () => {
             </div>
           ) : (
             <Table
-              dataSet={[...dataset]}
-              pagination={{pageSize: 20, maxPages: 3}}
+              handlePageChange={handlePageChange}
+              dataSet={[...dataSet]}
+              pagination={{pageSize, currentPage}}
+              totalItems={totalItems}
               columns={[
+                {
+                  name: 'ردیف',
+                  key: '',
+                  render: (v: any, record, index: number) => (
+                    <div className="flex w-full justify-center">
+                      {toPersianDigit(((currentPage - 1) * pageSize + (index + 1)).toString())}.
+                    </div>
+                  ),
+                },
                 {
                   name: 'نام گزارش',
                   key: 'reportName',
-                  render: (v, record, index: number, page: number) => (
-                    <div className="flex">
-                      {v || `گزارش شماره ${((page - 1) * 20 + (index + 1)).toLocaleString('fa')}`}
-                    </div>
-                  ),
+                  render: (v: any, record: any) => <span> {record.reportName}</span>,
                 },
-                {
-                  name: 'دسته‌بندی',
-                  key: 'category',
-                  render: v => <span>{v || '-'}</span>,
-                },
-                {
-                  name: 'تاریخ گزارش',
-                  key: '',
-                  render: (v, record: any) => (
-                    <div className="flex flex-col text-gray-500">
-                      <span>
-                        {record.from
-                          ? toPersianDigit(
-                              dayjs(record.from).calendar('jalali').format('YYYY/MM/DD')
-                            )
-                          : ''}
-                      </span>
-                      <span>-</span>
-                      <span>
-                        {record.to
-                          ? toPersianDigit(dayjs(record.to).calendar('jalali').format('YYYY/MM/DD'))
-                          : ''}
-                      </span>
-                    </div>
-                  ),
-                },
+
                 {
                   name: 'شماره شناسه',
                   key: 'trackingCode',
-                  render: v => <span>{v || '-'}</span>,
+                  render: (v: any, record: any) => (
+                    <span className="text-gray-500">{toPersianDigit(record.trackingCode)}</span>
+                  ),
                 },
                 {
                   name: 'تاریخ درخواست',
-                  key: 'createdDate',
-                  render: v => (
+                  key: 'requestDateTime',
+                  render: (v: any, record: any) => (
                     <div className="flex justify-center">
                       <div style={{width: '64px'}}>
-                        <span className="text-gray-400 whitespace-normal">
-                          {v
-                            ? toPersianDigit(dayjs(v).calendar('jalali').format('YYYY/MM/DD HH:mm'))
-                            : ''}
+                        <span className="text-gray-500 whitespace-normal">
+                          {convertGregorianDateToJalaliDate(record.requestDateTime)}
                         </span>
                       </div>
                     </div>
@@ -296,13 +237,11 @@ const GuildReport: React.FC<{}> = () => {
                 {
                   name: 'آخرین به‌روزرسانی',
                   key: 'lastModifiedDate',
-                  render: v => (
+                  render: (v: any, record: any) => (
                     <div className="flex justify-center">
                       <div style={{width: '64px'}}>
                         <span className="text-gray-500 whitespace-normal">
-                          {v
-                            ? toPersianDigit(dayjs(v).calendar('jalali').format('YYYY/MM/DD HH:mm'))
-                            : ''}
+                          {convertGregorianDateToJalaliDate(record.lastModifiedDate)}
                         </span>
                       </div>
                     </div>
@@ -313,10 +252,10 @@ const GuildReport: React.FC<{}> = () => {
                   key: 'reportStatus',
                   render: (v: string, record: any) =>
                     (() => {
-                      switch (v) {
-                        case 'PROCESSING':
+                      switch (record.reportStatus) {
+                        case EReportStatus.PROCESSING:
                           return <span className="text-yellow-500">در حال پردازش</span>;
-                        case 'READY_FOR_DOWNLOAD':
+                        case EReportStatus.READY_FOR_DOWNLOAD:
                           return (
                             <div className="inline-flex">
                               <button
@@ -329,8 +268,10 @@ const GuildReport: React.FC<{}> = () => {
                               </button>
                             </div>
                           );
-                        case 'DOWNLOADED':
+                        case EReportStatus.DOWNLOADED:
                           return <span className="text-gray-400">دانلود شده</span>;
+                        case EReportStatus.READY_FOR_SMS:
+                          return <span className="text-gray-400"> آماده جهت ارسال پیامک</span>;
                         default:
                           return (
                             <div className="flex justify-center items-center">
@@ -361,7 +302,6 @@ const GuildReport: React.FC<{}> = () => {
                     })(),
                 },
               ]}
-              totalItems={(dataset || []).length}
             />
           )}
         </div>
@@ -370,4 +310,4 @@ const GuildReport: React.FC<{}> = () => {
   );
 };
 
-export default GuildReport;
+export default TransportReport;
