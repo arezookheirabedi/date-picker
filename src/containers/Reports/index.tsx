558cd40bdc0632923c12aea5c7c57e0c6bf6d0ce
Muhammad Shahabipour <muh.shahabipour@gmail.com>
2022-05-13 09:57:48 +0430

    change style

--- src/containers/Reports/index.tsx
+++ src/containers/Reports/index.tsx
@@ -6,12 +6,12 @@ import TransportReport from './TransportReport';
 const Requested: React.FC<{}> = () => {
   return (
     <>
-      <fieldset className="text-center border rounded-xl p-4 mb-16">
-        <legend className="text-black mx-auto px-3">لیست درخواست دانلود گزارش</legend>
+      {/* <fieldset className="text-center border rounded-xl p-4 mb-16">
+        <legend className="text-black mx-auto px-3">لیست درخواست دانلود گزارش</legend> */}
 
         <TransportReport />
         <GuildReport />
-      </fieldset>
+      {/* </fieldset> */}
     </>
   );
 };
