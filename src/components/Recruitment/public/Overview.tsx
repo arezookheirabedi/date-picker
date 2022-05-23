812667fffa0e3861da850c98aa85ebf1c5707fa3
emadabbasii <emadabbasee@gmail.com>
2022-05-23 17:01:24 +0430

    make some changes in apis in overviewcategories in recruitment

--- src/components/Recruitment/public/Overview.tsx
+++ src/components/Recruitment/public/Overview.tsx
@@ -4,22 +4,39 @@ import {addTotalMembersAc} from 'src/store/action_creators';
 import vaccineServices from 'src/services/vaccine.service';
 import recruitmentServices from 'src/services/recruitment.service';
 import Statistic from '../../../containers/Guild/components/Statistic';
-import totalRecritment from '../../../assets/images/icons/people-navy.svg';
+import totalRecruitment from '../../../assets/images/icons/people-navy.svg';
 import sufferingIcon from '../../../assets/images/icons/suffering-color.svg';
 import saveIcon from '../../../assets/images/icons/save-color.svg';
 import deadIcon from '../../../assets/images/icons/dead-color.svg';
 import vaccineIcon from '../../../assets/images/icons/vaccine-color.svg';
 import grayVaccineIcon from '../../../assets/images/icons/gray-vaccine-lg.svg';
-import prescriptionIcon from '../../../assets/images/icons/prescription.svg';
 import testIcon from '../../../assets/images/icons/test-color.svg';
+import totalVaccinateStart from "../../../assets/images/icons/total-vaccinate-start-work-panel.svg";
+import noneVaccinateStart from "../../../assets/images/icons/none-vaccinate-start-wok-panel.svg";
+import passengerPositiveTest from "../../../assets/images/icons/passenger-positive-test.svg";
+
+const initialDoses = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, null: 0};
+const initialNumberOf = {
+  doses: {...initialDoses},
+  // dosesPercentage: {...initialDoses},
+  dosesToTotalPopulationPercentage: {...initialDoses},
+  gtDoses: {...initialDoses},
+  // gtDosesPercentage: {...initialDoses},
+  // gtDosesToTotalPopulationPercentage: {...initialDoses},
+  gtDosesToTotalDosesPercentage: {...initialDoses},
+  totalPopulation: 0,
+  totalUnknownVaccinesCount: 0,
+  totalPassengerCount: 0,
+  totalPassengerCountToTotalPopulationPercentage: 0,
+  totalVaccinesPercentage: 0,
+};
 
 const OverviewRecruitment: React.FC<{}> = () => {
   const [loading, setLoading] = useState(false);
-  const [numberOf, setNumberOf] = useState(null);
+  const [numberOf, setNumberOf] = useState<any>(initialNumberOf);
+  const [totalPopulation, setTotalPopulation] = useState(null);
   const [numberOfPositive, setNumberOfPositive] = useState(null);
   const [numberOfRecovered, setNumberOfRecovered] = useState(null);
-  const [numberOfVaccination, setNumberOfVaccination] = useState(null);
-  const [numberOfNanVaccinated, setNumberOfNanVaccinated] = useState(null);
   const [numberOfEmployeeTests, setNumberOfEmployeeTests] = useState(null);
 
   const dispatch = useDispatch();
@@ -33,7 +50,7 @@ const OverviewRecruitment: React.FC<{}> = () => {
       });
 
       dispatch(addTotalMembersAc(generalData.totalPopulation || 0));
-      setNumberOf(generalData.totalPopulation || 0);
+      setTotalPopulation(generalData.totalPopulation || 0);
       setNumberOfPositive(generalData.positiveMembersCount || 0);
       setNumberOfRecovered(generalData.recoveredMembersCount || 0);
       setNumberOfEmployeeTests(generalData.testResultsCount || 0);
@@ -42,8 +59,7 @@ const OverviewRecruitment: React.FC<{}> = () => {
         tag: 'employee',
         category: 'heName',
       });
-      setNumberOfVaccination(vaccineData.totalVaccinesCount || 0);
-      setNumberOfNanVaccinated(vaccineData.totalNonVaccinesCount || 0);
+      setNumberOf({...vaccineData});
     } catch (error) {
       // eslint-disable-next-line
       console.log(error);
@@ -61,11 +77,12 @@ const OverviewRecruitment: React.FC<{}> = () => {
       <legend className="text-black mx-auto px-3">نگاه کلی به وضعیت کارکنان دولت در کل کشور</legend>
 
       <div className="flex flex-col justify-between space-y-8">
-        <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+        <div
+          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
           <Statistic
-            icon={totalRecritment}
+            icon={totalRecruitment}
             text="مجموع کارکنان دولت"
-            count={numberOf}
+            count={totalPopulation}
             loading={loading}
           />
           <Statistic
@@ -78,34 +95,62 @@ const OverviewRecruitment: React.FC<{}> = () => {
             icon={saveIcon}
             text="مجموع بهبود یافتگان"
             count={numberOfRecovered}
-            // loading={loading}
+            loading={loading}
           />
-          <Statistic icon={deadIcon} text="مجموع فوت‌ شدگان" count="-" loading={false} />
+          <Statistic icon={deadIcon} text="مجموع فوت‌ شدگان" count="-" loading={false}/>
         </div>
-        <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+        <div
+          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
           <Statistic
             icon={vaccineIcon}
             text="مجموع افراد واکسینه شده"
-            count={numberOfVaccination}
+            count={numberOf.totalVaccinesCount}
             loading={loading}
           />
           <Statistic
             icon={grayVaccineIcon}
             text="مجموع افراد واکسینه نشده"
-            count={numberOfNanVaccinated}
+            count={numberOf.totalNonVaccinesCount}
             loading={loading}
           />
           <Statistic
+            icon={totalVaccinateStart}
+            text="تعداد مراجعات واکسیناسیون بعد از شروع سامانه"
+            count={numberOf.totalVaccinesCountAfterStartOfSystem}
+            loading={loading}
+          />
+          <Statistic
+            icon={noneVaccinateStart}
+            text="مجموع افراد واکسینه نشده در زمان شروع سامانه"
+            count={numberOf.totalNonVaccinesCountBeforeStartOfSystem}
+            loading={loading}
+          />
+        </div>
+        <div
+          className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+          <Statistic
             icon={testIcon}
-            text="تعداد آزمایش‌های کارمندان"
+            text="تعداد آزمایش های کارکنان دولت"
             count={numberOfEmployeeTests}
             loading={loading}
           />
           <Statistic
-            icon={prescriptionIcon}
-            text="مجموع استعلام از مراجعین دولتی"
-            count="-"
-            loading={false}
+            icon={grayVaccineIcon}
+            text="درصد افراد واکسینه نشده"
+            count={numberOf.totalNonVaccinesCountToTotalPopulationPercentage}
+            loading={loading}
+          />
+          <Statistic
+            icon={vaccineIcon}
+            text="درصد افراد واکسینه شده"
+            count={numberOf.totalVaccinesCountToTotalPopulationPercentage}
+            loading={loading}
+          />
+          <Statistic
+            icon={passengerPositiveTest}
+            text="درصد ابتلا به کل"
+            count={numberOf.positiveMembersCountToTotalPopulationPercentage}
+            loading={loading}
           />
         </div>
       </div>
