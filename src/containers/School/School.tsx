85c89a1dad982b37296fe5a55652becf94f128cc
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-05-28 11:09:17 +0430

    update ui

--- src/containers/School/School.tsx
+++ src/containers/School/School.tsx
@@ -1,62 +1,36 @@
 import React from 'react';
+import OverviewToVaccinationStatus from 'src/components/School/public/OverviewSchoolVaccinationStatus';
 import OverviewDepartmentEmploye from 'src/components/School/public/OverviewDepartmentEmploye';
 import OverviewEducationalEmploye from 'src/components/School/public/OverviewEducationalEmploye';
 import OverviewOfVaccinationProcess from 'src/components/School/public/OverviewOfVaccinationProcess';
-import TheLatestOverwiewOfVaccination from 'src/components/School/public/TheLatestOverwiewOfVaccination';
-import OverviewPositivePcr from 'src/components/School/public/OverviewPositivePcr';
+import OverviewSchoolsPositivePcr from 'src/components/School/public/OverviewSchoolsPositivePcr';
 import OverviewSteudent from 'src/components/School/public/OverviewSteudent';
-
-// import OverviewSchool from '../../components/School/public/Overview';
-// import OverviewSchoolEmploye from '../../components/School/public/OverviewSchoolEmploye';
-// import OverviewSchoolStudent from '../../components/School/public/OverviewSchoolStudent';
 import OverviewPositivePcrPercentage from 'src/components/School/public/OverviewPositivePcrPercentage';
-import OverviewVaccination from 'src/components/School/public/OverviewVaccination';
+import OverviewVaccination from 'src/components/School/public/OverviewSchoolVaccination';
+import TheLatestOverwiewOfVaccination from 'src/components/School/public/TheLatestSchoolOverwiewOfVaccination';
+import OverviewSchoolsVaccinationPercentagePerGrade from 'src/components/School/public/OverviewSchoolsVaccinationPercentagePerGrade';
+import TestStatus from '../../components/School/public/TestStatus';
 import OverviewCategories from '../../components/School/public/OverviewCategories';
-// import OverviewPatients from '../../components/School/public/OverviewPatients';
-// import OverviewOfVaccination from '../../components/School/public/OverviewOfVaccination';
-// import TestStatus from '../../components/School/public/TestStatus';
 
-// import IconWrapper from "../../components/IconWrapper";
 const School: React.FC<{}> = () => {
   return (
     <div className="space-y-16 mb-8">
-      {/* <OverviewSchoolEmploye />
-      <OverviewSchoolStudent />
-      <OverviewCategories />
-      <OverviewPatients />
-      <OverviewOfVaccination />
-      <TestStatus /> */}
+
 
       <OverviewEducationalEmploye />
       <OverviewDepartmentEmploye />
       <OverviewSteudent />
       <OverviewCategories />
-      <OverviewPositivePcr />
+      <OverviewSchoolsPositivePcr />
       <OverviewOfVaccinationProcess />
       <TheLatestOverwiewOfVaccination />
       <OverviewPositivePcrPercentage />
-      <OverviewVaccination />
+      <OverviewSchoolsVaccinationPercentagePerGrade /> 
+       <OverviewVaccination /> 
+       <OverviewToVaccinationStatus /> 
+      <TestStatus /> 
     </div>
   );
 };
 
 export default School;
-
-/* 
-/*       {
-        color: {
-          linearGradient: {
-            x1: 0,
-            x2: 0,
-            y1: 0,
-            y2: 1
-          },
-          stops: [
-            [0, '#5F5B97'],
-            [1, '#DDDCE9']
-          ]
-        },
-        name: 'وضعیت پاسخ',
-        type: 'column',
-        data: props.charData
-      },npm */
