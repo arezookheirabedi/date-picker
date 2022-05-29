a20d75857395b6d14dbf68375428d9b77f8867b4
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-05-29 10:59:00 +0430

    fix ui

--- src/components/Guild/public/TheLatestOverwiewOfVaccination/OverviewOfTheLatestPublicGuildVaccinationStatus.tsx
+++ src/components/Guild/public/TheLatestOverwiewOfVaccination/OverviewOfTheLatestPublicGuildVaccinationStatus.tsx
@@ -319,7 +319,7 @@ const OverviewOfTheLatestPublicGuildVaccinationStatus = () => {
   }, [dataset]);
 
   return (
-    <fieldset className="text-center border rounded-xl p-4 mb-16">
+    <fieldset className="text-center  p-4 mb-16">
       <div className="flex flex-col align-center justify-center w-full rounded-lg bg-white p-4 shadow">
         <div className="flex items-center justify-between mb-10 mt-6 px-8">
           <div className="flex align-center justify-between w-3/4">
