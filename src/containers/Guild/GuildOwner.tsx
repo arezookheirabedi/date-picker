e88f4291b47f91da2aed08513f588ce1dbd96839
DESKTOP-P5OO4UH\VASL <muh.shahabipour@gmail.com>
2022-05-29 15:27:18 +0430

    fix large items in select box

--- src/containers/Guild/GuildOwner.tsx
+++ src/containers/Guild/GuildOwner.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
-import OverviewOfVaccination from 'src/components/Guild/public/OverviewOfVaccination';
-// import OverviewGuildPositivePcrPercentage from 'src/components/Guild/public/OverviewGuildPositivePcrPercentage';
-// import OverviewGuildRegisterNumber from 'src/components/Guild/public/OverviewGuildRegisterNumber';
-// import OverviewGuildRegisterPercentage from 'src/components/Guild/public/OverviewGuildRegisterPercentage';
+// import OverviewOfVaccination from 'src/components/Guild/public/OverviewOfVaccination';
+import OverviewGuildPositivePcrPercentage from 'src/components/Guild/public/OverviewGuildPositivePcrPercentage';
+import OverviewGuildRegisterNumber from 'src/components/Guild/public/OverviewGuildRegisterNumber';
+import OverviewGuildRegisterPercentage from 'src/components/Guild/public/OverviewGuildRegisterPercentage';
 import OverviewGuilds from 'src/components/Guild/public/OverviewGuilds';
 import OverviewGuildsPerCategory from 'src/components/Guild/public/OverviewGuildsPerCategory';
 import OverviewGuildsPerProvince from 'src/components/Guild/public/OverviewGuildsPerProvince';
-// import OverviewVaccination from 'src/components/Guild/public/OverviewGuildVaccination';
-// import OverviewOfGuildVaccinationProcess from 'src/components/Guild/public/OverviewOfGuildVaccinationProcess';
+import OverviewVaccination from 'src/components/Guild/public/OverviewGuildVaccination';
+import OverviewOfGuildVaccinationProcess from 'src/components/Guild/public/OverviewOfGuildVaccinationProcess';
 import TestStatus from 'src/components/Guild/public/TestStatus';
-// import TheLatestOverwiewOfVaccination from 'src/components/Guild/public/TheLatestOverwiewOfVaccination';
+import TheLatestOverwiewOfVaccination from 'src/components/Guild/public/TheLatestOverwiewOfVaccination';
 
 const GuildOwner: React.FC<any> = () => {
   return (
     <div className="space-y-16 mb-8">
-      <OverviewGuilds />
+      {/* <OverviewGuilds />
       <OverviewGuildsPerCategory />
       <OverviewGuildsPerProvince />
       <OverviewOfVaccination />
-      <TestStatus />
-      {/* <OverviewGuilds />
+      <TestStatus /> */}
+      <OverviewGuilds />
       <OverviewGuildsPerCategory />
       <OverviewOfGuildVaccinationProcess />
       <TheLatestOverwiewOfVaccination />
@@ -28,7 +28,7 @@ const GuildOwner: React.FC<any> = () => {
       <OverviewGuildRegisterNumber />
       <OverviewVaccination />
       <OverviewGuildsPerProvince />
-      <TestStatus /> */}
+      <TestStatus />
     </div>
   );
 };
