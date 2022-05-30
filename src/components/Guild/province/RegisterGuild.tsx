1dc8eb6ef0680952dd237d149fa0778765896718
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-05-30 12:28:40 +0430

    register guild in province

---
+++ src/components/Guild/province/RegisterGuild.tsx
@@ -0,0 +1,225 @@
+import React, {useEffect, useState} from 'react';
+// @ts-ignore
+import moment from 'moment-jalaali';
+import guildService from 'src/services/guild.service';
+import {useHistory, useLocation} from 'react-router-dom';
+import {cancelTokenSource, msgRequestCanceled, sideCities} from 'src/helpers/utils';
+import DatePickerModal from '../../DatePickerModal';
+import Calendar from '../../Calendar';
+import Table from '../../TableScopeSort';
+
+interface IRegisterGuildProps {
+  cityTitle?: any;
+}
+const RegisterGuild: React.FC<IRegisterGuildProps> = ({cityTitle}) => {
+  const location = useLocation();
+  const history = useHistory();
+  const [showDatePicker, setShowDatePicker] = useState(false);
+  const [searchQuery, setSearchQuery] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [dataset, setDataset] = useState<any>([]);
+  const [orgDataset, setOrgDataset] = useState<any>([]);
+  const [selectedDayRange, setSelectedDayRange] = useState({
+    from: null,
+    to: null,
+  }) as any;
+  const [queryParams, setQueryParams] = useState({
+    from: null,
+    to: null,
+  });
+  const cancelToken = cancelTokenSource();
+
+  function cancelRequest() {
+    cancelToken.cancel(msgRequestCanceled);
+  }
+  async function getTestResultByCategory(params: any) {
+    setLoading(true);
+    try {
+      const {data} = await guildService.guildTestResultByCategory(params, {
+        cancelToken: cancelToken.token,
+      });
+      const normalizedData: any[] = [];
+      data.forEach((item: any, index: number) => {
+        normalizedData.push({
+          id: `ovca_${index}`,
+          name: item.categoryValue || 'نامشخص',
+          total: item.testResultsCount || 0,
+          positiveCountPercentage: item.positiveTestResultsCountToTestResultsCountPercentage || 0,
+          negativeCountPercentage: item.negativeTestResultsCountToTestResultsCountPercentage || 0,
+        });
+      });
+      setDataset([...normalizedData]);
+      setOrgDataset([...normalizedData]);
+    } catch (error) {
+      // eslint-disable-next-line
+      console.log(error);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    const params = new URLSearchParams(location.search);
+    const provinceName = params.get('provinceName') || ('تهران' as any);
+    const existsCity = sideCities.some((item: any) => {
+      return item.name === provinceName;
+    });
+    if (existsCity) {
+      getTestResultByCategory({
+        ...queryParams,
+        tag: 'guild',
+        category: 'categoryDesc',
+        province: provinceName,
+      });
+    } else {
+      history.push('/dashboard/guild/province');
+    }
+    return () => {
+      cancelRequest();
+      setDataset([]);
+      setOrgDataset([]);
+    };
+  }, [queryParams, location.search]);
+
+  const focusFromDate = () => {
+    setShowDatePicker(true);
+  };
+
+  useEffect(() => {
+    if (selectedDayRange.from && selectedDayRange.to) {
+      setSearchQuery('');
+      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
+      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
+      setQueryParams({
+        ...queryParams,
+
+        from: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
+        to: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
+      });
+    } else {
+      setQueryParams({
+        ...queryParams,
+        from: null,
+        to: null,
+      });
+    }
+  }, [selectedDayRange]);
+
+  function handleSearch(e: any) {
+    const {value} = e.target;
+    let tmp = [...orgDataset];
+    if (value) {
+      tmp = [...tmp].filter(x => x.name.indexOf(value) !== -1);
+    }
+    setDataset([...tmp]);
+    setSearchQuery(value);
+  }
+
+  // function handlePageChange(page: number = 1) {
+  //   setCurrentPage(page);
+  // }
+  return (
+    <fieldset className="text-center border rounded-xl p-4 mb-16">
+      <legend className="text-black mx-auto px-3">
+        {' '}
+        اصناف ثبت نام شده در سامانه استان {cityTitle}
+      </legend>
+
+      <div className="flex align-center justify-spacebetween space-x-5 rtl:space-x-reverse mb-8">
+        <div className="flex align-center space-x-5 rtl:space-x-reverse">
+          <div className="flex items-center">
+            {showDatePicker ? (
+              <DatePickerModal
+                setSelectedDayRange={setSelectedDayRange}
+                selectedDayRange={selectedDayRange}
+                setShowDatePicker={setShowDatePicker}
+                showDatePicker
+              />
+            ) : null}
+
+            <Calendar
+              action={focusFromDate}
+              from={selectedDayRange.from}
+              to={selectedDayRange.to}
+              setSelectedDayRange={setSelectedDayRange}
+            />
+          </div>
+        </div>
+
+        <div className="flex flex-grow align-center justify-end">
+          <div className="relative inline-flex align-center leading-3">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="w-4 h-4 absolute top-1/2 transform -translate-y-1/2 right-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+              />
+            </svg>
+            <input
+              type="text"
+              placeholder="جستجوی واحد صنفی"
+              className="py-2 px-4 pr-10 text-sm border border-gray-300 rounded-lg focus:outline-none"
+              onChange={handleSearch}
+              value={searchQuery}
+            />
+          </div>
+        </div>
+      </div>
+
+      <div className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
+        <Table
+          loading={loading}
+          dataSet={[...dataset]}
+          pagination={{pageSize: 10, maxPages: 3}}
+          columns={[
+            {
+              name: 'تعداد',
+              key: 'registerCount',
+              sortable: true,
+              render: (v: any) => (
+                <span>
+                  {Number(v || 0)
+                    .commaSeprator()
+                    .toPersianDigits()}
+                </span>
+              ),
+            },
+            {
+              name: 'نام رسته',
+              key: 'name',
+
+              render: (v: any, record, index: number, page: number) => (
+                <div className="flex">
+                  {((page - 1) * 10 + index + 1).toPersianDigits()}.{v}
+                </div>
+              ),
+            },
+
+            {
+              name: 'استان',
+              key: 'province',
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              render: (v: any) => <span>تهران</span>,
+            },
+            {
+              name: 'شهر',
+              key: 'negativeCountPercentage',
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              render: (v: any) => <span>تهران</span>,
+            },
+          ]}
+          totalItems={(dataset || []).length}
+        />
+      </div>
+    </fieldset>
+  );
+};
+
+export default RegisterGuild;
