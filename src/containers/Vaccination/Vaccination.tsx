be11c9df6738133f0d7ad43cab3af77cbbaa6ae3
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-05-31 13:27:20 +0430

    fix vaccine new ui

--- src/containers/Vaccination/Vaccination.tsx
+++ src/containers/Vaccination/Vaccination.tsx
@@ -1,16 +1,21 @@
-import OverviewVaccinationStatus from "../../components/Vaccination/public/OverviewVaccinationStatus";
-import OverviewExistVaccine from "../../components/Vaccination/public/OverviewExistVaccine";
-import OverviewVaccinationStatusChart from "../../components/Vaccination/public/OverviewVaccinationStatusChart";
-
+// import OverViewVaccinationPercentageStatus from 'src/components/Vaccination/public/OverviewVaccine/OverViewVaccinationPercentageStatus';
+import OverviewExistVaccinePercentage from 'src/components/Vaccination/public/OverviewExistVaccinePercentage';
+// import OverviewVaccinationStatus from '../../components/Vaccination/public/OverviewVaccine/OverviewVaccinationStatus';
+import OverviewVaccine from 'src/components/Vaccination/public/OverviewVaccine';
+import OverviewExistVaccine from '../../components/Vaccination/public/OverviewExistVaccine';
+import OverviewVaccinationStatusChart from '../../components/Vaccination/public/OverviewVaccinationStatusChart';
 
 const Vaccination = () => {
   return (
     <div className="space-y-16 mb-8">
-      <OverviewVaccinationStatus/>
-      <OverviewExistVaccine/>
-      <OverviewVaccinationStatusChart/>
+      {/* <OverviewVaccinationStatus />
+      <OverViewVaccinationPercentageStatus /> */}
+      <OverviewVaccine />
+      <OverviewExistVaccine />
+      <OverviewExistVaccinePercentage />
+      <OverviewVaccinationStatusChart />
     </div>
-  )
-}
+  );
+};
 
 export default Vaccination;
