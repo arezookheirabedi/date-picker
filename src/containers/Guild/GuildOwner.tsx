555ce69a944486470a08155d2a9fa60e3fd4ab33
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-06-07 10:05:39 +0430

    guild owner update

--- src/containers/Guild/GuildOwner.tsx
+++ src/containers/Guild/GuildOwner.tsx
@@ -1,30 +1,30 @@
 import React from 'react';
-// import OverviewGuildPositivePcr from 'src/components/Guild/public/OverviewGuildPositivePcr';
-// import OverviewGuildPositivePcrPercentage from 'src/components/Guild/public/OverviewGuildPositivePcrPercentage';
-// import OverviewGuildRegisterNumber from 'src/components/Guild/public/OverviewGuildRegisterNumber';
-// import OverviewGuildRegisterPercentage from 'src/components/Guild/public/OverviewGuildRegisterPercentage';
-// import OverviewGuilds from 'src/components/Guild/public/OverviewGuilds';
-// import OverviewGuildsPerCategory from 'src/components/Guild/public/OverviewGuildsPerCategory';
-// import OverviewGuildsPerProvince from 'src/components/Guild/public/OverviewGuildsPerProvince';
-// import OverviewVaccination from 'src/components/Guild/public/OverviewGuildVaccination';
+import OverviewGuildPositivePcr from 'src/components/Guild/public/OverviewGuildPositivePcr';
+import OverviewGuildPositivePcrPercentage from 'src/components/Guild/public/OverviewGuildPositivePcrPercentage';
+import OverviewGuildRegisterNumber from 'src/components/Guild/public/OverviewGuildRegisterNumber';
+import OverviewGuildRegisterPercentage from 'src/components/Guild/public/OverviewGuildRegisterPercentage';
+import OverviewGuilds from 'src/components/Guild/public/OverviewGuilds';
+import OverviewGuildsPerCategory from 'src/components/Guild/public/OverviewGuildsPerCategory';
+import OverviewGuildsPerProvince from 'src/components/Guild/public/OverviewGuildsPerProvince';
+import OverviewVaccination from 'src/components/Guild/public/OverviewGuildVaccination';
 import OverviewOfGuildVaccinationProcess from 'src/components/Guild/public/OverviewOfGuildVaccinationProcess';
-// import TestStatus from 'src/components/Guild/public/TestStatus';
-// import TheLatestOverwiewOfVaccination from 'src/components/Guild/public/TheLatestOverwiewOfVaccination';
+import TestStatus from 'src/components/Guild/public/TestStatus';
+import TheLatestOverwiewOfVaccination from 'src/components/Guild/public/TheLatestOverwiewOfVaccination';
 
 const GuildOwner: React.FC<any> = () => {
   return (
     <div className="space-y-16 mb-8">
-      {/* <OverviewGuilds />
+      <OverviewGuilds />
       <OverviewGuildsPerCategory />
-      <OverviewGuildPositivePcr /> */}
-   <OverviewOfGuildVaccinationProcess /> 
-      {/* <TheLatestOverwiewOfVaccination />  */}
-      {/* <OverviewGuildPositivePcrPercentage /> */}
-      {/* <OverviewGuildRegisterPercentage />
+      <OverviewGuildPositivePcr />
+      <OverviewOfGuildVaccinationProcess />
+      <TheLatestOverwiewOfVaccination />
+      <OverviewGuildPositivePcrPercentage />
+      <OverviewGuildRegisterPercentage />
       <OverviewGuildRegisterNumber />
       <OverviewVaccination />
       <OverviewGuildsPerProvince />
-      <TestStatus /> */}
+      <TestStatus />
     </div>
   );
 };
