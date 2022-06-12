fd619fc5eceb1fc8872ca0bade3d507b5a2d4af5
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-06-12 18:15:06 +0430

    add mok data

--- src/components/Bakery/Monitoring/index.tsx
+++ src/components/Bakery/Monitoring/index.tsx
@@ -1,20 +1,24 @@
 import React, {useEffect, useState} from 'react';
 // @ts-ignore
-import moment from 'moment-jalaali';
+// import moment from 'moment-jalaali';
 import Table from 'src/components/TableXHR';
 import dayjs from 'dayjs';
 // import DatePickerModal from 'src/components/DatePickerModal';
 // import Calendar from 'src/components/Calendar';
-import {cancelTokenSource, msgRequestCanceled, toPersianDigit} from 'src/helpers/utils';
+import {toPersianDigit} from 'src/helpers/utils';
 import Spinner from 'src/components/Spinner';
 import {isEmpty} from 'lodash';
-import guildService from 'src/services/guild.service';
-import FilterInterceptors from './FilterInterceptors';
+// import guildService from 'src/services/guild.service';
+import {mock} from './constant';
+import {ExpandedForm} from './ExpandedForm';
+// import FilterInterceptors from './FilterInterceptors';
 
 const BakeryMonitoringList: React.FC<{}> = () => {
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [loading, setLoading] = useState(false);
   const [dataSet, setDataSet] = useState<any[]>([]);
-  const [totalItems, setTotalItems] = useState(0);
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [totalItems, setTotalItems] = useState(2);
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [selectedDayRange, setSelectedDayRange] = useState({
@@ -29,96 +33,108 @@ const BakeryMonitoringList: React.FC<{}> = () => {
     inspectorId: null,
     qrCode: null,
   });
-  const cancelToken = cancelTokenSource();
+  // const cancelToken = cancelTokenSource();
 
-  function cancelRequest() {
-    cancelToken.cancel(msgRequestCanceled);
-  }
-  const pageSize = 10;
+  // function cancelRequest() {
+  //   cancelToken.cancel(msgRequestCanceled);
+  // }
+  const pageSize = 1;
 
-  async function fetcher(params: any) {
-    setLoading(true);
-    try {
-      const {data} = await guildService.bakeryInspections(params, {
-        cancelToken: cancelToken.token,
+  // async function fetcher(params: any) {
+  //   setLoading(true);
+  //   try {
+  //     const {data} = await guildService.bakeryInspections(params, {
+  //       cancelToken: cancelToken.token,
+  //     });
+  //     const normalizedData: any[] = [];
+  //     data.content.forEach((item: any, index: number) => {
+  //       normalizedData.push({
+  //         id: `bakery${index}`,
+  //         inspectionDateTime: item.inspectionDateTime,
+  //         lastInspectionDateTime: item.lastInspectionDateTime,
+  //         inspectorId: item.inspectors ? item.inspectors[0].inspectorId : '-',
+  //         qrCode: item.qrCode,
+  //         unitNumber: item.unitNumber,
+  //         allData: item,
+  //       });
+  //     });
+
+  //     setDataSet([...normalizedData]);
+  //     setTotalItems(data.totalElements);
+  //   } catch (error: any) {
+  //     // eslint-disable-next-line
+  //     console.log(error);
+  //   } finally {
+  //     setLoading(false);
+  //   }
+  // }
+
+  // useEffect(() => {
+  //   if (selectedDayRange.from && selectedDayRange.to) {
+  //     const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
+  //     const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
+  //     setQuery({
+  //       ...query,
+  //       currentPage: 1,
+  //       fromReportDate: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
+  //       toReportDate: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
+  //     });
+  //   }
+  //   if (selectedDayRange.clear) {
+  //     setQuery({
+  //       ...query,
+  //       fromReportDate: null,
+  //       toReportDate: null,
+  //     });
+  //   }
+  // }, [selectedDayRange]);
+
+  const normalizer = () => {
+    const normalizedData: any[] = [];
+
+    mock.forEach((item: any, index: number) => {
+      normalizedData.push({
+        id: `bakery${index}`,
+        inspectionDateTime: item.inspectionDateTime,
+        lastInspectionDateTime: item.lastInspectionDateTime,
+        inspectorId: item.inspectors ? item.inspectors[0].inspectorId : '-',
+        qrCode: item.qrCode,
+        unitNumber: item.unitNumber,
+        allData: item,
       });
-      const normalizedData: any[] = [];
-      data.content.forEach((item: any, index: number) => {
-        normalizedData.push({
-          id: `bakery${index}`,
-          inspectionDateTime: item.inspectionDateTime,
-          lastInspectionDateTime: item.lastInspectionDateTime,
-          inspectorId: item.inspectors ? item.inspectors[0].inspectorId : '-',
-          qrCode: item.qrCode,
-          unitNumber: item.unitNumber,
-        });
-      });
-
-      setDataSet([...normalizedData]);
-      setTotalItems(data.totalElements);
-    } catch (error: any) {
-      // eslint-disable-next-line
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    if (selectedDayRange.from && selectedDayRange.to) {
-      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-      setQuery({
-        ...query,
-        currentPage: 1,
-        fromReportDate: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        toReportDate: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-      });
-    }
-    if (selectedDayRange.clear) {
-      setQuery({
-        ...query,
-        fromReportDate: null,
-        toReportDate: null,
-      });
-    }
-  }, [selectedDayRange]);
-
-  useEffect(() => {
-    fetcher({
-      sort: 'DESC',
-      //   sortKey: ['reportStatus'].join(','),
-      fromReportDate: query.fromReportDate,
-      toReportDate: query.toReportDate,
-      pageNumber: query.currentPage - 1,
-      pageSize,
     });
-    return () => {
-      setDataSet([]);
-      cancelRequest();
-    };
-  }, [query]);
 
-  //   const focusFromDate = () => {
-  //     setShowDatePicker(true);
-  //   };
+    setDataSet([...normalizedData]);
+  };
+
+  useEffect(() => {
+    normalizer();
+
+    // fetcher({
+    //   sort: 'DESC',
+    //   //   sortKey: ['reportStatus'].join(','),
+    //   fromReportDate: query.fromReportDate,
+    //   toReportDate: query.toReportDate,
+    //   pageNumber: query.currentPage - 1,
+    //   pageSize,
+    // });
+    // return () => {
+    //   setDataSet([]);
+    //   cancelRequest();
+    // };
+  }, [query]);
 
   function handlePageChange(page: number = 1) {
     setQuery({...query, currentPage: page});
   }
 
-  //   function handleSearch(e: any, key: string) {
-  //     setQuery({...query, [key]: e});
-  //     setSearchQuery({...searchQuery, [key]: e});
-  //   }
   return (
     <>
-      <fieldset className="text-center border rounded-xl p-4 mb-16">
-        <div className="mb-8 flex flex-col items-center justify-center space-y-6">
+      <fieldset className="mb-16 rounded-xl border p-4 text-center">
+        {/* <div className="mb-8 flex flex-col items-center justify-center space-y-6">
           <FilterInterceptors handleSetFilters={setQuery} />
-        </div>
-
-        <div className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
+        </div> */}
+        <div className="align-center flex w-full flex-col justify-center rounded-xl bg-white p-4 shadow">
           {loading ? (
             <div className="p-20">
               <Spinner />
@@ -127,13 +143,7 @@ const BakeryMonitoringList: React.FC<{}> = () => {
             <Table
               expandable={{
                 rowExpandable: (record: any) => record && !isEmpty(record),
-                expandedRowRender: ({...record}: any) => {
-                  return (
-                    <div className="w-full rounded bg-white p-4 px-2 pb-2 shadow-lg">
-                      <span>{record?.id}</span>
-                    </div>
-                  );
-                },
+                expandedRowRender: ({...record}) => <ExpandedForm {...record} />,
               }}
               handlePageChange={handlePageChange}
               dataSet={[...dataSet]}
@@ -182,7 +192,7 @@ const BakeryMonitoringList: React.FC<{}> = () => {
                   key: 'inspectionDateTime',
                   render: (v: any, record: any) => (
                     <div className="flex w-full justify-center">
-                      <span className="text-gray-500 whitespace-normal ">
+                      <span className="whitespace-normal text-gray-500 ">
                         {toPersianDigit(
                           dayjs(record.inspectionDateTime).calendar('jalali').format('YYYY/MM/DD')
                         )}
@@ -197,14 +207,14 @@ const BakeryMonitoringList: React.FC<{}> = () => {
                   render: (v: any, record: any) => (
                     <>
                       <div className="flex w-full justify-center">
-                        <span className="text-gray-500 whitespace-normal ">
+                        <span className="whitespace-normal text-gray-500 ">
                           {toPersianDigit(
                             dayjs(record.inspectionDateTime).calendar('jalali').format('YYYY/MM/DD')
                           )}
                         </span>
                       </div>
                       <div className="pt-3">
-                        <span className="text-gray-500 whitespace-normal">
+                        <span className="whitespace-normal text-gray-500">
                           {toPersianDigit(
                             dayjs(record.requestDateTime).calendar('jalali').format('HH:mm')
                           )}
