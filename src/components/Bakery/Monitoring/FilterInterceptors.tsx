2ad03b6ec57dc2349597fee42d5fe85fe042683d
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-06-13 14:43:52 +0430

    update form

--- src/components/Bakery/Monitoring/FilterInterceptors.tsx
+++ src/components/Bakery/Monitoring/FilterInterceptors.tsx
@@ -2,25 +2,27 @@ import React, {useState} from 'react';
 import {Controller, useForm} from 'react-hook-form';
 import {yupResolver} from '@hookform/resolvers/yup';
 import * as Yup from 'yup';
-import dayjs from 'dayjs';
+// import dayjs from 'dayjs';
 import DotLoading from 'src/components/Loading/DotLoading';
-import DatePicker from './DatePicker';
+import DatePicker from 'src/components/Form/DatePicker';
+// import calendar from "src/assets/images/icons/calendar.svg";
 
 interface Iprops {
   handleSetFilters: any;
 }
 
 interface ISavedVisite {
-  date: any;
-  nationalId: any;
-  status: any;
-  numberOfDoses: any;
+  from: any;
+  to: any;
+  inspectorId: any;
+  inspectorNationalId: any;
+  qrCode: any;
 }
 
-const convertDay = (day: any) => {
-  const date = new Date(day);
-  return dayjs(date).locale('en').format('YYYY-MM-DD');
-};
+// const convertDay = (day: any) => {
+//   const date = new Date(day);
+//   return dayjs(date).locale('en').format('YYYY-MM-DD');
+// };
 
 const FilterSavedInquiry: React.FC<Iprops> = ({handleSetFilters}) => {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -44,10 +46,10 @@ const FilterSavedInquiry: React.FC<Iprops> = ({handleSetFilters}) => {
     handleSetFilters((filters: any) => {
       return {
         ...filters,
-        inquiryDate: values.inquiryDate ? convertDay(values.inquiryDate) : null,
-        permissionStatus: values.status,
-        dose: values.numberOfDoses,
-        nationalId: values.nationalId ? values.nationalId : null,
+        // from: values.inquiryDate ? convertDay(values.inquiryDate) : null,
+        inspectorId: values.inspectorId,
+        inspectorNationalId: values.inspectorNationalId,
+        qrCode: values.qrCode,
         currentPage: 1,
       };
     });
@@ -55,38 +57,92 @@ const FilterSavedInquiry: React.FC<Iprops> = ({handleSetFilters}) => {
 
   return (
     <>
-      <form className="w-2/3 space-y-4" onSubmit={handleSubmit(onSubmit)}>
-        <div className="flex space-x-3 rtl:space-x-reverse">
-          <div className="w-full">
+      <form className="w-full space-y-4" onSubmit={handleSubmit(onSubmit)}>
+        <div className="flex  rtl:space-x-reverse">
+    
+        <div className="w-full">
             <Controller
               control={control}
-              name="date"
+              name="from"
               // eslint-disable-next-line @typescript-eslint/no-unused-vars
               render={({field: {onChange, onBlur, value, ref, name}}) => (
                 <DatePicker
                   onChange={onChange}
                   onBlur={onBlur}
+                  placeholder='از تاریخ'
                   selected={value}
-                  error={errors.date}
+                  error={errors.from}
                   iClass={`ltr  relative block w-full rounded-full  bg-white px-5 py-2 placeholder-gray-400 shadow-lg        focus:outline-none disabled:bg-gray-50 sm:text-sm ${
-                    errors ? 'border-1 border-rose-600' : ''
+                    errors ? "border-1 border-rose-600" : ""
                   } focus-visible disabled:shadow-none`}
                   name={name}
-                  // aria-invalid={errors.date ? true : false}
+                  // eslint-disable-next-line no-unneeded-ternary
+                  aria-invalid={errors.from? true : false}
                 />
               )}
             />
 
-            <p className={`${errors.date ? 'visible' : 'invisible'} mt-1 text-xs text-red-600`}>
-              {errors.date?.message}
+            <p
+              className={`${
+                errors.from? "visible" : "invisible"
+              } mt-1 text-xs text-red-600`}
+            >
+              {errors.from?.message}
+            </p>
+          </div>
+
+          <div className="w-full">
+            <Controller
+              control={control}
+              name="to"
+              // eslint-disable-next-line @typescript-eslint/no-unused-vars
+              render={({field: {onChange, onBlur, value, ref, name}}) => (
+                <DatePicker
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  placeholder='تا تاریخ'
+                  selected={value}
+                  error={errors.to}
+                  iClass={`ltr  relative block w-full rounded-full  bg-white px-5 py-2 placeholder-gray-400 shadow-lg        focus:outline-none disabled:bg-gray-50 sm:text-sm ${
+                    errors ? "border-1 border-rose-600" : ""
+                  } focus-visible disabled:shadow-none`}
+                  name={name}
+                  // eslint-disable-next-line no-unneeded-ternary
+                  aria-invalid={errors.to? true : false}
+                />
+              )}
+            />
+
+            <p
+              className={`${
+                errors.to? "visible" : "invisible"
+              } mt-1 text-xs text-red-600`}
+            >
+              {errors.to?.message}
             </p>
           </div>
 
           <div className="w-full">
             <input
               type="text"
-              {...register('nationalId')}
-              placeholder="کد ملی مد نظر"
+              {...register('inspectorNationalId')}
+              placeholder="کد ملی بازرس"
+              className="rtl  focus-visible relative block w-full  rounded-full bg-white px-5 py-2 placeholder-gray-400 shadow-lg focus:outline-none disabled:bg-gray-50  disabled:shadow-none sm:text-sm"
+            />
+          </div>
+          <div className="w-full">
+            <input
+              type="text"
+              {...register('inspectorId')}
+              placeholder="کد بازرس"
+              className="rtl  focus-visible relative block w-full  rounded-full bg-white px-5 py-2 placeholder-gray-400 shadow-lg focus:outline-none disabled:bg-gray-50  disabled:shadow-none sm:text-sm"
+            />
+          </div>
+          <div className="w-full">
+            <input
+              type="text"
+              {...register('qrCode')}
+              placeholder="QR-code"
               className="rtl  focus-visible relative block w-full  rounded-full bg-white px-5 py-2 placeholder-gray-400 shadow-lg focus:outline-none disabled:bg-gray-50  disabled:shadow-none sm:text-sm"
             />
           </div>
@@ -95,20 +151,21 @@ const FilterSavedInquiry: React.FC<Iprops> = ({handleSetFilters}) => {
               type="button"
               onClick={() => {
                 reset({
-                  date: null,
-                  status: null,
-                  numberOfDoses: null,
-                  nationalId: null,
+                  from: null,
+                  to: null,
+                  inspectorId: null,
+                  inspectorNationalId: null,
+                  qrCode: null,
                 });
               }}
-              className="flex w-full items-center justify-center whitespace-nowrap rounded-full border border-[#175A76] bg-white px-4 py-1.5 text-sm text-[#175A76] rtl:space-x-reverse"
+              className="flex w-full items-center justify-center whitespace-nowrap rounded-full border  bg-white px-4 py-1.5 text-sm  rtl:space-x-reverse "style={{borderColor:'#175A76', color:"#175A76"}}
             >
               <span>حذف فیلتر</span>
             </button>
             <button
               type="submit"
               disabled={submitted}
-              className="flex w-full items-center justify-center whitespace-nowrap rounded-full bg-[#175A76] px-4 py-1.5 text-sm text-white rtl:space-x-reverse"
+              className="flex w-full items-center justify-center whitespace-nowrap rounded-full  px-4 py-1.5 text-sm text-white rtl:space-x-reverse"style={{backgroundColor:'#175A76'}}
             >
               <span>{!submitted ? 'اعمال فیلتر' : <DotLoading />}</span>
             </button>
