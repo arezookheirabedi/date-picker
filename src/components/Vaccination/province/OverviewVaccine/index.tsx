d7d62872d92b2a9b35f852ca8c73906e8a1c0fe2
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-06-14 14:18:52 +0430

    update

--- src/components/Vaccination/province/OverviewVaccine/index.tsx
+++ src/components/Vaccination/province/OverviewVaccine/index.tsx
@@ -3,26 +3,19 @@ import {cancelTokenSource, msgRequestCanceled, sideCities} from 'src/helpers/uti
 import vaccineService from 'src/services/vaccine.service';
 import {useHistory, useLocation} from 'react-router-dom';
 import hcsService from 'src/services/hcs.service';
-import { isEmpty } from 'lodash';
 import {IInitialVacinatelInfo, initialVacinatelInfo} from '../../public/constant';
 import OverViewVaccinationPercentageStatus from './OverviewVaccinePercentage';
 import OverviewVaccinationStatus from './OverviewVaccineCount';
-import OverviewVaccinePerDoses from './OverviewVaccinePerDoses';
 
 const OverviewVaccine: React.FC<{cityTitle: string}> = ({cityTitle}) => {
   const location = useLocation();
   const history = useHistory();
   const [loading, setLoading] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
   const [numberOf, setNumberOf] = useState<IInitialVacinatelInfo>(initialVacinatelInfo);
   const [thelatestNumberOf, setThelatestNumberOf] =
     useState<IInitialVacinatelInfo>(initialVacinatelInfo);
   const [theLatestloading, setTheLatestLoading] = useState(false);
-  const [chartData, setChartData] = useState<any>();
-  const [queryParams, setQueryParams] = useState({
-    from: null,
-    to: null,
-  });
+
 
   const cancelToken = cancelTokenSource();
 
@@ -31,96 +24,18 @@ const OverviewVaccine: React.FC<{cityTitle: string}> = ({cityTitle}) => {
   }
 
   const getNumberOf = async (provinceName: string) => {
-    // setLoading(true);
-    // try {
-    //   const {data} = await vaccineService.membersGeneral(
-    //     {province: provinceName},
-    //     {cancelToken: cancelToken.token}
-    //   );
-    setErrorMessage(null);
     setLoading(true);
     try {
-      const res =  await vaccineService.membersGeneral(
+      const {data} =  await vaccineService.membersGeneral(
             {province: provinceName},
             {cancelToken: cancelToken.token}
           );
-      const finalResponse: any = {...res.data};
-      if (!isEmpty(finalResponse)) {
-        const dataChart: any = {
-          null: 5,
-          '0': finalResponse.totalNonVaccinesCountToTotalPopulationPercentage || 0, // واکسن نزدع
-          '1': finalResponse.dosesToTotalPopulationPercentage[1] || 0, // دوز اول
-          '2': finalResponse.dosesToTotalPopulationPercentage[2] || 0, // دوز دوم
-          '3': finalResponse.dosesToTotalPopulationPercentage[3] || 0, // دوز سوم
-          '4': finalResponse.dosesToTotalPopulationPercentage[4] || 0, // دوز چهارم
-          '5': finalResponse.dosesToTotalPopulationPercentage[5] || 0, // دوز پنجم
-        };
-
-        // eslint-disable-next-line
-        let firstDose: number = 0;
-        // eslint-disable-next-line
-        let secondDose: number = 0;
-        // eslint-disable-next-line
-        let thirdDose: number = 0;
-        // eslint-disable-next-line
-        let forthDoses: number = 0;
-        // eslint-disable-next-line
-        let fifthDoses: number = 0;
-        // eslint-disable-next-line
-        let noDose: number = 0;
-
-        Object.entries(dataChart).forEach(([key, value]: any[]) => {
-          switch (key) {
-            case 'null':
-              // noDose += value;
-              break;
-            case '0':
-              noDose += value;
-              break;
-            case '1':
-              firstDose += value;
-              break;
-            case '2':
-              secondDose += value;
-              break;
-            case '3':
-              thirdDose += value;
-              break;
-            case '4':
-              forthDoses += value;
-              break;
-            case '5':
-              fifthDoses += value;
-              break;
-            default:
-              break;
-          }
-        });
-
-        const newInitialData = {
-          categories: ['دوز اول', 'دوز دوم', 'دوز سوم', 'دوز چهارم', 'دوز پنجم', 'واکسن نزده'],
-          series: [
-            {
-              name: 'واکسیناسیون',
-              data: [
-                {name: 'دوز اول', y: firstDose, color: '#F3BC06'},
-                {name: 'دوز دوم', y: secondDose, color: '#209F92'},
-                {name: 'دوز سوم', y: thirdDose, color: '#004D65'},
-                {name: ' دوز چهارم', y: forthDoses, color: '#bfdde7'},
-                {name: 'دوز پنجم', y: fifthDoses, color: '#716de3'},
-                {name: 'واکسن نزده', y: noDose, color: '#FF0060'},
-              ],
-            },
-          ],
-        } as any;
-
-        setChartData({...newInitialData});
-      }
+    
 
       setNumberOf((prev: any) => {
         return {
           ...prev,
-          ...finalResponse,
+          ...data,
         };
       });
     } catch (error) {
@@ -166,7 +81,7 @@ const OverviewVaccine: React.FC<{cityTitle: string}> = ({cityTitle}) => {
       setNumberOf(initialVacinatelInfo);
       setThelatestNumberOf(initialVacinatelInfo);
     };
-  }, [queryParams, location.search,location.search]);
+  }, [location.search]);
 
   return (
     <div id="vaccination-overview">
@@ -188,19 +103,7 @@ const OverviewVaccine: React.FC<{cityTitle: string}> = ({cityTitle}) => {
         />
 
 </fieldset>
-<fieldset className="mb-16 rounded-xl border p-4 text-center">
-<legend className="mx-auto px-3 text-black">
-نگاه کلی به وضعیت واکسیناسیون استان {cityTitle}
-        </legend>
 
-<OverviewVaccinePerDoses
-    loading={loading}
-    numberOf={chartData}
-    setQueryParams={setQueryParams}
-    queryParams={queryParams}
-    errorMessage={errorMessage}
-      />
-      </fieldset>
     </div>
   );
 };
