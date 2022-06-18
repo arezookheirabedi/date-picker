f7cb41632f6a3d0f20f088443c67ac92aab4a010
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-06-18 16:07:52 +0430

    update data in canceltoken

--- src/components/School/public/OverviewOfVaccinationProcess.tsx
+++ src/components/School/public/OverviewOfVaccinationProcess.tsx
@@ -195,7 +195,7 @@ const OverviewOfVaccinationProcess = () => {
     return () => {
       clearTimeout(idSetTimeOut);
       cancelRequest();
-      setDataset([]);
+      setDataset({});
     };
   }, []);
 
