99c7e9dfb6cb0df564f8aa9ee0dc62a0555eb32f
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-06-18 11:35:03 +0430

    format

--- src/components/School/province/OverviewPatientsProvince.tsx
+++ src/components/School/province/OverviewPatientsProvince.tsx
@@ -75,27 +75,6 @@ const OverviewPatientsProvince: React.FC<OverviewPatientsProvinceProps> = ({city
     }
   };
 
-  // const getLinearOverview = async (params: any) => {
-  //   setLoading(true);
-  //   setErrorMessage(null);
-  //   setIsCancel(false);
-  //   try {
-  //     const response = await hcsService.testResultTimeBased(params, {cancelToken: source.token});
-  //     setData(response.data);
-  //   } catch (error: any) {
-  //     if (error.message !== 'cancel') {
-  //       setErrorMessage(error.message);
-  //     }
-  //     if (error && error.message === 'cancel') {
-  //       setIsCancel(true);
-  //     }
-  //     // eslint-disable-next-line
-  //     console.log(error);
-  //   } finally {
-  //     setLoading(false);
-  //   }
-  // };
-
   useEffect(() => {
     const params = new URLSearchParams(location.search);
     const provinceName = params.get('provinceName') || ('تهران' as any);
@@ -149,59 +128,15 @@ const OverviewPatientsProvince: React.FC<OverviewPatientsProvinceProps> = ({city
     }
   }, [selectedDayRange]);
 
-  // useEffect(() => {
-  //   if (selectedDayRange.from && selectedDayRange.to) {
-  //     const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-  //     const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-
-  //     const tmp: any[] = [];
-  //     let lastState = 'ANNUAL';
-
-  //     const start = moment(finalFromDate, 'jYYYY/jM/jD');
-  //     const end = moment(finalToDate, 'jYYYY/jM/jD');
-
-  //     const duration = moment.duration(end.diff(start));
-
-  //     if (!duration.years()) {
-  //       tmp.push(3);
-  //       lastState = 'MONTHLY';
-  //     }
-
-  //     if (!duration.months() && !duration.years()) {
-  //       tmp.push(2);
-  //       lastState = 'WEEKLY';
-  //     }
-
-  //     if (!duration.weeks() && !duration.months() && !duration.years()) {
-  //       tmp.push(1);
-  //       lastState = 'DAILY';
-  //     }
-
-  //     setQueryParams({
-  //       ...queryParams,
-  //       type: lastState,
-  //       from: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DDTHH:mm:ss'),
-  //       to: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DDTHH:mm:ss'),
-  //     });
-  //   } else {
-  //     setQueryParams({
-  //       ...queryParams,
-  //       type: 'MONTHLY',
-  //       from: null,
-  //       to: null,
-  //     });
-  //   }
-  // }, [selectedDayRange]);
-
   return (
-    <fieldset className="text-center border rounded-xl p-4 mb-16">
-      <legend className="text-black mx-auto px-3">
+    <fieldset className="mb-16 rounded-xl border p-4 text-center">
+      <legend className="mx-auto px-3 text-black">
         نگاه کلی مبتلایان آموزش و پرورش در استان &nbsp;
         {cityTitle}
       </legend>
-      <div className="flex flex-col align-center justify-center w-full rounded-lg bg-white p-4 shadow">
-        <div className="flex items-center justify-between mb-10 mt-6">
-          <div className="flex align-center justify-start flex-grow px-8">
+      <div className="align-center flex w-full flex-col justify-center rounded-lg bg-white p-4 shadow">
+        <div className="mb-10 mt-6 flex items-center justify-between">
+          <div className="align-center flex flex-grow justify-start px-8">
             <SearchableSingleSelect
               objectKey="categoryValue"
               placeholder="کل آموزش و پرورش"
@@ -210,7 +145,7 @@ const OverviewPatientsProvince: React.FC<OverviewPatientsProvinceProps> = ({city
               setQueryParams={setQuery}
               queryParams={query}
             />
-            <div className="flex align-center justify-between mr-8">
+            <div className="align-center mr-8 flex justify-between">
               {showDatePicker ? (
                 <DatePickerModal
                   setSelectedDayRange={setSelectedDayRange}
