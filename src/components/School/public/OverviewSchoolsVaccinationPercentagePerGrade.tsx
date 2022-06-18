1da9966cf8c802f46e11f1c7b8ea26d76900262d
z.kheyrabadi <a.kheirabadi92@gmail.com>
2022-06-18 13:12:22 +0430

    update chart

--- src/components/School/public/OverviewSchoolsVaccinationPercentagePerGrade.tsx
+++ src/components/School/public/OverviewSchoolsVaccinationPercentagePerGrade.tsx
@@ -5,24 +5,21 @@ import hcsService from 'src/services/hcs.service';
 import DatePickerModal from 'src/components/SingleDatePickerModal';
 import Calendar from 'src/components/Calendar/SingleCalendar';
 // import SearchableSingleSelect from 'src/components/SearchableSingleSelect';
+import {isEmpty} from 'lodash';
+import Highcharts from 'highcharts';
 import Charts from '../../Charts';
 import {cancelTokenSource, msgRequestCanceled} from '../../../helpers/utils';
 import Spinner from '../../Spinner';
 
-const {Stacked} = Charts;
+const {HeadlessChart} = Charts;
 
 interface OverviewPerProvinceProps {}
 
 const OverviewSchoolsVaccinationPercentagePerGrade: React.FC<OverviewPerProvinceProps> = () => {
-  const [dataset, setDataset] = useState<any[]>([]);
-  const [categories, setCategories] = useState<any[]>([]);
-  // eslint-disable-next-line
+  const [dataset, setDataset] = useState<any>({});
   const [showDatePicker, setShowDatePicker] = useState(false);
-  // eslint-disable-next-line
   const [errorMessage, setErrorMessage] = useState(null);
-  // eslint-disable-next-line
   const [loading, setLoading] = useState(false);
-  // eslint-disable-next-line
   const [selectedDay, setSelectedDay] = useState(null) as any;
 
   const focusFromDate = () => {
@@ -61,7 +58,7 @@ const OverviewSchoolsVaccinationPercentagePerGrade: React.FC<OverviewPerProvince
         grade.push(item.categoryValue);
       });
 
-      setDataset([
+      const newData = [
         {
           name: 'واکسن نزده',
           data: [...nonVaccinesPercentage],
@@ -80,21 +77,9 @@ const OverviewSchoolsVaccinationPercentagePerGrade: React.FC<OverviewPerProvince
           name: 'واکسن زده',
           data: [...vaccinesPercentage],
           color: '#04b086',
-          // color: {
-          //   linearGradient: {
-          //     x1: 0,
-          //     x2: 0,
-          //     y1: 0,
-          //     y2: 1,
-          //   },
-          //   stops: [
-          //     [0, '#048365'],
-          //     [1, '#04d2a0'],
-          //   ],
-          // },
         },
-      ]);
-      setCategories([...grade]);
+      ];
+      setDataset({categories: [...grade], series: [...newData]});
     } catch (error: any) {
       setErrorMessage(error.message);
       // eslint-disable-next-line
@@ -106,7 +91,12 @@ const OverviewSchoolsVaccinationPercentagePerGrade: React.FC<OverviewPerProvince
 
   useEffect(() => {
     const idSetTimeOut = setTimeout(() => {
-      getLinearOverview({...queryParams, tag: 'edu', category: 'grade'});
+      getLinearOverview({
+        ...queryParams,
+        tag: 'edu',
+        category: 'grade',
+        //  province: 'تهران'
+      });
     }, 500);
     return () => {
       clearTimeout(idSetTimeOut);
@@ -129,7 +119,114 @@ const OverviewSchoolsVaccinationPercentagePerGrade: React.FC<OverviewPerProvince
       });
     }
   }, [selectedDay]);
+  const converters = {
+    fa(number: any) {
+      return number.toString().replace(/\d/g, (d: any) => {
+        return String.fromCharCode(d.charCodeAt(0) + 1728);
+      });
+    },
+  };
+  const optionChart = {
+    chart: {
+      renderTo: 'container',
+      type: 'column',
+      numberFormatter() {
+        // eslint-disable-next-line prefer-rest-params
+        const ret = Highcharts.numberFormat.apply(0, arguments as any);
+        return converters.fa(ret);
+      },
 
+      events: {
+        redraw: () => {
+          // eslint-disable-next-line
+          // console.log('redraw');
+        },
+      },
+      // zoomType: 'x'
+      // styledMode: true
+    },
+    title: {
+      text: '',
+    },
+    scrollbar: {
+      enabled: true,
+      barBackgroundColor: '#656565',
+      barBorderColor: '#eee',
+      barBorderRadius: 4,
+      barBorderWidth: 0,
+      height: 6,
+      buttonArrowColor: '#eee',
+      rifleColor: '#656565',
+      buttonBackgroundColor: 'transparent',
+      buttonBorderWidth: 0,
+      buttonBorderRadius: 0,
+      trackBackgroundColor: '#eee',
+      trackBorderWidth: 0,
+      trackBorderRadius: 4,
+    },
+    credits: {
+      enabled: false,
+    },
+    // colors: ['#FFC700', '#883BA4', '#175A76', '#00AAB1'],
+    plotOptions: {
+      series: {
+        // stacking: 'percent',
+        stacking: 'percent',
+        // borderRadius: 5,
+        pointWidth: 15,
+      },
+      column: {
+        threshold: null,
+        grouping: false,
+        shadow: false,
+        borderWidth: 0,
+      },
+    },
+    yAxis: {
+      tickInterval: 25,
+      tickAmount: 5,
+      softMin: 0,
+      gridLineDashStyle: 'dash',
+      lineDashStyle: 'dash',
+      lineColor: '#000000',
+      lineWidth: 1,
+      opposite: true,
+      title: {
+        enabled: false,
+      },
+      labels: {
+        format: '٪{text}',
+      },
+    },
+    legend: {
+      enabled: false,
+    },
+    xAxis: {
+      categories: [],
+      type: 'category',
+      labels: {
+        rotation: 45,
+      },
+      // lineDashStyle: 'dash',
+      // lineColor: '#000000',
+      // lineWidth: 1
+    },
+    tooltip: {
+      shared: true,
+      useHTML: true,
+      valueSuffix: '٪',
+      style: {
+        direction: 'rtl',
+        textAlign: 'right',
+        fontFamily: 'inherit',
+        fontSize: 10,
+      },
+      borderWidth: 0,
+      // headerFormat: `<div style="min-width:220px">{point.x}</div>`
+    },
+
+    series: [],
+  };
   return (
     <fieldset className="mb-16 rounded-xl border p-4 text-center">
       <legend className="mx-auto px-3 text-black">
@@ -168,15 +265,10 @@ const OverviewSchoolsVaccinationPercentagePerGrade: React.FC<OverviewPerProvince
           </div>
         )}
         {errorMessage && <div className="p-40 text-red-500">{errorMessage}</div>}
-        {!loading && dataset.length > 0 && !errorMessage && (
-          <Stacked
-            data={dataset}
-            categories={categories}
-            tooltipSuffix="٪"
-            yAxisLabelFormat="٪{text}"
-          />
+        {!loading && !isEmpty(dataset) && !errorMessage && (
+          <HeadlessChart data={dataset} optionsProp={optionChart} />
         )}
-        {dataset.length === 0 && !loading && !errorMessage && (
+        {isEmpty(dataset) && !loading && !errorMessage && (
           <div className="p-40 text-red-500">موردی برای نمایش وجود ندارد.</div>
         )}
       </div>
