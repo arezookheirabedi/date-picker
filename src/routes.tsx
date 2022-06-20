65e144d29e7d99726607c5b14f9b7f94d50ef3e6
Muhammad Sh <muh.shahabipour@gmail.com>
2022-06-20 10:24:28 +0430

    change route

--- src/routes.tsx
+++ src/routes.tsx
@@ -284,7 +284,7 @@ const routes: IRoute[] = [
     simLink: '/dashboard/guilds',
     exact: true,
     inMenu: true,
-    title: 'اصناف',
+    title: 'بازرسی اصناف',
     subMenu: [
       {
         keyIndex: '10',
