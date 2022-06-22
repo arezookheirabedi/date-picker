f84a956017583989b67745e054143b8adf0d83c6
emadabbasii <emadabbasee@gmail.com>
2022-06-22 18:09:50 +0430

    refactor all components that have datepicker

--- src/components/Transport/public/OverviewOfThePublicTransportVaccinationStatusInCountry.tsx
+++ src/components/Transport/public/OverviewOfThePublicTransportVaccinationStatusInCountry.tsx
@@ -1,16 +1,13 @@
 import React, {useEffect, useState} from 'react';
 import axios from "axios";
-// @ts-ignore
-import moment from 'moment-jalaali';
+
 import Highcharts from "highcharts/highstock";
 // import vaccineService from 'src/services/vaccine.service';
 // import axios from 'axios';
-import DatePickerModal from '../../DatePickerModal';
-import {toPersianDigit} from '../../../helpers/utils';
-import calendar from '../../../assets/images/icons/calendar.svg';
 import Spinner from '../../Spinner';
 import Charts from '../../Charts';
 import hcsService from "../../../services/hcs.service";
+import DatepickerQuery from "../../DatepickerQuery";
 
 
 const {HeadlessChart} = Charts;
@@ -163,45 +160,15 @@ const OverviewOfThePublicTransportVaccinationStatusInCountry = () => {
   const source = CancelToken.source();
   const [dataset, setDataset] = useState<any[]>(initialData) as any;
   // const [categories, setCategories] = useState<any[]>([]);
-  const [showDatePicker, setShowDatePicker] = useState(false);
+
   // eslint-disable-next-line
   const [errorMessage, setErrorMessage] = useState(null) as any;
   // eslint-disable-next-line
   const [loading, setLoading] = useState(false);
-  const [selectedDayRange, setSelectedDayRange] = useState({
+  const [query, setQuery] = useState({
     from: null,
     to: null,
-    clear: false,
   }) as any;
-
-  const [query, setQuery] = useState({
-    resultReceiptDateFrom: null,
-    resultReceiptDateTo: null,
-  }) as any;
-
-  const focusFromDate = () => {
-    setShowDatePicker(true);
-  };
-
-  const generateFromDate: any = () => {
-    // eslint-disable-next-line
-    return selectedDayRange.from
-      ? // eslint-disable-next-line
-      selectedDayRange.from.year +
-      '/' +
-      selectedDayRange.from.month +
-      '/' +
-      selectedDayRange.from.day
-      : '';
-  };
-
-  const generateToDate: any = () => {
-    // eslint-disable-next-line
-    return selectedDayRange.to
-      ? // eslint-disable-next-line
-      selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day
-      : '';
-  };
 
   // eslint-disable-next-line
   const getVaccinesGroupedByProvinceReport = async (params: any) => {
@@ -305,10 +272,7 @@ const OverviewOfThePublicTransportVaccinationStatusInCountry = () => {
 
   useEffect(() => {
     const idSetTimeOut = setTimeout(() => {
-      getVaccinesGroupedByProvinceReport({
-        from: query.resultReceiptDateFrom,
-        to: query.resultReceiptDateTo
-      });
+      getVaccinesGroupedByProvinceReport(query);
     }, 500);
 
     return () => {
@@ -319,118 +283,13 @@ const OverviewOfThePublicTransportVaccinationStatusInCountry = () => {
   }, [query]);
 
 
-  useEffect(() => {
-    if (selectedDayRange.from && selectedDayRange.to) {
-      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-      // const m = moment(finalFromDate, 'jYYYY/jM/jD'); // Parse a Jalaali date
-      // console.log(moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-M-DTHH:mm:ss'));
-      setQuery({
-        ...query,
-        resultReceiptDateFrom: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        resultReceiptDateTo: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-      });
-    } else {
-      setQuery({
-        ...query,
-        resultReceiptDateFrom: null,
-        resultReceiptDateTo: null,
-      });
-    }
-
-  }, [selectedDayRange]);
-
-  const clearSelectedDayRange = (e: any) => {
-    e.stopPropagation();
-    setSelectedDayRange({
-      from: null,
-      to: null,
-    });
-  };
-
   return (
     <fieldset className="text-center border rounded-xl p-4 mb-16">
       <legend className="text-black mx-auto px-3">نگاه کلی به وضعیت واکسیناسیون حمل و نقل عمومی کشور</legend>
       <div className="flex flex-col align-center justify-center w-full rounded-lg bg-white p-4 shadow">
         <div className="flex items-center justify-between mb-10 mt-6 px-8">
-          <div className="flex align-center justify-between w-3/4">
-            <div className="flex align-center justify-between">
-              {showDatePicker ? (
-                <DatePickerModal
-                  setSelectedDayRange={setSelectedDayRange}
-                  selectedDayRange={selectedDayRange}
-                  setShowDatePicker={setShowDatePicker}
-                  showDatePicker
-                />
-              ) : null}
-              <div className="relative z-20 inline-block text-left shadow-custom rounded-lg px-4 py-1">
-                <div
-                  className="inline-flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
-                  onClick={focusFromDate}
-                >
-                  {selectedDayRange.from && (
-                    <span className="ml-4 whitespace-nowrap truncate text-xs">
-                      {toPersianDigit(generateFromDate())}
-                    </span>
-                  )}
-                  {selectedDayRange.to || selectedDayRange.from ? (
-                    <button type="button" onClick={clearSelectedDayRange}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 text-gray-400"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={1}
-                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                        />
-                      </svg>
-                    </button>
-                  ) : (
-                    <img src={calendar} alt="x" className="w-5 h-5"/>
-                  )}
-                </div>
-              </div>
-              <div className="flex items-center justify-start mx-4">
-                <span className="dash-separator"/>
-              </div>
-              <div className=" shadow-custom rounded-lg px-4 py-1">
-                <div
-                  className="flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
-                  onClick={focusFromDate}
-                >
-                  {selectedDayRange.to && (
-                    <span className="ml-4 whitespace-nowrap truncate text-xs">
-                      {toPersianDigit(generateToDate())}
-                    </span>
-                  )}
-                  {selectedDayRange.to || selectedDayRange.from ? (
-                    <button type="button" onClick={clearSelectedDayRange}>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="h-5 w-5 text-gray-400"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={1}
-                          d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                        />
-                      </svg>
-                    </button>
-                  ) : (
-                    <img src={calendar} alt="x" className="w-5 h-5"/>
-                  )}
-                </div>
-              </div>
-            </div>
+          <div className="flex align-center justify-start w-3/4">
+            <DatepickerQuery query={query} setQuery={setQuery}/>
           </div>
 
           <div className="w-2/4">
--- src/components/Transport/public/OverviewOfVaccinationInPublicTransport.tsx
+++ src/components/Transport/public/OverviewOfVaccinationInPublicTransport.tsx
@@ -1,7 +1,5 @@
 import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-// @ts-ignore
-import moment from 'moment-jalaali';
 // import {Menu} from '@headlessui/react';
 import Statistic from '../../../containers/Guild/components/Statistic';
 import totalDriver from '../../../assets/images/icons/transport-color.svg';
@@ -18,23 +16,9 @@ import Spinner from '../../Spinner';
 // import {ReactComponent as DownIcon} from '../../../assets/images/icons/down.svg';
 import vaccineService from '../../../services/vaccine.service';
 import hcsService from '../../../services/hcs.service';
-import DatePickerModal from "../../DatePickerModal";
-import {toPersianDigit} from "../../../helpers/utils";
-import calendar from "../../../assets/images/icons/calendar.svg";
 import OrangeVaccine from "../../../assets/images/icons/orange-vaccine.svg";
 import DarkgreenVaccine from "../../../assets/images/icons/darkgreen-vaccine.svg";
-
-
-// const filterTypes = [
-//   {
-//     name: 'بیشترین',
-//     enName: 'HIGHEST',
-//   },
-//   {
-//     name: 'کمترین',
-//     enName: 'LOWEST',
-//   },
-// ];
+import DatepickerQuery from "../../DatepickerQuery";
 
 const initialDoses = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, null: 0};
 const initialNumberOf = {
@@ -55,57 +39,16 @@ const initialNumberOf = {
 };
 
 const OverviewOfVaccinationInPublicTransport: React.FC<{}> = () => {
-  const [filterType, setFilterType] = useState({name: 'کمترین', enName: 'LOWEST'});
+  // const [filterType, setFilterType] = useState({name: 'کمترین', enName: 'LOWEST'});
   const [numberOf, setNumberOf] = useState<any>(initialNumberOf);
   const [loading, setLoading] = useState(false);
-  const [orgDataset, setOrgDataset] = useState<any>([]);
+  // const [orgDataset, setOrgDataset] = useState<any>([]);
   const [dataset, setDataset] = useState<any>([]);
-  const [showDatePicker, setShowDatePicker] = useState(false);
   const [datasetLoading, setDatasetLoading] = useState<any>([]);
   const [query, setQuery] = useState({
     from: null,
     to: null
   })
-  // const [countsLoading, setCountsLoading] = useState(false);
-
-  const [selectedDayRange, setSelectedDayRange] = useState({
-    from: null,
-    to: null,
-  }) as any;
-
-  const focusFromDate = () => {
-    setShowDatePicker(true);
-  };
-
-  const generateFromDate: any = () => {
-    // eslint-disable-next-line
-    return selectedDayRange.from
-      ? // eslint-disable-next-line
-      selectedDayRange.from.year +
-      '/' +
-      selectedDayRange.from.month +
-      '/' +
-      selectedDayRange.from.day
-      : '';
-  };
-
-  const generateToDate: any = () => {
-    // eslint-disable-next-line
-    return selectedDayRange.to
-      ? // eslint-disable-next-line
-      selectedDayRange.to.year + '/' + selectedDayRange.to.month + '/' + selectedDayRange.to.day
-      : '';
-  };
-
-  const clearSelectedDayRange = (e: any) => {
-    e.stopPropagation();
-    setSelectedDayRange({
-      from: null,
-      to: null,
-      clear : true
-    });
-  };
-
   // eslint-disable-next-line
 
   // const [counts, setCounts] = useState<any>({
@@ -165,8 +108,8 @@ const OverviewOfVaccinationInPublicTransport: React.FC<{}> = () => {
       });
 
       setDataset([...normalizedData]);
-      setOrgDataset([...normalizedData]);
-      setFilterType({name: 'کمترین', enName: 'LOWEST'});
+      // setOrgDataset([...normalizedData]);
+      // setFilterType({name: 'کمترین', enName: 'LOWEST'});
     } catch (e: any) {
       console.log(e);
     } finally {
@@ -174,9 +117,9 @@ const OverviewOfVaccinationInPublicTransport: React.FC<{}> = () => {
     }
   };
 
-  useEffect(()=>{
+  useEffect(() => {
     getNumberOf();
-  },[]);
+  }, []);
 
   useEffect(() => {
     getOverviewByVaccine(query);
@@ -184,28 +127,6 @@ const OverviewOfVaccinationInPublicTransport: React.FC<{}> = () => {
       source.cancel('Operation canceled by the user.');
     };
   }, [query]);
-
-  useEffect(() => {
-    if (selectedDayRange.from && selectedDayRange.to) {
-      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-      // const m = moment(finalFromDate, 'jYYYY/jM/jD'); // Parse a Jalaali date
-      // console.log(moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-M-DTHH:mm:ss'));
-      setQuery({
-        ...query,
-        from: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        to: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-      });
-    }
-    if (selectedDayRange.clear) {
-      setQuery({
-        ...query,
-        from: null,
-        to: null,
-      });
-    }
-  }, [selectedDayRange]);
-
 
   // const [reportsDose, setReportsDose] = useState({}) as any;
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
@@ -404,26 +325,26 @@ const OverviewOfVaccinationInPublicTransport: React.FC<{}> = () => {
   //   };
   // }, []);
 
-  useEffect(() => {
-    const tmp = [...orgDataset].sort((a: any, b: any) => {
-      // eslint-disable-next-line
-      const reverse =
-        // eslint-disable-next-line no-nested-ternary
-        filterType.enName === 'HIGHEST' ? 1 : filterType.enName === 'LOWEST' ? -1 : 1;
-
-      if (a.allDoses < b.allDoses) {
-        return reverse * 1;
-      }
-
-      if (a.allDoses > b.allDoses) {
-        return reverse * -1;
-      }
-      // a must be equal to b
-      return 0;
-    });
-
-    setDataset(tmp);
-  }, [filterType]);
+  // useEffect(() => {
+  //   const tmp = [...orgDataset].sort((a: any, b: any) => {
+  //     // eslint-disable-next-line
+  //     const reverse =
+  //       // eslint-disable-next-line no-nested-ternary
+  //       filterType.enName === 'HIGHEST' ? 1 : filterType.enName === 'LOWEST' ? -1 : 1;
+  //
+  //     if (a.allDoses < b.allDoses) {
+  //       return reverse * 1;
+  //     }
+  //
+  //     if (a.allDoses > b.allDoses) {
+  //       return reverse * -1;
+  //     }
+  //     // a must be equal to b
+  //     return 0;
+  //   });
+  //
+  //   setDataset(tmp);
+  // }, [filterType]);
 
   return (
     <fieldset className="text-center border rounded-xl p-4 mb-16">
@@ -571,83 +492,7 @@ const OverviewOfVaccinationInPublicTransport: React.FC<{}> = () => {
       </div>
 
       <div className="flex align-center justify-start space-x-5 rtl:space-x-reverse mb-8">
-        <div className="flex align-center justify-between">
-          {showDatePicker ? (
-            <DatePickerModal
-              setSelectedDayRange={setSelectedDayRange}
-              selectedDayRange={selectedDayRange}
-              setShowDatePicker={setShowDatePicker}
-              showDatePicker
-            />
-          ) : null}
-          <div className="relative z-20 inline-block text-left shadow-custom rounded-lg px-4 py-1">
-            <div
-              className="inline-flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
-              onClick={focusFromDate}
-            >
-              {selectedDayRange.from && (
-                <span className="ml-4 whitespace-nowrap truncate text-xs">
-                      {toPersianDigit(generateFromDate())}
-                    </span>
-              )}
-              {selectedDayRange.to || selectedDayRange.from ? (
-                <button type="button" onClick={clearSelectedDayRange}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={1}
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    />
-                  </svg>
-                </button>
-              ) : (
-                <img src={calendar} alt="x" className="w-5 h-5"/>
-              )}
-            </div>
-          </div>
-          <div className="flex items-center justify-start mx-4">
-            <span className="dash-separator"/>
-          </div>
-          <div className=" shadow-custom rounded-lg px-4 py-1">
-            <div
-              className="flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 cursor-pointer"
-              onClick={focusFromDate}
-            >
-              {selectedDayRange.to && (
-                <span className="ml-4 whitespace-nowrap truncate text-xs">
-                      {toPersianDigit(generateToDate())}
-                    </span>
-              )}
-              {selectedDayRange.to || selectedDayRange.from ? (
-                <button type="button" onClick={clearSelectedDayRange}>
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    className="h-5 w-5 text-gray-400"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                    stroke="currentColor"
-                  >
-                    <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth={1}
-                      d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16"
-                    />
-                  </svg>
-                </button>
-              ) : (
-                <img src={calendar} alt="x" className="w-5 h-5"/>
-              )}
-            </div>
-          </div>
-        </div>
+        <DatepickerQuery query={query} setQuery={setQuery}/>
       </div>
 
       {datasetLoading ? (
