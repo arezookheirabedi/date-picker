f96dfa7822ac2ca6b51d4127dd6929b4c24b02a1
z.khayrabadi <a_kheirabadi92@gmail.com>
2022-06-27 14:45:37 +0430

    add model to hook vaccine and test result

--- src/hooks/apis/useGetNumberOf.ts
+++ src/hooks/apis/useGetNumberOf.ts
@@ -1,39 +1,52 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
-import vaccineService from "../../services/vaccine.service";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import vaccineService from '../../services/vaccine.service';
 
-const initialDoses = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, null: 0};
-const initialNumberOf = {
+export const initialDoses = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, null: 0};
+export const initialVacinatelInfo = {
   doses: {...initialDoses},
   dosesToTotalPopulationPercentage: {...initialDoses},
   gtDoses: {...initialDoses},
-  gtDosesToTotalDosesPercentage: {...initialDoses},
+  totalPopulation: 0,
   totalNonVaccinesCount: 0,
   totalNonVaccinesCountToTotalPopulationPercentage: 0,
-  totalPopulation: 0,
-  totalVaccinesCount: 0,
   totalVaccinesCountToTotalPopulationPercentage: 0,
-};
 
-export default function useGetNumberOf(query : any) {
+  totalNonVaccinesCountBeforeStartOfSystem: 0,
+  totalVaccinesCountAfterStartOfSystem: 0,
+  totalVaccinesCount: 0,
+};
+export interface IInitialVacinatelInfo {
+  totalPopulation: number;
+  totalNonVaccinesCount: number;
+  totalNonVaccinesCountToTotalPopulationPercentage: number;
+  totalVaccinesCountToTotalPopulationPercentage: number;
+  gtDoses: any;
+  doses: any;
+  dosesToTotalPopulationPercentage: any;
+  totalNonVaccinesCountBeforeStartOfSystem: number;
+  totalVaccinesCountAfterStartOfSystem: number;
+  totalVaccinesCount: number;
+}
+
+export default function useGetNumberOf(query: any) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
-  const [data, setData] = useState<any>(initialNumberOf);
+  const [data, setData] = useState<IInitialVacinatelInfo>(initialVacinatelInfo);
 
   const {CancelToken} = axios;
   const source = CancelToken.source();
 
-  const getNumberOf = async (params : any) => {
+  const getNumberOf = async (params: any) => {
     setLoading(true);
     setError(false);
     try {
-      const {data: result} = await vaccineService.membersGeneral(
-        params,
-        {cancelToken: source.token}
-      );
+      const {data: result} = await vaccineService.membersGeneral(params, {
+        cancelToken: source.token,
+      });
       setData({...result});
     } catch (err: any) {
-      setError(err.message || '')
+      setError(err.message || '');
     } finally {
       setLoading(false);
     }
@@ -42,7 +55,7 @@ export default function useGetNumberOf(query : any) {
   useEffect(() => {
     getNumberOf(query);
     return () => {
-      setData(initialNumberOf);
+      setData(initialVacinatelInfo);
       source.cancel('Operation canceled by the user.');
     };
   }, []);
