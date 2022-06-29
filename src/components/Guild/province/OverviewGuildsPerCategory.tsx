3b6f1f96163174f5ff355fad0dc7c7b5cfc189ec
z.khayrabadi <a_kheirabadi92@gmail.com>
2022-06-29 16:08:07 +0430

    overview per category status

--- src/components/Guild/province/OverviewGuildsPerCategory.tsx
+++ src/components/Guild/province/OverviewGuildsPerCategory.tsx
@@ -1,134 +1,28 @@
-import React, {useEffect, useState} from 'react';
-
-// @ts-ignore
-import moment from 'moment-jalaali';
-// import Spinner from 'src/components/Spinner';
-import {cancelTokenSource, msgRequestCanceled, sideCities} from 'src/helpers/utils';
-import {useHistory, useLocation} from 'react-router-dom';
-import guildService from 'src/services/guild.service';
-import Calendar from 'src/components/Calendar';
-import DatePickerModal from '../../DatePickerModal';
-import Table from '../../TableScopeSort';
+import React, {useState} from 'react';
+import LocalTableSearch from 'src/components/LocalTableSearch';
+import DatepickerQuery from 'src/components/DatepickerQuery';
+import useGetOverviewOfCategories from 'src/hooks/apis/useGetOverviewOfCategories';
 import CategoryDonut from '../../../containers/Guild/components/CategoryDonut';
+import Table from '../../TableScopeSort';
 
 interface IProps {
   cityTitle: string;
 }
 const OverviewGuildsPerCategory: React.FC<IProps> = ({cityTitle}) => {
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [loading, setLoading] = useState(false);
-  const [searchQuery, setSearchQuery] = useState('');
-  const [dataset, setDataset] = useState<any>([]);
-  // eslint-disable-next-line
-  const [orgDataset, setOrgDataset] = useState<any>([]);
-  const [selectedDayRange, setSelectedDayRange] = useState({
+  const [query, setQuery] = useState({
     from: null,
     to: null,
-    clear: false,
-  }) as any;
-  const [queryParams, setQueryParams] = useState({
-    from: null,
-    to: null,
-    tags: [],
+    tag: 'guild',
+    category: 'categoryDesc',
   });
-
-  const location = useLocation();
-  const history = useHistory();
-
-  const cancelToken = cancelTokenSource();
-
-  function cancelRequest() {
-    cancelToken.cancel(msgRequestCanceled);
-  }
-
-  // eslint-disable-next-line
-  async function getOverviewByCategory(params: any) {
-    setLoading(true);
-    try {
-      const {data} = await guildService.guildOverviewByCategory(params, {
-        cancelToken: cancelToken.token,
-      });
-
-      const normalizedData: any[] = [];
-      data.forEach((item: any, index: number) => {
-        normalizedData.push({
-          id: `ovca_${index}`,
-          name: item.categoryValue || 'نامشخص',
-          employeesCount: item.membersCount || 0,
-          infectedCount: item.positiveMembersCount || 0,
-          infectedPercent: item.positiveMembersCountToMembersCountPercentage || 0,
-          saveCount: item.recoveredMembersCount || 0,
-          deadCount: 0,
-        });
-      });
-
-      setDataset([...normalizedData]);
-      setOrgDataset([...normalizedData]);
-    } catch (error) {
-      // eslint-disable-next-line
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  }
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const provinceName = params.get('provinceName') || ('تهران' as any);
-    const existsCity = sideCities.some((item: any) => {
-      return item.name === provinceName;
-    });
-    if (existsCity) {
-      getOverviewByCategory({
-        ...queryParams,
-        tag: 'guild',
-        category: 'categoryDesc',
-        province: provinceName,
-      });
-    } else {
-      history.push('/dashboard/guild/province');
-    }
-    return () => {
-      cancelRequest();
-      setDataset([]);
-      setOrgDataset([]);
-    };
-  }, [queryParams, location.search]);
-  const focusFromDate = () => {
-    setShowDatePicker(true);
-  };
-
-  useEffect(() => {
-    if (selectedDayRange.from && selectedDayRange.to) {
-      setSearchQuery('');
-      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-      setQueryParams({
-        ...queryParams,
-        from: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        to: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-      });
-    }
-    if (selectedDayRange.clear) {
-      setQueryParams({
-        ...queryParams,
-        from: null,
-        to: null,
-      });
-    }
-  }, [selectedDayRange]);
-
-  function handleSearch(e: any) {
-    const {value} = e.target;
-
-    let tmp = [...orgDataset];
-    if (value) {
-      tmp = [...tmp].filter(x => x.name.indexOf(value) !== -1);
-    }
-
-    setDataset([...tmp]);
-    setSearchQuery(value);
-  }
+  const {
+    data: dataset,
+    loading,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    error,
+    setData,
+    orgDataset,
+  } = useGetOverviewOfCategories(query, true);
 
   return (
     <fieldset className="mb-16 rounded-xl border p-4 text-center">
@@ -139,50 +33,21 @@ const OverviewGuildsPerCategory: React.FC<IProps> = ({cityTitle}) => {
       <div className="align-center justify-spacebetween mb-8 flex space-x-5 rtl:space-x-reverse">
         <div className="align-center flex space-x-5 rtl:space-x-reverse">
           <div className="flex items-center">
-            {showDatePicker ? (
-              <DatePickerModal
-                setSelectedDayRange={setSelectedDayRange}
-                selectedDayRange={selectedDayRange}
-                setShowDatePicker={setShowDatePicker}
-                showDatePicker
-              />
-            ) : null}
-            <Calendar
-              action={focusFromDate}
-              from={selectedDayRange.from}
-              to={selectedDayRange.to}
-              setSelectedDayRange={setSelectedDayRange}
-            />
+            <DatepickerQuery query={query} setQuery={setQuery} />
           </div>
         </div>
 
         <div className="align-center flex flex-grow justify-end">
           <div className="align-center relative inline-flex leading-3">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="absolute top-1/2 right-4 h-4 w-4 -translate-y-1/2 transform"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth={2}
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-            <input
-              type="text"
+            <LocalTableSearch
+              orgDataset={orgDataset}
+              setData={setData}
+              query={query}
               placeholder="جستجوی واحد صنفی"
-              className="focus:outline-none rounded-lg border border-gray-300 py-2 px-4 pr-10 text-sm"
-              onChange={handleSearch}
-              value={searchQuery}
             />
           </div>
         </div>
       </div>
-
       <div className="align-center flex w-full flex-col justify-center rounded-xl bg-white p-4 shadow">
         <Table
           loading={loading}
