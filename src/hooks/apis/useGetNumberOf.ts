aa35151862d138320965a608a4632a19ab9eb93d
z.khayrabadi <a_kheirabadi92@gmail.com>
2022-06-29 12:16:35 +0430

    refactor vaccines cart

--- src/hooks/apis/useGetNumberOf.ts
+++ src/hooks/apis/useGetNumberOf.ts
@@ -1,6 +1,6 @@
-import {useEffect, useState} from "react";
-import axios from "axios";
-import hcsService from "../../services/hcs.service";
+import {useEffect, useState} from 'react';
+import axios from 'axios';
+import hcsService from '../../services/hcs.service';
 
 export const initialDoses = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, null: 0};
 export const initialVacinatelInfo = {
@@ -11,25 +11,31 @@ export const initialVacinatelInfo = {
   totalNonVaccinesCount: 0,
   totalNonVaccinesCountToTotalPopulationPercentage: 0,
   totalVaccinesCountToTotalPopulationPercentage: 0,
-
+  totalNonVaccinesCountBeforeStartOfSystemToTotalPopulationPercentage: 0,
+  totalVaccinesCountAfterStartOfSystemToTotalPopulationPercentage: 0,
   totalNonVaccinesCountBeforeStartOfSystem: 0,
   totalVaccinesCountAfterStartOfSystem: 0,
   totalVaccinesCount: 0,
 };
+export interface IObjectOption {
+  [key: number]: number;
+}
 export interface IInitialVacinatelInfo {
   totalPopulation: number;
   totalNonVaccinesCount: number;
   totalNonVaccinesCountToTotalPopulationPercentage: number;
   totalVaccinesCountToTotalPopulationPercentage: number;
-  gtDoses: any;
-  doses: any;
-  dosesToTotalPopulationPercentage: any;
+  gtDoses: IObjectOption;
+  doses: IObjectOption;
+  dosesToTotalPopulationPercentage: IObjectOption;
   totalNonVaccinesCountBeforeStartOfSystem: number;
   totalVaccinesCountAfterStartOfSystem: number;
   totalVaccinesCount: number;
+  totalNonVaccinesCountBeforeStartOfSystemToTotalPopulationPercentage: number;
+  totalVaccinesCountAfterStartOfSystemToTotalPopulationPercentage: number;
 }
 
-export default function useGetNumberOf(query: any) {
+export default function useGetNumberOf(query?: any) {
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
   const [data, setData] = useState<IInitialVacinatelInfo>(initialVacinatelInfo);
@@ -37,14 +43,11 @@ export default function useGetNumberOf(query: any) {
   const {CancelToken} = axios;
   const source = CancelToken.source();
 
-  const getNumberOf = async (params: any) => {
+  const getNumberOf = async (params: any = {}) => {
     setLoading(true);
     setError(false);
     try {
-      const {data: result} = await hcsService.numberOf(
-        params,
-        {cancelToken: source.token}
-      );
+      const {data: result} = await hcsService.numberOf(params, {cancelToken: source.token});
       setData({...result});
     } catch (err: any) {
       setError(err.message || '');
