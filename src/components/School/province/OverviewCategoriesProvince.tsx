1cc130b8d65eb4bf3610cc36f2e1f1be13760587
z.khayrabadi <a_kheirabadi92@gmail.com>
2022-07-02 16:49:35 +0430

    refactor school province

--- src/components/School/province/OverviewCategoriesProvince.tsx
+++ src/components/School/province/OverviewCategoriesProvince.tsx
@@ -1,147 +1,41 @@
-import React, {useEffect, useState} from 'react';
-import axios from 'axios';
-
-// @ts-ignore
-import moment from 'moment-jalaali';
-import {useHistory, useLocation} from 'react-router-dom';
+import React, {useState} from 'react';
 import Table from 'src/components/TableScopeSort';
-import DatePickerModal from '../../DatePickerModal';
+import useGetOverviewOfCategories from 'src/hooks/apis/useGetOverviewOfCategories';
+import DatepickerQuery from 'src/components/DatepickerQuery';
 import CategoryDonut from '../../../containers/Guild/components/CategoryDonut';
-import {sideCities} from '../../../helpers/utils';
-
-import Calendar from '../../Calendar';
-import hcsService from '../../../services/hcs.service';
 
 interface OverviewCategoriesProvinceProps {
   cityTitle?: any;
 }
 
 const OverviewCategoriesProvince: React.FC<OverviewCategoriesProvinceProps> = ({cityTitle}) => {
-  const location = useLocation();
-  const history = useHistory();
-  const [loading, setLoading] = useState(false);
-  // const [isCancel, setIsCancel] = useState(false);
-  const [dataset, setDataset] = useState<any>([]);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  // eslint-disable-next-line
-  const [selectedDayRange, setSelectedDayRange] = useState({
+  const [query, setQuery] = useState({
     from: null,
     to: null,
-  }) as any;
-  const [query, setQuery] = useState({
-    resultReceiptDateFrom: null,
-    resultReceiptDateTo: null,
-    province: null,
-  }) as any;
-
-  const {CancelToken} = axios;
-  const source = CancelToken.source();
-
-  const overviewTestResults = async (from: any = null, to: any = null, province: any) => {
-    try {
-      setLoading(true);
-      const {data} = await hcsService.tableOverviewTestResults('edu', 'grade', {
-        lang: 'fa',
-        from,
-        to,
-        province,
-      });
-      const normalizedData: any[] = [];
-      data.forEach((item: any, index: number) => {
-        // if (item.total !== 0) {
-        normalizedData.push({
-          id: `ovca_${index}`,
-          name: item.categoryValue,
-          employeesCount: item.membersCount || 0,
-          infectedCount: item.positiveMembersCount || 0,
-          infectedPercent: item.positiveMembersCountToMembersCountPercentage || 0,
-          saveCount: item.recoveredMembersCount || 0,
-          // deadCount: 120,
-        });
-        // }
-      });
-      setDataset([...normalizedData]);
-    } catch (e) {
-      console.log(e);
-    } finally {
-      setLoading(false);
-    }
-  };
-  const focusFromDate = () => {
-    setShowDatePicker(true);
-  };
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const provinceName = params.get('provinceName') || ('تهران' as any);
-    const existsCity = sideCities.some((item: any) => {
-      return item.name === provinceName;
-    });
-    if (existsCity) {
-      overviewTestResults(query.resultReceiptDateFrom, query.resultReceiptDateTo, provinceName);
-      // getOverviewByCategory({
-      //   resultStatus: 'POSITIVE',
-      //   recoveredCount: true,
-      //   total: true,
-      //   count: true,
-      //   province: provinceName,
-      // });
-      //
-    } else {
-      history.push('/dashboard/school/province');
-    }
-    return () => {
-      setDataset([]);
-      source.cancel('Operation canceled by the user.');
-    };
-  }, [location.search, query]);
-
-  useEffect(() => {
-    if (selectedDayRange.from && selectedDayRange.to) {
-      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-      // const m = moment(finalFromDate, 'jYYYY/jM/jD'); // Parse a Jalaali date
-      // console.log(moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-M-DTHH:mm:ss'));
-      setQuery({
-        ...query,
-        resultReceiptDateFrom: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        resultReceiptDateTo: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-      });
-    }
-    if (selectedDayRange.clear) {
-      setQuery({
-        ...query,
-        resultReceiptDateFrom: null,
-        resultReceiptDateTo: null,
-      });
-    }
-  }, [selectedDayRange]);
+    tag: 'edu',
+    category: 'grade',
+  });
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const {
+    data: dataset,
+    loading,
+    // eslint-disable-next-line @typescript-eslint/no-unused-vars
+    error,
+  } = useGetOverviewOfCategories(query, true);
 
   return (
-    <fieldset className="text-center border rounded-xl p-4 mb-16">
-      <legend className="text-black mx-auto px-3">
+    <fieldset className="mb-16 rounded-xl border p-4 text-center">
+      <legend className="mx-auto px-3 text-black">
         نگاه کلی به آموزش و پرورش در استان &nbsp;
         {cityTitle}
       </legend>
-      <div className="flex flex-grow items-center justify-start space-x-5 rtl:space-x-reverse mb-8">
-        <div className="flex align-center justify-start">
-          {showDatePicker ? (
-            <DatePickerModal
-              setSelectedDayRange={setSelectedDayRange}
-              selectedDayRange={selectedDayRange}
-              setShowDatePicker={setShowDatePicker}
-              showDatePicker
-            />
-          ) : null}
-          <Calendar
-            action={focusFromDate}
-            from={selectedDayRange.from}
-            to={selectedDayRange.to}
-            setSelectedDayRange={setSelectedDayRange}
-          />
+
+      <div className="mb-8 flex flex-grow items-center justify-start space-x-5 rtl:space-x-reverse">
+        <div className="align-center flex justify-start">
+          <DatepickerQuery query={query} setQuery={setQuery} />
         </div>
       </div>
-      <div className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
+      <div className="align-center flex w-full flex-col justify-center rounded-xl bg-white p-4 shadow">
         <Table
           loading={loading}
           dataSet={[...dataset]}
--- src/components/School/province/OverviewPatientsProvince.tsx
+++ src/components/School/province/OverviewPatientsProvince.tsx
@@ -1,17 +1,10 @@
-import React, {useEffect, useState} from 'react';
-import {useHistory, useLocation} from 'react-router-dom';
-// @ts-ignore
-import moment from 'moment-jalaali';
+import React, {useState} from 'react';
 import SearchableSingleSelect from 'src/components/SearchableSingleSelect';
+import useGetOverviewOfPatients from 'src/hooks/apis/useGetOverviewOfPatients';
+import DatepickerQuery from 'src/components/DatepickerQuery';
 import RangeDateSliderFilter from '../../RangeDateSliderFilter';
-import DatePickerModal from '../../DatePickerModal';
-// import calendar from '../../../assets/images/icons/calendar.svg';
-// import RangeDateSliderFilter from '../../RangeDateSliderFilter';
 import Charts from '../../Charts';
-import {cancelTokenSource, msgRequestCanceled, sideCities} from '../../../helpers/utils';
 import Spinner from '../../Spinner';
-import Calendar from '../../Calendar';
-import hcsService from '../../../services/hcs.service';
 
 const {Line} = Charts;
 
@@ -20,114 +13,21 @@ interface OverviewPatientsProvinceProps {
 }
 
 const OverviewPatientsProvince: React.FC<OverviewPatientsProvinceProps> = ({cityTitle}) => {
-  const [data, setData] = useState([]);
-  // eslint-disable-next-line
-  const [provinceTitle, setProvinceTitle] = useState(null);
-  const [showDatePicker, setShowDatePicker] = useState(false);
-  const [errorMessage, setErrorMessage] = useState(null);
-  // eslint-disable-next-line
-  const [loading, setLoading] = useState(false);
   // eslint-disable-next-line
   const [selectedDayRange, setSelectedDayRange] = useState({
     from: null,
     to: null,
+    clear: false,
   }) as any;
-
   const [query, setQuery] = useState({
     timeBoxType: 'DAILY',
     from: null,
     to: null,
-    category: 'grade',
     categoryValue: null,
     tag: 'edu',
+    category: 'grade',
   });
-
-  const location = useLocation();
-  const history = useHistory();
-
-  const cancelToken = cancelTokenSource();
-
-  function cancelRequest() {
-    cancelToken.cancel(msgRequestCanceled);
-  }
-
-  const focusFromDate = () => {
-    setShowDatePicker(true);
-  };
-
-  const getColumnChartTestResult = async (params: any, province: any) => {
-    setLoading(true);
-    setErrorMessage(null);
-    try {
-      const response = await hcsService.columnChartTestResultService(
-        {...params, province},
-        {
-          cancelToken: cancelToken.token,
-        }
-      );
-      setData(response.data);
-    } catch (error: any) {
-      setErrorMessage(error.message);
-      // eslint-disable-next-line
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  useEffect(() => {
-    const params = new URLSearchParams(location.search);
-    const provinceName = params.get('provinceName') || ('تهران' as any);
-
-    const existsCity = sideCities.some((item: any) => {
-      return item.name === provinceName;
-    });
-
-    let idSetTimeOut: any;
-    if (existsCity) {
-      setProvinceTitle(provinceName);
-      idSetTimeOut = setTimeout(() => {
-        getColumnChartTestResult(query, provinceName);
-      }, 500);
-    } else {
-      history.push('/dashboard/school/province');
-    }
-
-    return () => {
-      if (existsCity) {
-        cancelRequest();
-        clearTimeout(idSetTimeOut);
-      }
-    };
-  }, [query, location.search]);
-
-  useEffect(() => {
-    return () => {
-      setData([]);
-    };
-  }, [history]);
-
-  useEffect(() => {
-    if (selectedDayRange.from && selectedDayRange.to) {
-      const finalFromDate = `${selectedDayRange.from.year}/${selectedDayRange.from.month}/${selectedDayRange.from.day}`;
-      const finalToDate = `${selectedDayRange.to.year}/${selectedDayRange.to.month}/${selectedDayRange.to.day}`;
-      // const m = moment(finalFromDate, 'jYYYY/jM/jD'); // Parse a Jalaali date
-      // console.log(moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-M-DTHH:mm:ss'));
-      setQuery({
-        ...query,
-        from: moment(finalFromDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-        to: moment(finalToDate, 'jYYYY/jM/jD').format('YYYY-MM-DD'),
-      });
-    }
-    if (selectedDayRange.clear) {
-      setQuery({
-        ...query,
-        from: null,
-        to: null,
-      });
-    }
-  }, [selectedDayRange]);
-
+  const {data, loading, error: errorMessage} = useGetOverviewOfPatients(query, true);
   return (
     <fieldset className="mb-16 rounded-xl border p-4 text-center">
       <legend className="mx-auto px-3 text-black">
@@ -146,20 +46,7 @@ const OverviewPatientsProvince: React.FC<OverviewPatientsProvinceProps> = ({city
               queryParams={query}
             />
             <div className="align-center mr-8 flex justify-between">
-              {showDatePicker ? (
-                <DatePickerModal
-                  setSelectedDayRange={setSelectedDayRange}
-                  selectedDayRange={selectedDayRange}
-                  setShowDatePicker={setShowDatePicker}
-                  showDatePicker
-                />
-              ) : null}
-              <Calendar
-                action={focusFromDate}
-                from={selectedDayRange.from}
-                to={selectedDayRange.to}
-                setSelectedDayRange={setSelectedDayRange}
-              />
+              <DatepickerQuery query={query} setQuery={setQuery} />
             </div>
           </div>
 
