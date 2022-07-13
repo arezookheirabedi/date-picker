1093a76c446a5ad93000b579aaacad9eac71c67e
z.khayrabadi <a_kheirabadi92@gmail.com>
2022-07-13 16:24:55 +0430

    update transport monitoring

--- src/components/Transport/monitoring/OverviewDriversStatus.tsx
+++ src/components/Transport/monitoring/OverviewDriversStatus.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
 import transportService from 'src/services/transport.service';
 import dayjs from 'dayjs';
+import RetryButton from 'src/components/RetryButton';
 import Table from '../../TableXHR';
 import {
   toPersianDigit,
@@ -8,7 +9,6 @@ import {
   cancelTokenSource,
   msgRequestCanceled,
 } from '../../../helpers/utils';
-import Spinner from '../../Spinner';
 import ExportButton from './ExportButton';
 import {getHealthStatusTextAndColor, getStatusColor, getVaccinesStatusText} from './constant';
 
@@ -23,18 +23,27 @@ const OverviewDriverStatus: React.FC<OverviewDriverStatusProps> = ({cityTitle}) 
   // eslint-disable-next-line
   const [errorMessage, setErrorMessage] = useState(null);
   const [dataSet, setDataSet] = useState<any[]>([]);
-  const [currentPage, setCurrentPage] = useState(1);
-
+  // const [currentPage, setCurrentPage] = useState(1);
+  const [query, setQuery] = useState({
+    // pageNumber: Number(currentPage) - 1,
+    currentPage: 1,
+    pageSize,
+    sort: 'ASC',
+    reportType: 'GENERAL',
+    province: cityTitle,
+    healthStatusSet: [].join(','),
+    retry: false,
+  });
   const cancelToken = cancelTokenSource();
-
   function cancelRequest() {
     cancelToken.cancel(msgRequestCanceled);
   }
-  const getOverviewTransportReport = async (params: any) => {
+  const getOverviewTransportReport = async ({retry, currentPage, ...params}: any = {}) => {
+    const newData = {...params, pageNumber: Number(query.currentPage) - 1};
     setErrorMessage(null);
     setLoading(true);
     try {
-      const {data}: any = await transportService.overviewReport(params, {
+      const {data}: any = await transportService.overviewReport(newData, {
         cancelToken: cancelToken.token,
       });
 
@@ -64,26 +73,17 @@ const OverviewDriverStatus: React.FC<OverviewDriverStatusProps> = ({cityTitle}) 
   };
 
   useEffect(() => {
-    const province = cityTitle || null;
-    const params: any = {
-      pageNumber: Number(currentPage) - 1,
-      pageSize,
-      sort: 'ASC',
-      reportType: 'GENERAL',
-      province,
-      healthStatusSet: [].join(','),
-    };
-    getOverviewTransportReport(params);
+    getOverviewTransportReport({...query, province: cityTitle});
     return () => {
       cancelRequest();
       setDataSet([]);
     };
-  }, [cityTitle, currentPage]);
+  }, [query]);
   useEffect(() => {
-    setCurrentPage(1);
+    setQuery({...query, currentPage: 1, province: cityTitle});
   }, [cityTitle]);
   function handlePageChange(page: number = 1) {
-    setCurrentPage(page);
+    setQuery({...query, currentPage: page});
   }
   return (
     <fieldset className="mb-16 rounded-xl border p-4 text-center" id="drivers-overview">
@@ -106,99 +106,99 @@ const OverviewDriverStatus: React.FC<OverviewDriverStatusProps> = ({cityTitle}) 
         </div>
       </div>
 
-      {loading && dataSet.length === 0 ? (
-        <div className="p-20">
-          <Spinner />
-        </div>
-      ) : (
-        <>
-          <div className="flex w-full flex-col items-center justify-center rounded-xl bg-white p-4 shadow">
-            <Table
-              handlePageChange={handlePageChange}
-              dataSet={[...dataSet]}
-              pagination={{pageSize, currentPage}}
-              totalItems={totalItems}
-              columns={[
-                {
-                  name: 'رسته',
-                  key: 'serviceType',
-                  render: (v: any, record, index: number) => (
-                    <span className="flex w-full justify-start">
-                      {`${toPersianDigit(
-                        ((currentPage - 1) * pageSize + (index + 1)).toString()
-                      )}. ${getServiceTypeName(v)}`}
-                    </span>
-                  ),
-                },
-                {
-                  name: 'پلاک',
-                  key: 'plaque',
-                  render: (v: any, record: any) => (
-                    <span className="">{toPersianDigit(record.plaque)}</span>
-                  ),
-                },
-                {
-                  name: 'کدملی راننده',
-                  key: 'nationalId',
-                  render: (v: any, record: any) => (
-                    <span className="text-gray-500">{toPersianDigit(record.nationalId)}</span>
-                  ),
-                },
-                {
-                  name: 'استان',
-                  key: 'province',
-                  render: (v: any) => <span>{v || '-'}</span>,
-                },
-                {
-                  name: 'وضعیت',
-                  key: 'personHealthStatus',
-                  render: (v: string, record: any) => {
-                    const {colors} = getStatusColor(record.personHealthStatus);
-
-                    return (
-                      <div className="flex justify-center">
-                        <div
-                          className={`bg-gradient-to-l ${colors} h-4 w-4 rounded-full shadow-2xl`}
-                          style={{boxShadow: '-3px 4px 8px -3px rgba(0,0,0,.5)'}}
-                        />
-                      </div>
-                    );
-                  },
-                },
-
-                {
-                  name: 'تاریخ ابتلا',
-                  key: 'date',
-                  render: (v: any) => (
-                    <span className="text-gray-500">
-                      {v ? toPersianDigit(dayjs(v).calendar('jalali').format('YYYY/MM/DD')) : '-'}
-                    </span>
-                  ),
-                },
-
-                {
-                  name: 'آزمایش',
-                  key: 'personHealthStatus',
-                  render: (v: any, record: any) => {
-                    const {colors, text} = getHealthStatusTextAndColor(record.personHealthStatus);
-                    return <span className={`${colors}`}>{text} </span>;
-                  },
-                },
-                {
-                  name: 'واکسیناسیون',
-                  key: 'numberOfReceivedDoses',
-
-                  render: (v: any, record: any) => {
-                    const {text} = getVaccinesStatusText(record.numberOfReceivedDoses);
-
-                    return <span>{text} </span>;
-                  },
-                },
-              ]}
-            />
+      <div className="flex w-full flex-col items-center justify-center rounded-xl bg-white p-4 shadow">
+        {errorMessage && !loading ? (
+          <div className="p-40">
+            <div className="text-red-500">{errorMessage}</div>
+            <RetryButton setQuery={setQuery} />
           </div>
-        </>
-      )}
+        ) : (
+          <Table
+            loading={loading}
+            handlePageChange={handlePageChange}
+            dataSet={[...dataSet]}
+            pagination={{pageSize, currentPage: query.currentPage}}
+            totalItems={totalItems}
+            columns={[
+              {
+                name: 'رسته',
+                key: 'serviceType',
+                render: (v: any, record, index: number) => (
+                  <span className="flex w-full justify-start">
+                    {`${toPersianDigit(
+                      ((query.currentPage - 1) * pageSize + (index + 1)).toString()
+                    )}. ${getServiceTypeName(v)}`}
+                  </span>
+                ),
+              },
+              {
+                name: 'پلاک',
+                key: 'plaque',
+                render: (v: any, record: any) => (
+                  <span className="">{toPersianDigit(record.plaque)}</span>
+                ),
+              },
+              {
+                name: 'کدملی راننده',
+                key: 'nationalId',
+                render: (v: any, record: any) => (
+                  <span className="text-gray-500">{toPersianDigit(record.nationalId)}</span>
+                ),
+              },
+              {
+                name: 'استان',
+                key: 'province',
+                render: (v: any) => <span>{v || '-'}</span>,
+              },
+              {
+                name: 'وضعیت',
+                key: 'personHealthStatus',
+                render: (v: string, record: any) => {
+                  const {colors} = getStatusColor(record.personHealthStatus);
+
+                  return (
+                    <div className="flex justify-center">
+                      <div
+                        className={`bg-gradient-to-l ${colors} h-4 w-4 rounded-full shadow-2xl`}
+                        style={{boxShadow: '-3px 4px 8px -3px rgba(0,0,0,.5)'}}
+                      />
+                    </div>
+                  );
+                },
+              },
+
+              {
+                name: 'تاریخ ابتلا',
+                key: 'date',
+                render: (v: any) => (
+                  <span className="text-gray-500">
+                    {v ? toPersianDigit(dayjs(v).calendar('jalali').format('YYYY/MM/DD')) : '-'}
+                  </span>
+                ),
+              },
+
+              {
+                name: 'آزمایش',
+                key: 'personHealthStatus',
+                render: (v: any, record: any) => {
+                  const {colors, text} = getHealthStatusTextAndColor(record.personHealthStatus);
+                  return <span className={`${colors}`}>{text} </span>;
+                },
+              },
+              {
+                name: 'واکسیناسیون',
+                key: 'numberOfReceivedDoses',
+
+                render: (v: any, record: any) => {
+                  const {text} = getVaccinesStatusText(record.numberOfReceivedDoses);
+
+                  return <span>{text} </span>;
+                },
+              },
+            ]}
+          />
+        )}
+      </div>
     </fieldset>
   );
 };
