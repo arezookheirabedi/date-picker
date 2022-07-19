e766ed2a5e92c38b5c340a4d905eb8c63e1f9c39
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-07-19 12:49:12 +0430

    mokeb list

---
+++ src/components/Arborean/public/constant.ts
@@ -0,0 +1,46 @@
+export const mokebList = [
+  {
+    name: 'پایگاه کمیل',
+    location: 'مرز مهران',
+    capacity: '35',
+    type: 'اسکان و تغذیه',
+    owner: 'وزارت کشور',
+    managerName: 'نگین آساده',
+  },
+  {
+    name: 'پایگاه کمیل',
+    location: 'مرز مهران',
+    capacity: '35',
+    type: 'اسکان و تغذیه',
+    owner: 'وزارت کشور',
+    managerName: 'نگین آساده',
+  },
+  {
+    name: 'پایگاه کمیل',
+    location: 'مرز مهران',
+    capacity: '35',
+    type: 'اسکان و تغذیه',
+    owner: 'وزارت کشور',
+    managerName: 'نگین آساده',
+  },
+];
+export const redHelalList = [
+  {
+    name: 'پایگاه کمیل',
+    baseCode: '3456',
+    capacity: '35',
+    providedServicesCount:"5444",
+    location: 'درمانگاه مسیر نجف به کربلا',
+    managerName: 'محمد علی مدنی',
+    
+  },
+
+  {
+    name: 'پایگاه کمیل',
+    baseCode: '3456',
+    capacity: '35',
+    providedServicesCount:"5444",
+    location: 'درمانگاه مسیر نجف به کربلا',
+    managerName: 'محمد علی مدنی',
+  },
+]
