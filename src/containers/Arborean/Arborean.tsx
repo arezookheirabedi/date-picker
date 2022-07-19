b716fb553190d57d6c21cd3990490dec4a3b63b3
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-07-19 11:17:16 +0430

    add mokeb list

--- src/containers/Arborean/Arborean.tsx
+++ src/containers/Arborean/Arborean.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import MokebList from 'src/components/Arborean/public/MokebList';
 import OverviewArborean from 'src/components/Arborean/public/OverviewArborean';
 
 const Arborean = () => {
@@ -6,6 +7,7 @@ const Arborean = () => {
   return (
     <div className="space-y-16 mb-8">
       <OverviewArborean />
+      <MokebList/>
     </div>
   );
 };
