e0a818550742fe6e78e58e08318c06ec8636f7af
emadabbasii <emadabbasee@gmail.com>
2022-07-20 11:11:19 +0430

    add 4 components to public arbaeen container

--- src/containers/Arborean/Arborean.tsx
+++ src/containers/Arborean/Arborean.tsx
@@ -1,11 +1,22 @@
 import React from 'react';
 import OverviewArborean from 'src/components/Arborean/public/OverviewArborean';
+import OverviewOfTheLatestVaccinationStatusOfPilgrims
+  from 'src/components/Arborean/public/OverviewOfTheLatestVaccinationStatusOfPilgrims';
+import OverviewOfTheEntryAndExitOfPilgrims from 'src/components/Arborean/public/OverviewOfTheEntryAndExitOfPilgrims';
+import OverviewOfThePercentageOfEntryAndExitOfPilgrims
+  from 'src/components/Arborean/public/OverviewOfThePercentageOfEntryAndExitOfPilgrims';
+import GeneralLookAtTransportationAxesAndExitBordersOfCountry
+  from 'src/components/Arborean/public/GeneralLookAtTransportationAxesAndExitBordersOfCountry'
 
 const Arborean = () => {
 
   return (
     <div className="space-y-16 mb-8">
-      <OverviewArborean />
+      <OverviewArborean/>
+      <OverviewOfTheLatestVaccinationStatusOfPilgrims/>
+      <OverviewOfTheEntryAndExitOfPilgrims/>
+      <OverviewOfThePercentageOfEntryAndExitOfPilgrims/>
+      <GeneralLookAtTransportationAxesAndExitBordersOfCountry/>
     </div>
   );
 };
