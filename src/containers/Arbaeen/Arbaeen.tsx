fef6e31556e6e8f818fd4789623bef05672fff7f
z.khayrabadi <a_kheirabadi92@gmail.com>
2022-07-23 11:17:53 +0430

    add component to arbaeen

--- src/containers/Arbaeen/Arbaeen.tsx
+++ src/containers/Arbaeen/Arbaeen.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
+import MokebList from 'src/components/Arbaeen/public/MokebList';
+import RedHalalBasesList from 'src/components/Arbaeen/public/RedHalalBasesList';
+import TheLargestNumberOfOriginPilgrimsCitiesList from 'src/components/Arbaeen/public/TheLargestNumberOfOriginPilgrimsCitiesList';
+import TheLargestNumberOfOriginPilgrimsCountriesList from 'src/components/Arbaeen/public/TheLargestNumberOfOriginPilgrimsCountriesList';
 import OverviewArbaeen from '../../components/Arbaeen/public/OverviewArbaeen';
 
 const Arbaeen = () => {
-
   return (
     <div className="space-y-16 mb-8">
       <OverviewArbaeen />
+      <MokebList />
+      <RedHalalBasesList />
+      <TheLargestNumberOfOriginPilgrimsCitiesList />
+      <TheLargestNumberOfOriginPilgrimsCountriesList />
     </div>
   );
 };
