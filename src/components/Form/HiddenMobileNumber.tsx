057bb051f6b89a0550a246333b63abc1ae081d71
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-08-06 18:05:15 +0430

    remove to form

---
+++ src/components/Form/HiddenMobileNumber.tsx
@@ -0,0 +1,31 @@
+import React, {useState} from 'react';
+import ableEye from 'src/assets/images/icons/able-eye-icon.svg';
+import disableEye from 'src/assets/images/icons/disable-eye-icon.svg';
+
+interface IProps {
+  value: any;
+}
+const HiddenMobileNumber: React.FC<IProps> = ({value}) => {
+  const [typeInputText, setTypeInputText] = useState(false);
+
+  return (
+    <>
+      <div className="flex justify-start items-center">
+        <input
+          className="focus:outline-none"
+          value={value}
+          type={`${typeInputText ? 'text' : 'password'}`}
+          readOnly
+        />
+
+        {/* eslint-disable-next-line jsx-a11y/no-noninteractive-element-interactions */}
+        <img
+          src={`${typeInputText ? disableEye : ableEye}`}
+          alt=""
+          onClick={() => setTypeInputText(!typeInputText)}
+        />
+      </div>
+    </>
+  );
+};
+export default HiddenMobileNumber;
