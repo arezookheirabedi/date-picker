d0457369056e69dfaa48fa12c1e49f382c835fe5
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-08-09 14:45:13 +0430

    update ui

--- src/components/UserManagment/User.tsx
+++ src/components/UserManagment/User.tsx
@@ -12,8 +12,138 @@ import SearchableMultiSelect from '../SearchableMultiSelect.tsx';
 import Table from '../TableXHR';
 import CreateButton from './CreateButton';
 import Actions from './TableAction';
+import ConfirmIcon from '../../assets/images/icons/confirm.svg';
+import RejectIcon from '../../assets/images/icons/reject.svg';
+import PendingIcon from '../../assets/images/icons/pending.svg';
+import SimpleSelect from '../Select2/SimpleSelect';
 
 const pageSize = 10;
+const provinceOptions = [
+  {
+    value: 'آذربایجان شرقی',
+    title: 'آذربایجان شرقی',
+  },
+  {
+    value: 'آذربایجان غربی',
+    title: 'آذربایجان غربی',
+  },
+  {
+    value: 'اردبیل',
+    title: 'اردبیل',
+  },
+  {
+    value: 'اصفهان',
+    title: 'اصفهان',
+  },
+  {
+    value: 'البرز',
+    title: 'البرز',
+  },
+  {
+    value: 'ایلام',
+    title: 'ایلام',
+  },
+  {
+    value: 'بوشهر',
+    title: 'بوشهر',
+  },
+  {
+    value: 'تهران',
+    title: 'تهران',
+  },
+  {
+    value: 'خراسان جنوبی',
+    title: 'خراسان جنوبی',
+  },
+  {
+    value: 'خراسان رضوی',
+    title: 'خراسان رضوی',
+  },
+  {
+    value: 'خراسان شمالی',
+    title: 'خراسان شمالی',
+  },
+  {
+    value: 'خوزستان',
+    title: 'خوزستان',
+  },
+  {
+    value: 'زنجان',
+    title: 'زنجان',
+  },
+  {
+    value: 'سمنان',
+    title: 'سمنان',
+  },
+  {
+    value: 'سیستان و بلوچستان',
+    title: 'سیستان و بلوچستان',
+  },
+  {
+    value: 'فارس',
+    title: 'فارس',
+  },
+  {
+    value: 'قزوین',
+    title: 'قزوین',
+  },
+  {
+    value: 'قم',
+    title: 'قم',
+  },
+  {
+    value: 'کردستان',
+    title: 'کردستان',
+  },
+  {
+    value: 'کرمان',
+    title: 'کرمان',
+  },
+  {
+    value: 'کرمانشاه',
+    title: 'کرمانشاه',
+  },
+  {
+    value: 'کهگیلویه وبویراحمد',
+    title: 'کهگیلویه وبویراحمد',
+  },
+  {
+    value: 'گلستان',
+    title: 'گلستان',
+  },
+  {
+    value: 'گیلان',
+    title: 'گیلان',
+  },
+  {
+    value: 'لرستان',
+    title: 'لرستان',
+  },
+  {
+    value: 'مازندران',
+    title: 'مازندران',
+  },
+  {
+    value: 'مرکزی',
+    title: 'مرکزی',
+  },
+  {
+    value: 'هرمزگان',
+    title: 'هرمزگان',
+  },
+  {
+    value: 'همدان',
+    title: 'همدان',
+  },
+  {
+    value: 'یزد',
+    title: 'یزد',
+  },
+  {
+    value: 'چهارمحال و بختیاری',
+    title: 'چهارمحال و بختیاری',
+  },
+];
 
 export default function index() {
   const [loading, setLoading] = useState(false);
@@ -99,14 +229,59 @@ export default function index() {
   function handlePageChange(page: number = 1) {
     setQuery({...query, currentPage: page});
   }
+
+  const statusOption = [
+    {
+      value: 'تایید شده',
+      title: 'تایید شده',
+      icon: <img src={ConfirmIcon} alt="confirm" />,
+    },
+    {
+      value: 'رد شده',
+      title: 'رد شده',
+      icon: <img src={RejectIcon} alt="confirm" />,
+    },
+    {
+      value: 'در انتظار تایید',
+      title: 'در انتظار تایید',
+      icon: <img src={PendingIcon} alt="confirm" />,
+    },
+  ];
   return (
     <>
+      {/* <SearchableMultiSelect endPoint={authenticationService.rolePermision} /> */}
+
       <fieldset className="text-center border rounded-xl p-4 mb-16">
         <legend className="text-black mx-auto px-3">لیست کاربران</legend>
-        <div className="mb-8 flex items-center justify-between">
-          <div className="inline-flex">
-            <CreateButton />
-            <SearchableMultiSelect endPoint={authenticationService.rolePermision} />
+        <div className="mb-10 mt-6 flex items-center justify-between">
+          <div className="align-center flex flex-grow justify-start px-8">
+            <div className="relative inline-flex align-center leading-3 h-10 ml-4">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-4 h-4 absolute top-1/2 transform -translate-y-1/2 right-4"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth={2}
+                  d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+                />
+              </svg>
+              <input
+                type="text"
+                placeholder="جستجوی کدملی، موبایل"
+                className="py-2 px-4 pr-10 text-sm border-none rounded-lg focus:outline-none shadow-custom"
+              />
+
+              <SimpleSelect options={provinceOptions} defaultOption="تهران" />
+              <SimpleSelect options={statusOption} />
+            </div>
+          </div>
+          <div className="w-1/5">
+            <CreateButton actionType="add" />
           </div>
         </div>
         <div className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
