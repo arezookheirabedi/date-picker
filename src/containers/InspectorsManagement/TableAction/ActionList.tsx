c87d73b5a0e1e57c04b7edf0991716554db22cbf
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-08-09 17:26:49 +0430

    update

--- src/containers/InspectorsManagement/TableAction/ActionList.tsx
+++ src/containers/InspectorsManagement/TableAction/ActionList.tsx
@@ -3,7 +3,7 @@ import {EACTIONTABLE} from 'src/constants/acctionTable.enum';
 import Delete from '../../../assets/images/icons/user-delete.svg';
 import Edit from '../../../assets/images/icons/user-edit.svg';
 // import ResetPass from '../../../assets/images/icons/reset-pass.svg';
-import Confirm from '../../../assets/images/icons/inspectoere-confirm.svg';
+// import Confirm from '../../../assets/images/icons/inspectoere-confirm.svg';
 
 export interface IActionList {
   icon: ReactElement;
@@ -13,12 +13,12 @@ export interface IActionList {
 }
 
 export const ActionList: Array<IActionList> = [
-  {
-    icon: <>notaeed</>,
-    changIcon: <img alt="confirm" src={Confirm} className="h-4 w-4" />,
-    title: 'تایید بازرس',
-    type: EACTIONTABLE.CONFIRM_INSPECTOR,
-  },
+  // {
+  //   icon: <>notaeed</>,
+  //   changIcon: <img alt="confirm" src={Confirm} className="h-4 w-4" />,
+  //   title: 'تایید بازرس',
+  //   type: EACTIONTABLE.CONFIRM_INSPECTOR,
+  // },
   {
     icon: <img alt="update" src={Edit} className="h-4 w-4" />,
     title: 'ویرایش اطلاعات',
