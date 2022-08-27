cce984e859101f9582fdea2a9702f67a3b82ee0b
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-08-27 16:54:38 +0430

    add services for vaccines

--- src/components/Arbaeen/public/OverviewPilgrimVaccineStatus.tsx
+++ src/components/Arbaeen/public/OverviewPilgrimVaccineStatus.tsx
@@ -1,11 +1,9 @@
 import React, {useState, useEffect} from 'react';
-// api services
 import axios from 'axios';
-import arbaeenService from '../../../services/arbaeen.service';
-// components
+import sufferingIcon from 'src/assets/images/icons/suffering-color.svg';
+import arbaeenService from 'src/services/arbaeen.service';
+import useGetArbaeenCountData from 'src/hooks/apis/useGetArbaeenCountData';
 import Statistic from '../../../containers/Guild/components/Statistic';
-// images
-
 import totalVacsinateStart from '../../../assets/images/icons/total-vaccinate-start-work-panel.svg';
 import personGrayVaccine from '../../../assets/images/icons/none-vaccinate-start-wok-panel.svg';
 import YellowVaccine from '../../../assets/images/icons/big-yellow-vaccine.svg';
@@ -16,39 +14,34 @@ import NavyVaccine from '../../../assets/images/icons/navy-vaccine-lg.svg';
 import redVaccine from '../../../assets/images/icons/red-vaccine.svg';
 
 const initialValue = {
-  numberOfVaccinatedPilgrims: 0,
-  numberOfPilgrimsNotVaccinated: 0,
-  totalNumberOfPeoplewithFirstDose: 0,
-  totalNumberOfPeoplewithSecondDose: 0,
-  totalNumberOfPeoplewithThirdDose: 0,
-  totalNumberOfPeoplewithFourthDose: 0,
-  totalNumberOfPeoplewithFifthDose: 0,
+  pass6MonthFromLastVaccines: 0,
+  totalNonVaccines: 0,
+  totalVaccines: 0,
+  totalZaerin: 0,
+  zaerinGroupByDoses: [
+    {dose: 2, count: 0},
+    {dose: 5, count: 0},
+    {dose: 4, count: 0},
+    {dose: 1, count: 0},
+    {dose: 3, count: 0},
+  ],
 };
+
 const OverviewPilgrimVaccineStatus = () => {
   const [loading, setLoading] = useState(false);
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [pilgrims, setPilgrims] = useState<any>(initialValue);
   const {CancelToken} = axios;
   const source = CancelToken.source();
 
+  const {data: totalInfo, loading: loadingPositiveTest} = useGetArbaeenCountData({
+    countLastPositiveTestResultWhileRegistered: true,
+  });
   const getAllPilgrims = async () => {
     setLoading(true);
     try {
-      // eslint-disable-next-line @typescript-eslint/no-unused-vars
-      const {data} = await arbaeenService.arbaeenGetAll(
-        {tag: 'transparent'},
-        {cancelToken: source.token}
-      );
-      const newData = {
-        numberOfVaccinatedPilgrims: 9500,
-        numberOfPilgrimsNotVaccinated: 500,
-        totalNumberOfPeoplewithFirstDose: 1000,
-        totalNumberOfPeoplewithSecondDose: 1200,
-        totalNumberOfPeoplewithThirdDose: 6000,
-        totalNumberOfPeoplewithFourthDose: 800,
-        totalNumberOfPeoplewithFifthDose: 500,
-      };
-      setPilgrims(newData);
+      const {data} = await arbaeenService.getVaccineInfo({}, {cancelToken: source.token});
+
+      setPilgrims({...data});
     } catch (error) {
       console.log(error);
     } finally {
@@ -56,9 +49,15 @@ const OverviewPilgrimVaccineStatus = () => {
     }
   };
 
+  const getValue = (i: number) => {
+    const data = pilgrims?.zaerinGroupByDoses?.find((item: any) => item.dose === i);
+    return data?.count || 0;
+  };
   useEffect(() => {
     getAllPilgrims();
     return () => {
+      setPilgrims({...initialValue});
+
       source.cancel('Operation canceled by the user.');
     };
   }, []);
@@ -72,25 +71,25 @@ const OverviewPilgrimVaccineStatus = () => {
             <Statistic
               icon={totalVacsinateStart}
               text="تعداد زائران واکسن زده"
-              count={499922}
+              count={pilgrims.totalVaccines || 0}
               loading={loading}
             />
             <Statistic
-              icon={redVaccine}
+              icon={sufferingIcon}
               text=" تعداد زائران ثبت نامی با کوید مثبت"
-              count={81}
-              loading={loading}
+              count={totalInfo.countLastPositiveTestResultWhileRegistered || 0}
+              loading={loadingPositiveTest}
             />
             <Statistic
               icon={personGrayVaccine}
               text="تعداد زائران واکسن نزده"
-              count={20569}
+              count={pilgrims.totalNonVaccines || 0}
               loading={loading}
             />
             <Statistic
               icon={YellowVaccine}
               text="تعداد کل زائران  با دوز اول"
-              count={17340}
+              count={getValue(1)}
               loading={loading}
             />
           </div>
@@ -98,28 +97,39 @@ const OverviewPilgrimVaccineStatus = () => {
             <Statistic
               icon={OrangeVaccine}
               text="تعداد کل زائران  با دوز دوم"
-              count={156400}
+              count={getValue(2)}
               loading={loading}
             />
             <Statistic
               icon={PurppleVaccine}
               text="تعداد کل زائران  با دوز سوم"
-              count={238317}
+              count={getValue(3)}
               loading={loading}
             />
             <Statistic
               icon={DarkgreenVaccine}
               text="تعداد کل زائران  با دوز چهارم"
-              count={51534}
+              count={getValue(4)}
               loading={loading}
             />
             <Statistic
               icon={NavyVaccine}
               text="تعداد کل زائران  با دوز پنجم"
-              count={36331}
+              count={getValue(5)}
               loading={loading}
             />
-            {/* <div className="flex flex-col align-center justify-center w-full rounded-xl p-4 relative" /> */}
+          </div>
+          <div className="flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 space-x-0 md:space-x-5 rtl:space-x-reverse">
+            <Statistic
+              icon={redVaccine}
+              text=" تعداد زائران فاقد شرایط واکسیناسیون"
+              count={pilgrims.pass6MonthFromLastVaccines || 0}
+              loading={loading}
+            />
+
+            <div className="flex flex-col align-center justify-center w-full rounded-xl p-4 relative" />
+            <div className="flex flex-col align-center justify-center w-full rounded-xl p-4 relative" />
+            <div className="flex flex-col align-center justify-center w-full rounded-xl p-4 relative" />
           </div>
         </div>
       </fieldset>
