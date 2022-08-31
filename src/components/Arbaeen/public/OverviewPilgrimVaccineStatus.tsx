e323cd80d73d2cefffda9f6290cce907ae845d3a
Muhammad Shahabipour <muh.shahabipour@gmail.com>
2022-09-01 01:30:32 +0430

    change var to num in pilgrim

--- src/components/Arbaeen/public/OverviewPilgrimVaccineStatus.tsx
+++ src/components/Arbaeen/public/OverviewPilgrimVaccineStatus.tsx
@@ -123,7 +123,8 @@ const OverviewPilgrimVaccineStatus = () => {
             <Statistic
               icon={redVaccine}
               text=" تعداد زائران فاقد شرایط واکسیناسیون"
-              count={pilgrims.pass6MonthFromLastVaccines || 0}
+              // count={pilgrims.pass6MonthFromLastVaccines || 0}
+              count={388948}
               loading={loading}
             />
 
