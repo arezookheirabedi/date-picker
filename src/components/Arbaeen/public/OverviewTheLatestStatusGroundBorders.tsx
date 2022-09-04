fb24e4925225964fe7452c703c4d0451310b22e1
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-09-04 14:03:36 +0430

    add new component

---
+++ src/components/Arbaeen/public/OverviewTheLatestStatusGroundBorders.tsx
@@ -0,0 +1,127 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import React, {useEffect, useState} from 'react';
+import arbaeenService from 'src/services/arbaeen.service';
+import axios from 'axios';
+import Spinner from '../../Spinner';
+import RetryButton from '../../RetryButton';
+import Table from '../../Table';
+import {axisData} from './constant';
+
+const OverviewTheLatestStatusGroundBorders = () => {
+  const [error, setError] = useState(null);
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const [query, setQuery] = useState({
+    tag: 'transport',
+    category: 'serviceType',
+    from: null,
+    to: null,
+    retry: false,
+  }) as any;
+
+  const [loading, setLoading] = useState(false);
+  const [dataset, setDataSet] = useState<any>([]);
+  const {CancelToken} = axios;
+  const source = CancelToken.source();
+
+  const fetcher = async () => {
+    setLoading(true);
+    setError(null);
+
+    try {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const {data} = await arbaeenService.arbaeenGetAll(
+        {tag: 'transparent'},
+        {cancelToken: source.token}
+      );
+      setDataSet(axisData);
+    } catch (err: any) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetcher();
+    return () => {
+      source.cancel('Operation canceled by the user.');
+    };
+  }, []);
+
+  return (
+    <fieldset className="text-center border rounded-xl p-4 mb-16">
+      <legend className="text-black mx-auto px-3">نگاه کلی به آخرین وضعیت مرزهای زمینی</legend>
+      {/* <div className="p-40 text-red-500"> اطلاعات مورد نیاز دریافت نمی شود.</div> */}
+
+      <div className="flex flex-col align-center justify-center w-full rounded-xl bg-white p-4 shadow">
+        {loading && (
+          <div className="p-40">
+            <Spinner />
+          </div>
+        )}
+        {error && !loading && (
+          <div className="p-40">
+            <div className="text-red-500">{error}</div>
+            <RetryButton setQuery={setQuery} />
+          </div>
+        )}
+
+        {!error && !loading && (
+          <Table
+            dataSet={[...dataset]}
+            pagination={{pageSize: 20, maxPages: 3}}
+            columns={[
+              {
+                name: 'نام مرز',
+                key: 'name',
+                render: (v: any, record, index: number) => (
+                  <span>
+                    {(index + 1).toLocaleString('fa')}.{v}
+                  </span>
+                ),
+              },
+
+              {
+                name: ' تعداد مسافران ',
+                key: 'avaragePilgrim',
+                render: (v: any) => <span>{Number(v).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد زائران ',
+                key: 'avaragePilgrim',
+                render: (v: any) => <span>{Number(v).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد زائران در شعاع ۵۰ کیلومتری ',
+                key: 'avaragePilgrim',
+                render: (v: any) => <span>{Number(v).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد زائران در شعاع ۱۰۰ کیلومتری ',
+
+                key: 'avaragePilgrim',
+                render: (v: any) => <span>{Number(v).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد زائران در شعاع ۱۵۰ کیلومتری ',
+
+                key: 'avaragePilgrim',
+                render: (v: any) => <span>{Number(v).toLocaleString('fa')}</span>,
+              },
+              {
+                name: 'تعداد زائران در شعاع ۲۰۰ کیلومتری ',
+
+                key: 'avaragePilgrim',
+                render: (v: any) => <span>{Number(v).toLocaleString('fa')}</span>,
+              },
+            ]}
+            totalItems={(dataset || []).length}
+          />
+        )}
+      </div>
+    </fieldset>
+  );
+};
+
+export default OverviewTheLatestStatusGroundBorders;
