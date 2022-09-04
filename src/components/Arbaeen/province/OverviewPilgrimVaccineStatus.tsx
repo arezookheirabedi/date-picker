8ddc82ada62f07214375c0715536cdf244394376
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-09-04 11:02:31 +0430

    update data binding for card

--- src/components/Arbaeen/province/OverviewPilgrimVaccineStatus.tsx
+++ src/components/Arbaeen/province/OverviewPilgrimVaccineStatus.tsx
@@ -14,24 +14,11 @@ import PurppleVaccine from '../../../assets/images/icons/big-purpule-vaccine.svg
 import DarkgreenVaccine from '../../../assets/images/icons/darkgreen-vaccine.svg';
 import NavyVaccine from '../../../assets/images/icons/navy-vaccine-lg.svg';
 import redVaccine from '../../../assets/images/icons/red-vaccine.svg';
-
-const initialValue = {
-  pass6MonthFromLastVaccines: 0,
-  totalNonVaccines: 0,
-  totalVaccines: 0,
-  totalZaerin: 0,
-  zaerinGroupByDoses: [
-    {dose: 2, count: 0},
-    {dose: 5, count: 0},
-    {dose: 4, count: 0},
-    {dose: 1, count: 0},
-    {dose: 3, count: 0},
-  ],
-};
+import {initialVaccineValue} from '../public/constant';
 
 const OverviewPilgrimVaccineStatus: React.FC<{cityTitle: string}> = ({cityTitle}) => {
   const [loading, setLoading] = useState(false);
-  const [pilgrims, setPilgrims] = useState<any>(initialValue);
+  const [pilgrims, setPilgrims] = useState<any>(initialVaccineValue);
   const {CancelToken} = axios;
   const source = CancelToken.source();
 
@@ -77,7 +64,7 @@ const OverviewPilgrimVaccineStatus: React.FC<{cityTitle: string}> = ({cityTitle}
     }
     // eslint-disable-next-line consistent-return
     return () => {
-      setPilgrims({...initialValue});
+      setPilgrims({...initialVaccineValue});
 
       source.cancel('Operation canceled by the user.');
     };
