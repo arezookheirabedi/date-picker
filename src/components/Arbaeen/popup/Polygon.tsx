ef1c7a5b883de4f6c9a40ac3887e6bb6bb7dfa2a
Muhammad Sh <muh.shahabipour@gmail.com>
2022-09-05 21:39:30 +0430

    add file name

---
+++ src/components/Arbaeen/popup/Polygon.tsx
@@ -0,0 +1,90 @@
+import React, {useEffect, useState} from 'react';
+import Loading from 'src/components/Loading';
+import arbaeenService from 'src/services/arbaeen.service';
+import qs from 'qs';
+
+const Polygon: React.FC<any> = ({params}: any) => {
+  console.log(params);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [data, setData] = useState<any>({
+    location: null,
+    numberOfAvailableAmbulances: null,
+    numberOfAvailableAutolances: null,
+    numberOfAvailableMotolances: null,
+  });
+  console.log('data => ', data);
+
+  const fetchPopupData = async (param: any) => {
+    setLoading(true);
+    try {
+      const {data: polygonData} = await arbaeenService.getPolygonData(encodeURI(param));
+      setData(polygonData);
+    } catch (err) {
+      console.log(err);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    console.log('gdasafgsaf');
+    fetchPopupData(
+      qs.stringify(
+        {
+          coordinates: [...params.geometry.coordinates[0]].map(x => ({x: x[0], y: x[1]})),
+        },
+        {arrayFormat: 'indices', allowDots: true}
+      )
+    );
+  }, [params]);
+
+  return (
+    <>
+      {loading ? (
+        <div className="flex items-center text-xs">
+          <Loading />
+          <span>درحال دریافت اطلاعات</span>
+        </div>
+      ) : (
+        <>
+          <div className="hidden">{JSON.stringify(params, null, 2)}</div>
+          <div className="w-full max-h-60 overflow-y-auto px-2 custom-scrollbar-gray ">
+            <div className="flex item-center justify-start items-center border-b border-gray-300 pb-2 pt-1">
+              <span className="text-xs ml-2 text-gray-500">موقعیت : </span>
+              <span className="text-xs text-gray-500">{params.location}</span>
+            </div>
+
+            {data.numberOfAvailableAmbulances && (
+              <div className="flex justify-start items-center border-b border-gray-300 pb-2 pt-2">
+                <span className="text-xs ml-2 text-lime-600">تعداد آمبولانس : </span>
+                <span className="text-xs text-lime-600">
+                  {data.numberOfAvailableAmbulances || '-'}
+                </span>
+              </div>
+            )}
+
+            {data.numberOfAvailableAutolances && (
+              <div className="flex  justify-start items-center border-b border-gray-300 pb-2 pt-2">
+                <span className="text-xs ml-2  text-lime-600">تعداد اتولانس : </span>
+                <span className="text-xs text-lime-600">
+                  {data.numberOfAvailableAutolances || '-'}
+                </span>
+              </div>
+            )}
+
+            {data.numberOfAvailableMotolances && (
+              <div className="flex justify-start items-center  pb-1 pt-2">
+                <span className="text-xs ml-2 text-lime-600">تعداد موتولانس : </span>
+                <span className="text-xs text-lime-600">
+                  {data.numberOfAvailableMotolances || '-'}
+                </span>
+              </div>
+            )}
+          </div>
+        </>
+      )}
+    </>
+  );
+};
+
+export default Polygon;
--- src/store/action_creators/arbaeen/fetchZaerinAc.ts
+++ src/store/action_creators/arbaeen/fetchZaerinAc.ts
@@ -5,7 +5,8 @@ import csvtojson from 'csvtojson';
 import {ActionType} from '../../action_type';
 import {Action} from '../../actions/arbaeen/fetchZaerin.action';
 
-const FILE_NAME = 'ar_location_ptrue_tmp_loc';
+const FILE_NAME = 'samah_loc';
+const FILE_NAME_HOURLY = 'samah_loc_hourly';
 
 export const fetchZaerinAc = () => async (dispatch: Dispatch<Action>) => {
   dispatch({
@@ -23,7 +24,9 @@ export const fetchZaerinAc = () => async (dispatch: Dispatch<Action>) => {
     );
     const zip = await JSZip.loadAsync(response);
 
+    debugger;
     const file = await zip.file(`${FILE_NAME}.csv`)?.async('text');
+    debugger;
 
     const json = await csvtojson().fromString(file || '');
 
@@ -31,6 +34,37 @@ export const fetchZaerinAc = () => async (dispatch: Dispatch<Action>) => {
   } catch (error: any) {
     dispatch({
       type: ActionType.FETCH_ZAERIN_ERROR,
+      payload: error.message || 'خطایی در عملیات',
+    });
+  }
+};
+
+export const fetchZaerinHourlyAc = () => async (dispatch: Dispatch<Action>) => {
+  dispatch({
+    type: ActionType.FETCH_ZAERIN_HOURLY,
+    payload: undefined,
+  });
+
+  try {
+    // make a request to fetch guild info and dispatch with appropriate type and payload
+    const {data: response} = await arbaeenService.getPiligrimReportAsFile(
+      {
+        fileName: `${FILE_NAME_HOURLY}.zip`,
+      },
+      {responseType: 'blob'}
+    );
+    const zip = await JSZip.loadAsync(response);
+
+    debugger;
+    const file = await zip.file(`${FILE_NAME_HOURLY}.csv`)?.async('text');
+    debugger;
+
+    const json = await csvtojson().fromString(file || '');
+
+    dispatch({type: ActionType.FETCH_ZAERIN_HOURLY_SUCCESS, payload: json});
+  } catch (error: any) {
+    dispatch({
+      type: ActionType.FETCH_ZAERIN_HOURLY_ERROR,
       payload: error.message || 'خطایی در عملیات',
     });
   }
