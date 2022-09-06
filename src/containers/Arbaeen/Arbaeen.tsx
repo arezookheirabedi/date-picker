31891f492ebf25da2cff34bdd04581092b80d63e
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-09-06 11:43:23 +0430

    update

--- src/containers/Arbaeen/Arbaeen.tsx
+++ src/containers/Arbaeen/Arbaeen.tsx
@@ -2,7 +2,6 @@
 import React from 'react';
 import Irancell from 'src/assets/images/logos/irancell-logo.svg';
 import Vasl from 'src/assets/images/logos/vasl-logo.svg';
-import OverviewPilgrimVaccineStatus from 'src/components/Arbaeen/public/OverviewPilgrimVaccineStatus';
 // import OverviewOfTheLatestVaccinationStatusOfPilgrims from 'src/components/Arbaeen/public/OverviewOfTheLatestVaccinationStatusOfPilgrims';
 // import OverviewArbaeen from 'src/components/Arbaeen/public/OverviewArbaeen';
 import OverviewOfTheEntryAndExitOfPilgrims from 'src/components/Arbaeen/public/OverviewOfTheEntryAndExitOfPilgrims';
@@ -12,27 +11,22 @@ import MokebList from 'src/components/Arbaeen/public/MokebList';
 import TheLargestNumberOfOriginPilgrimsCitiesList from 'src/components/Arbaeen/public/ThPilgrimsProvinceList';
 // import TheLargestNumberOfOriginPilgrimsCountriesList from 'src/components/Arbaeen/public/TheLargestNumberOfOriginPilgrimsCountriesList';
 // import RedHalalBasesList from 'src/components/Arbaeen/public/RedHalalBasesList';
-import OverviewPilgrim from 'src/components/Arbaeen/public/OverviewPilgrim';
-import OverviewPligrimTripType from 'src/components/Arbaeen/public/OverviewPligrimTripType';
 // import OverviewPilgrimHelthStatus from 'src/components/Arbaeen/public/OverviewPilgrimHelthStatus';
-import OverviewOfExistBorders from 'src/components/Arbaeen/public/OverviewOfExistBorders';
 import OverviewPilgrimGenderByProvince from 'src/components/Arbaeen/public/OverviewPilgrimGenderByProvince';
-import OverviewPligrimAge from 'src/components/Arbaeen/public/OverviewPligrimAge';
-import TheLatestOverviewPilgrimVaccineStatus from 'src/components/Arbaeen/public/TheLatestOverviewPilgrimVaccineStatus';
 import OverviewPligrimGroundTripType from 'src/components/Arbaeen/public/OverviewPligrimGroundTripType';
-import OverviewOfExistBordersPercentage from 'src/components/Arbaeen/public/OverviewOfExistBordersPercentage';
-import OverviewPligrimAgePercentage from 'src/components/Arbaeen/public/OverviewPligrimAgePercentage';
-import OverviewPilgrimPercentage from 'src/components/Arbaeen/public/OverviewPilgrimPercentage';
-import OverviewPligrimTripTypePercentage from 'src/components/Arbaeen/public/OverviewPligrimTripTypePercentage';
-import OverviewPilgrimVaccineStatusPercentage from 'src/components/Arbaeen/public/OverviewPilgrimVaccineStatusPercentage';
 // import OverviewOfTheLatestVaccinationStatusOfPilgrims from 'src/components/Arbaeen/public/OverviewOfTheLatestVaccinationStatusOfPilgrims';
-import TheLatestOverviewPilgrimVaccineStatusPercentage from 'src/components/Arbaeen/public/TheLatestOverviewPilgrimVaccineStatusPercentage';
 import ComparsionPasengersEntranceAndExistanceAtBoarders from 'src/components/Arbaeen/public/ComparsionPasengersEntranceAndExistanceAtBoarders';
 import OverviewTheLatestStatusGroundBorders from 'src/components/Arbaeen/public/OverviewTheLatestStatusGroundBorders';
 // import ListOfTheBusiestBorderCrossings from '../../components/Arbaeen/public/ListOfTheBusiestBorderCrossings';
 import EmergencyList from 'src/components/Arbaeen/public/EmergencyList';
 import ParckingList from 'src/components/Arbaeen/public/ParckingList';
 import OverviewPilgrimPercentagePerProvince from 'src/components/Arbaeen/public/OverviewPilgrimPercentagePerProvince';
+import OverviewPilgrim from 'src/components/Arbaeen/public/overviewPilgrim';
+import OverviewPligrimTripType from 'src/components/Arbaeen/public/OverviewPligrimTripType';
+import OverviewRegisterVaccinesInfo from 'src/components/Arbaeen/public/OverviewRegisterVaccinesInfo';
+import OverviewLatestVaccinesInfo from 'src/components/Arbaeen/public/OverviewLatestVaccinesInfo';
+import OverviewOfExistBorder from 'src/components/Arbaeen/public/OverviewOfExistBorder';
+import OverviewPilgrimAge from 'src/components/Arbaeen/public/OverviewPilgrimAge';
 import ListOfTransportationAxesStatus from '../../components/Arbaeen/public/ListOfTransportationAxesStatus';
 import GeneralLookAtTheProcessOfTheEntryAndExitOfPilgrims from '../../components/Arbaeen/public/GeneralLookAtTheProcessOfTheEntryAndExitOfPilgrims';
 // import GeneralLookAtTheLocationOfProcessionsAndRedCrescentBases from '../../components/Arbaeen/public/GeneralLookAtTheLocationOfProcessionsAndRedCrescentBases';
@@ -41,21 +35,17 @@ const Arbaeen = () => {
   return (
     <div className="space-y-16 mb-8">
       <OverviewPilgrim />
-      <OverviewPilgrimPercentage />
       <OverviewPligrimTripType />
-      <OverviewPligrimTripTypePercentage />
+
       <OverviewPligrimGroundTripType />
       {/* <OverviewPilgrimHelthStatus /> */}
-      <OverviewPilgrimVaccineStatus />
-      <OverviewPilgrimVaccineStatusPercentage />
-      <TheLatestOverviewPilgrimVaccineStatus />
-      <TheLatestOverviewPilgrimVaccineStatusPercentage />
-      {/* <OverviewOfTheLatestVaccinationStatusOfPilgrims /> */}
-      <OverviewOfExistBorders />
-      <OverviewOfExistBordersPercentage />
+      <OverviewRegisterVaccinesInfo />
+      <OverviewLatestVaccinesInfo />
 
-      <OverviewPligrimAge />
-      <OverviewPligrimAgePercentage />
+      {/* <OverviewOfTheLatestVaccinationStatusOfPilgrims /> */}
+
+      <OverviewOfExistBorder />
+      <OverviewPilgrimAge />
       <OverviewPilgrimGenderByProvince />
       <OverviewPilgrimPercentagePerProvince />
       <TheLargestNumberOfOriginPilgrimsCitiesList />
