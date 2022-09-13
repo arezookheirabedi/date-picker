d0c520bab7f71da0b808a1c808c16f4e44d206e5
emadabbasii <emadabbasee@gmail.com>
2022-09-13 16:25:22 +0430

    fix some error

--- src/components/Layout/Private/components/Today.tsx
+++ src/components/Layout/Private/components/Today.tsx
@@ -14,16 +14,19 @@ const Today: React.FC<{}> = () => {
       const {data} = await publicService.getTime();
       setDate(new Date(data.dateTime));
     } catch (e) {
-      setDate(e);
+      setDate(null);
     }
   };
 
   useEffect(() => {
     let id: any = null;
     id = setInterval(() => {
-      setDate((prev: any) => {
-        return new Date(prev.setTime(prev.getTime() + 30000));
-      });
+      if (date) {
+        setDate((prev: any) => {
+          return new Date(prev.setTime(prev.getTime() + 30000));
+        });
+      }
+
     }, 60000);
 
     return () => clearInterval(id);
@@ -35,20 +38,21 @@ const Today: React.FC<{}> = () => {
 
   return (
     <div className="relative z-20 inline-block text-left shadow-xl rounded-full px-5 py-1">
-      <div className="inline-flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
-        <img src={event} alt="time" className="w-5 h-5" />
+      <div
+        className="inline-flex justify-center items-center w-full py-2 text-sm font-medium focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75">
+        <img src={event} alt="time" className="w-5 h-5"/>
         <span className="mx-3 whitespace-nowrap truncate" style={{direction: 'ltr'}}>
           {date !== null
             ? date
-                .toLocaleDateString('fa-IR', {
-                  year: 'numeric',
-                  weekday: 'long',
-                  month: '2-digit',
-                  day: '2-digit',
-                  hour: '2-digit',
-                  minute: '2-digit',
-                })
-                .replace(/،/, ' ')
+              .toLocaleDateString('fa-IR', {
+                year: 'numeric',
+                weekday: 'long',
+                month: '2-digit',
+                day: '2-digit',
+                hour: '2-digit',
+                minute: '2-digit',
+              })
+              .replace(/،/, ' ')
             : dayjs(new Date().getTime()).calendar('jalali').locale('fa').format('dddd-DD MMMM')}
         </span>
       </div>
