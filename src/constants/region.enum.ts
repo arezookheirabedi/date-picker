c872da649bd631865d08c964fde53be6382e7cb1
a.kheirabadi92@gmail.com <a.kheirabadi92@gmail.com>
2022-09-18 17:17:17 +0430

    enum region

---
+++ src/constants/region.enum.ts
@@ -0,0 +1,11 @@
+export enum EREGION {
+  KHORAMSHAR = 'خرمشهر',
+  PAYANE_SHALAMCHE = 'پایانه مرزی شلمچه',
+  PAYANE_MEHRAN = 'پایانه مرزی مهران',
+  MEHRAN = 'مهران',
+  GHASRSHIRIN = 'قصر شیرین',
+  PAYANE_CHAZABE = 'پایانه مرزی چذابه',
+  PAYANE_BASHMAGH = 'پایانه مرزی باشماق',
+  PAYANE_KHOSRAVI = 'پایانه مرزی خسروی',
+  PAYANE_TAMARCHIN = 'پایانه مرزی تمرچین',
+}
